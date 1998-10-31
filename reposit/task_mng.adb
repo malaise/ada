401dd1a532dca8bc679d67b195dd9121c9dc055c
Pascal MALAISE <malaise@magic.fr>
1998-10-31 08:16:53 +0000

    Clean termination

--- reposit/task_mng.adb
+++ reposit/task_mng.adb
@@ -6,16 +6,44 @@ package body TASK_MNG is
 
   -- The effective period of task and its state
   REAL_PERIOD : DURATION := ACTIVATION_PERIOD;
-  TASK_STATE : TASK_STATE_LIST := STOPPED;
 
+  -- The current state of the task
+  TASK_STATE : TASK_STATE_LIST := STOPPED;
 
   task THE_TASK is
     pragma PRIORITY (SYSTEM.PRIORITY'FIRST);
     -- change task's state
-    entry NEW_STATE;
+    entry SET_STATE;
     -- activate
     entry SCHEDULE;
   end THE_TASK;
+
+  procedure SET_TASK_STATE (NEW_STATE : in TASK_STATE_LIST) is
+  begin
+    if not THE_TASK'CALLABLE then
+      raise TASK_ABORTED;
+    end if;
+    if TASK_STATE = ABORTED then
+      raise TASK_ABORTED;
+    end if;
+    if NEW_STATE = TASK_STATE then
+      return;
+    end if;
+
+    TASK_STATE := NEW_STATE;
+    if NEW_STATE /= ABORTED then
+      THE_TASK.SET_STATE;
+    else
+      select
+        -- Try to warn the task
+        THE_TASK.SET_STATE;
+      or
+        delay 1.0;
+        -- kill the task which does not answer
+        abort THE_TASK;
+      end select;
+    end if;
+  end SET_TASK_STATE;
 
   task body THE_TASK is
     use CALENDAR;
@@ -25,30 +53,29 @@ package body TASK_MNG is
     loop
 
       select
-        accept NEW_STATE do 
-          -- Tasks's state changes
-          if TASK_STATE = RUNNING then
-            -- Task starts : activate it immediatly
-            NEXT_GO := CALENDAR.CLOCK;
-          end if;
-        end;
+        accept SET_STATE;
+        -- Tasks's state changes
+        if TASK_STATE = RUNNING then
+          -- Task starts : activate it immediatly
+          NEXT_GO := CALENDAR.CLOCK;
+        end if;
       or
         when TASK_STATE = RUNNING =>
           accept SCHEDULE do
+            -- Call_back in rendez-vous for re-entrance
             if CALENDAR.CLOCK >= NEXT_GO then
               NEXT_GO := NEXT_GO + REAL_PERIOD;
               begin
                 CALL_BACK;
               exception
                 when others =>
-                TASK_STATE := ABORTED;
+                  TASK_STATE := ABORTED;
               end;
             end if;
-          end;
+          end SCHEDULE;
+      or
+        terminate;
       end select;
-
-
-      exit when TASK_STATE = ABORTED;
 
     end loop;
   end THE_TASK;
@@ -58,7 +85,7 @@ package body TASK_MNG is
   -- If period is <  minimum return minimum
   -- Else return period
   function CHECK_PERIOD (NEW_PERIOD : DURATION; INIT : BOOLEAN := FALSE)
-  return DURATION is
+                        return DURATION is
   begin
     if NEW_PERIOD <= 0.0 then
       if not INIT then
@@ -81,15 +108,9 @@ package body TASK_MNG is
   -- If the task has been aborted, exception is raised.
   procedure START (NEW_PERIOD : in DURATION := ACTIVATION_PERIOD) is
   begin
-    if TASK_STATE /= ABORTED then
-      REAL_PERIOD := CHECK_PERIOD(NEW_PERIOD);
-      TASK_STATE := RUNNING;
-      -- Warn task about new state
-      THE_TASK.NEW_STATE;
-    else
-      raise TASK_ABORTED;
-    end if;
-
+    REAL_PERIOD := CHECK_PERIOD(NEW_PERIOD);
+    -- Warn task about new state
+    SET_TASK_STATE (RUNNING);
   end START;
 
   -- When the the task is started, stops it.
@@ -97,32 +118,16 @@ package body TASK_MNG is
   -- If the task has been aborted, exception is raised.
   procedure STOP is
   begin
-    if TASK_STATE /= ABORTED then
-      TASK_STATE := STOPPED;
-      -- Warn task about new state
-      THE_TASK.NEW_STATE;
-    else
-      raise TASK_ABORTED;
-    end if;
+    -- Warn task about new state
+    SET_TASK_STATE (STOPPED);
   end STOP;
 
   -- Aborts the task, mandatory for the main program to exit.
   -- If the task is already aborted, exception is raised.
   procedure ABORT_TASK is
   begin
-    if TASK_STATE /= ABORTED then
-      TASK_STATE := ABORTED;
-      select
-        -- Try to warn the task
-        THE_TASK.NEW_STATE;
-      or
-        delay 1.0;
-        -- kill the task which does not answer
-        abort THE_TASK;
-      end select;
-    else
-      raise TASK_ABORTED;
-    end if;
+    -- Try to warn the task
+    SET_TASK_STATE (ABORTED);
   end ABORT_TASK;
 
 
@@ -145,7 +150,7 @@ package body TASK_MNG is
       when RUNNING =>
         THE_TASK.SCHEDULE;
       when ABORTED =>
-        raise TASK_ABORTED;
+        return;
     end case;
   end SCHEDULE;
 
