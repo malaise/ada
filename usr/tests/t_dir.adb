76cacbc4bd5d5cec5e5e455eb060669e10e8734a
Pascal MALAISE <malaise@magic.fr>
1998-11-29 09:26:56 +0000

    Display link target List argument_dir if any

--- usr/tests/t_dir.adb
+++ usr/tests/t_dir.adb
@@ -1,6 +1,7 @@
-with MY_IO, DIRECTORY, TEXT_HANDLER;
+with MY_IO, DIRECTORY, TEXT_HANDLER, ARGUMENT;
 procedure T_DIR is
   FILE_NAME : TEXT_HANDLER.TEXT(DIRECTORY.MAX_DIR_NAME_LEN);
+  DIR_NAME : TEXT_HANDLER.TEXT(DIRECTORY.MAX_DIR_NAME_LEN);
   KIND : DIRECTORY.FILE_KIND_LIST;
   RIGHTS : NATURAL;
   MAX_LEN : constant := 50;
@@ -15,6 +16,7 @@ procedure T_DIR is
     return STR (1 .. LEN);
   end  GET_NEW_DIR;
 
+  use DIRECTORY;
 begin
 
   loop
@@ -24,19 +26,34 @@ begin
     declare
       DSC : DIRECTORY.DIR_DESC;
     begin
-      DSC := DIRECTORY.OPEN(DIRECTORY.GET_CURRENT);
+      if ARGUMENT.GET_NBRE_ARG /= 0 then
+        ARGUMENT.GET_PARAMETER(DIR_NAME);
+      else
+        DIRECTORY.GET_CURRENT(DIR_NAME);
+      end if;
+      DSC := DIRECTORY.OPEN(TEXT_HANDLER.VALUE(DIR_NAME));
       loop
         DIRECTORY.NEXT_ENTRY(DSC, FILE_NAME);
         MY_IO.PUT ("  ---->" & TEXT_HANDLER.VALUE (FILE_NAME) & "< ");
         MY_IO.PUT (PAD(1 .. MAX_LEN - TEXT_HANDLER.LENGTH(FILE_NAME)));
-        DIRECTORY.FILE_STAT (TEXT_HANDLER.VALUE (FILE_NAME), KIND, RIGHTS);
+        DIRECTORY.FILE_STAT (
+         TEXT_HANDLER.VALUE (DIR_NAME) & '/' &
+         TEXT_HANDLER.VALUE (FILE_NAME), KIND, RIGHTS);
         MY_IO.PUT (RIGHTS, BASE => 8, WIDTH => 4);
         MY_IO.PUT_LINE (" " & DIRECTORY.FILE_KIND_LIST'IMAGE(KIND));
+        if KIND = DIRECTORY.SYMBOLIC_LINK then
+          MY_IO.PUT_LINE ("    ++++>" & DIRECTORY.READ_LINK (
+              TEXT_HANDLER.VALUE (DIR_NAME) & '/' &
+              TEXT_HANDLER.VALUE (FILE_NAME)) & '<');
+        end if;
       end loop;
     exception
       when DIRECTORY.END_ERROR =>
         DIRECTORY.CLOSE (DSC);
     end;
+    if ARGUMENT.GET_NBRE_ARG /= 0 then
+      return;
+    end if;
 
     MY_IO.NEW_LINE;
     loop
