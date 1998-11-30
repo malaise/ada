536119db25c4f95fb481250830184f1ac68d03af
Pascal MALAISE <malaise@magic.fr>
1998-11-30 18:12:49 +0000

    Improve robustnes and rights format

--- usr/tests/t_dir.adb
+++ usr/tests/t_dir.adb
@@ -1,4 +1,4 @@
-with MY_IO, DIRECTORY, TEXT_HANDLER, ARGUMENT;
+with MY_IO, INT_IO, DIRECTORY, TEXT_HANDLER, ARGUMENT;
 procedure T_DIR is
   FILE_NAME : TEXT_HANDLER.TEXT(DIRECTORY.MAX_DIR_NAME_LEN);
   DIR_NAME : TEXT_HANDLER.TEXT(DIRECTORY.MAX_DIR_NAME_LEN);
@@ -16,6 +16,30 @@ procedure T_DIR is
     return STR (1 .. LEN);
   end  GET_NEW_DIR;
 
+  procedure PUT_RIGHTS (RIGHTS : in NATURAL) is
+    STR : STRING(1 .. 7) := (others => ' ');
+    ZSTR : STRING(1 .. 4) := (others => '0');
+    F,L : NATURAL;
+  begin
+    INT_IO.PUT (STR, RIGHTS, BASE => 8);
+    for I in STR'RANGE loop
+      if STR(I) = '#' then
+        F := I+1;
+        exit;
+      end if;
+    end loop;
+    for I in reverse STR'RANGE loop
+      if STR(I) = '#' then
+        L := I - 1;
+        exit;
+      end if;
+    end loop;
+
+    ZSTR(4-L+F .. 4) := STR(F .. L);
+    MY_IO.PUT(ZSTR);
+  end PUT_RIGHTS;
+
+
   use DIRECTORY;
 begin
 
@@ -32,20 +56,28 @@ begin
         DIRECTORY.GET_CURRENT(DIR_NAME);
       end if;
       DSC := DIRECTORY.OPEN(TEXT_HANDLER.VALUE(DIR_NAME));
+      if TEXT_HANDLER.VALUE(DIR_NAME) = "/" then
+        TEXT_HANDLER.EMPTY(DIR_NAME);
+      end if;
       loop
         DIRECTORY.NEXT_ENTRY(DSC, FILE_NAME);
         MY_IO.PUT ("  ---->" & TEXT_HANDLER.VALUE (FILE_NAME) & "< ");
         MY_IO.PUT (PAD(1 .. MAX_LEN - TEXT_HANDLER.LENGTH(FILE_NAME)));
-        DIRECTORY.FILE_STAT (
-         TEXT_HANDLER.VALUE (DIR_NAME) & '/' &
-         TEXT_HANDLER.VALUE (FILE_NAME), KIND, RIGHTS);
-        MY_IO.PUT (RIGHTS, BASE => 8, WIDTH => 4);
-        MY_IO.PUT_LINE (" " & DIRECTORY.FILE_KIND_LIST'IMAGE(KIND));
-        if KIND = DIRECTORY.SYMBOLIC_LINK then
-          MY_IO.PUT_LINE ("    ++++>" & DIRECTORY.READ_LINK (
-              TEXT_HANDLER.VALUE (DIR_NAME) & '/' &
-              TEXT_HANDLER.VALUE (FILE_NAME)) & '<');
-        end if;
+        begin
+          DIRECTORY.FILE_STAT (
+             TEXT_HANDLER.VALUE (DIR_NAME) & '/' &
+             TEXT_HANDLER.VALUE (FILE_NAME), KIND, RIGHTS);
+          PUT_RIGHTS (RIGHTS);
+          MY_IO.PUT_LINE (" " & DIRECTORY.FILE_KIND_LIST'IMAGE(KIND));
+          if KIND = DIRECTORY.SYMBOLIC_LINK then
+            MY_IO.PUT_LINE ("    ++++>" & DIRECTORY.READ_LINK (
+                TEXT_HANDLER.VALUE (DIR_NAME) & '/' &
+                TEXT_HANDLER.VALUE (FILE_NAME)) & '<');
+          end if;
+        exception
+          when DIRECTORY.NAME_ERROR =>
+            MY_IO.PUT_LINE ("???? ???");
+        end;
       end loop;
     exception
       when DIRECTORY.END_ERROR =>
