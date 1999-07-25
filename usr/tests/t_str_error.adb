ff02e1dc730957ba1fe6a135bcd2388ab96ee1cb
Pascal MALAISE <malaise@magic.fr>
1999-07-25 07:09:21 +0000

    256 values of errno is far enough

--- usr/tests/t_str_error.adb
+++ usr/tests/t_str_error.adb
@@ -2,7 +2,7 @@ with MY_IO, SYS_CALLS, NORMAL;
 procedure T_STR_ERROR is
 begin
 
-  for I in 1 .. 999 loop
+  for I in 1 .. 256 loop
     exit when SYS_CALLS.STR_ERROR(I) = "";
     MY_IO.PUT_LINE (NORMAL(I, 3) & " -> " & SYS_CALLS.STR_ERROR(I) );
   end loop;
