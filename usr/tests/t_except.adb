204068d8b671eea51c35fb6beb897f704f2bd9fc
Pascal MALAISE <malaise@magic.fr>
1999-07-25 07:08:50 +0000

    current_exception.exception_name

---
+++ usr/tests/t_except.adb
@@ -0,0 +1,17 @@
+with Current_Exception;
+with My_Io;
+
+procedure T_Except is
+
+begin
+  
+
+  My_Io.Put_Line ("Before exception");
+  raise Program_Error;
+  My_Io.Put_Line ("After  exception");
+
+exception
+  when others =>
+    My_Io.Put_Line ("In handler");
+    My_Io.Put_Line ("Exception: " & Current_exception.Exception_Name);
+end T_Except;
