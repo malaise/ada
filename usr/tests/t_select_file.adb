e34b05496914cce1d5916d3641f8c1709ba76053
Pascal MALAISE <malaise@magic.fr>
1999-08-03 17:33:36 +0000

    test for file selection, moved in reposit

---
+++ usr/tests/t_select_file.adb
@@ -0,0 +1,26 @@
+with TEXT_IO;
+with TEXT_HANDLER;
+with SELECT_FILE;
+
+procedure T_FILE_SELECTION is
+
+  READ : BOOLEAN;
+  FILE : TEXT_HANDLER.TEXT(128);
+
+  procedure INIT is
+  begin
+    NULL;
+  end INIT;
+
+  function MY_SELECT_FILE is new SELECT_FILE(INIT);
+
+begin
+  READ := TRUE;
+
+  loop
+    TEXT_HANDLER.SET (FILE, MY_SELECT_FILE (1, TEXT_HANDLER.VALUE(FILE), READ));
+    TEXT_IO.PUT_LINE (TEXT_HANDLER.VALUE(FILE));
+    exit when TEXT_HANDLER.EMPTY(FILE);
+    READ := not READ;
+  end loop;
+end T_FILE_SELECTION;
