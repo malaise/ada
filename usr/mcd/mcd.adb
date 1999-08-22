d8d15fdf6b925d55961c27d5cec3e0a10a892306
Pascal MALAISE <malaise@magic.fr>
1999-08-22 07:57:34 +0000

    Remap exceptions. Warning on stderr.

--- usr/mcd/mcd.adb
+++ usr/mcd/mcd.adb
@@ -1,10 +1,14 @@
 with TEXT_IO;
-with ARGUMENT;
+with ARGUMENT, SYS_CALLS;
 with DEBUG, PARSER, MCD_MNG;
 
 procedure MCD is
   ITEM : MCD_MNG.ITEM_REC;
   THE_END : BOOLEAN;
+  INVALID_ARGUMENT, ARGUMENT_MISMATCH, INVALID_REGISTER, EMTPY_REGISTER,
+                    EMPTY_STACK : exception;
+  PARSING_ERROR : exception;
+
 begin
 
   DEBUG.INIT;
@@ -21,8 +25,22 @@ begin
    end loop;
    
    if not MCD_MNG.CHECK_EMPTY_STACK then
-     TEXT_IO.PUT_LINE ("Warning, the stack was not empty.");
+     SYS_CALLS.PUT_LINE_ERROR ("Warning, the stack was not empty.");
    end if;
 
+exception
+  -- Clean mapping of exceptions
+  when MCD_MNG.INVALID_ARGUMENT =>
+    raise INVALID_ARGUMENT;
+  when MCD_MNG.ARGUMENT_MISMATCH =>
+    raise ARGUMENT_MISMATCH;
+  when MCD_MNG.INVALID_REGISTER =>
+    raise INVALID_REGISTER;
+  when MCD_MNG.EMTPY_REGISTER =>
+    raise EMTPY_REGISTER;
+  when MCD_MNG.EMPTY_STACK =>
+    raise EMPTY_STACK;
+  when PARSER.PARSING_ERROR =>
+    raise PARSING_ERROR;
 end MCD;
 
