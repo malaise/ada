1a29ee868d628d824dd88e6b0a7cb5d3a8b9e106
Pascal MALAISE <malaise@magic.fr>
1999-11-21 10:24:49 +0000

    3.12p tasking_soft_links -> soft_links

--- reposit/current_exception.adb
+++ reposit/current_exception.adb
@@ -1,11 +1,11 @@
 with Ada.Exceptions;
-with System.Tasking_Soft_Links;
+with System.Soft_Links;
 package body Current_Exception is
 
   function Exception_Name return String is
   begin
     return Ada.Exceptions.Exception_Name(
-             System.Tasking_Soft_Links.Get_Current_Excep_NT.all);
+             System.Soft_Links.Get_Current_Excep_NT.all);
   end Exception_Name;
 
 end Current_Exception;
