c48e28079f6c59ea9a07cd495e52cf0252decfaf
Pascal MALAISE <malaise@magic.fr>
2000-01-07 18:29:25 +0000

    Specify gap

--- usr/tests/t_norm.adb
+++ usr/tests/t_norm.adb
@@ -4,14 +4,16 @@ procedure T_NORM is
   I : INTEGER;
   M : POSITIVE;
   R : BOOLEAN;
+  G : CHARACTER;
 begin
   loop
     begin
       PUT ("I ? "); GET (I);
       PUT ("Max ? "); GET (M);
       PUT ("Right (TRUE, FALSE) ? "); GET (R);
+      PUT ("Gap char ? "); GET (G);
       PUT_LINE (" 12345678901234567890");
-      PUT_LINE (">" & NORMAL (I, M, R) & "<");
+      PUT_LINE (">" & NORMAL (I, M, R, G) & "<");
     exception
       when others =>
         PUT_LINE ("Exception.");
