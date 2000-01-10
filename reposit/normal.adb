19d8c62e6214d86017bee6bbd517d96ae0d399ca
Pascal MALAISE <malaise@magic.fr>
2000-01-10 17:40:15 +0000

    Have trunc and round local.

--- reposit/normal.adb
+++ reposit/normal.adb
@@ -1,4 +1,3 @@
-with MATH;
 -- Puts an INTEGER in a string of fixed length.
 -- If I is shorter than max, it is aligned at right or left
 -- If I is longer, it is rounded if possible (or truncated)
@@ -9,39 +8,71 @@ with MATH;
 --   or at left (not RIGHT) and fill with GAP
 -- GAP : When string is shorter than len, fill empty positions with GAP
 
-function NORMAL (I : INTEGER; LEN : POSITIVE; RIGHT : BOOLEAN := TRUE;
- GAP : CHARACTER := ' ') return STRING is
+function NORMAL (I : INTEGER; LEN : POSITIVE;
+ RIGHT : BOOLEAN := TRUE; GAP : CHARACTER := ' ') return STRING is
   L : POSITIVE := INTEGER'IMAGE(I)'LAST;
-  SI : STRING (1 .. L) :=  INTEGER'IMAGE(I);
+  SI : STRING (1 .. L) := INTEGER'IMAGE(I);
   SM : STRING (1 .. LEN);
-  use MATH;
+
+  -- Real -> integer : round or trunc
+  function TRUNC (X : in FLOAT) return INTEGER is
+    INT : INTEGER;
+  begin
+    INT := INTEGER (X);
+    -- Adjust to 1
+    if X > 0.0 then
+      -- If x>0 error is 1 too much
+      if FLOAT (INT) > X then INT := INT - 1; end if;
+      return INT;
+    else
+      -- If x<0 error is 1 too less
+      if FLOAT (INT) < X then INT := INT + 1; end if;
+      return INT;
+    end if;
+  exception
+    when others => raise CONSTRAINT_ERROR;
+  end TRUNC;
+
+  function ROUND (X : in FLOAT) return INTEGER is
+    RESULTAT : INTEGER;
+  begin
+    if X > 0.0 then
+      RESULTAT := TRUNC  (X + 0.5);
+    else
+      RESULTAT := TRUNC  (X - 0.5);
+    end if;
+    return RESULTAT;
+  exception
+    when others => raise CONSTRAINT_ERROR;
+  end ROUND;
+
+
 begin
-  -- skip first char if space
+  -- Skip first char if space
   if SI(1) = ' ' then
     L := L - 1;
     SI (1 .. L) := SI (2 .. L + 1);
   end if;
   if L > LEN then
-    -- round I at LEN digits
+    -- Round I at LEN digits
     declare
-      -- round I at len digits
-      R : MATH.REAL := MATH.REAL(I) / (10.0 ** (L-LEN) );
-      NI : INTEGER := INTEGER(MATH.ROUND (R));
+      -- Round I at len digits
+      R : FLOAT := FLOAT(I) / (10.0 ** (L-LEN) );
+      NI : INTEGER := INTEGER(ROUND (R));
       NL : INTEGER := INTEGER'IMAGE(NI)'LAST;
     begin
-      -- correction when i.e. 999.6 is rounded to 1000 : keep 999
+      -- Correction when i.e. 999.6 is rounded to 1000 : keep 999
       if INTEGER'IMAGE(NI)(1) = ' ' then
         NL := NL - 1;
       end if;
       if NL > LEN then
-        NI := INTEGER(MATH.TRUNC (R) );
+        NI := INTEGER(TRUNC (R) );
       end if;
-      -- should be OK now
-      SM := NORMAL (NI, LEN, RIGHT);
+      -- Should be OK now
+      return NORMAL (NI, LEN, RIGHT);
     end;
-  end if;
-  if L <= LEN then
-    -- gap with gap_character, in SM
+  else -- L <= LEN
+    -- Gap with gap_character, in SM
     if RIGHT then
       SM (1 .. LEN-L) := (others => GAP);
       SM (LEN-L+1 .. LEN) := SI (1 ..L);
@@ -49,7 +80,7 @@ begin
       SM (1 .. L) := SI (1 .. L);
       SM (L+1 .. LEN) := (others => GAP);
     end if;
+    return SM;
   end if;
-  return SM;
 end NORMAL;
 
