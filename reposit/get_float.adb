33198bcded4996d64f7c5ee1205822ced757c863
Pascal MALAISE <malaise@magic.fr>
2000-01-22 09:46:37 +0000

    Copy from approx coo_str

--- reposit/get_float.adb
+++ reposit/get_float.adb
@@ -3,18 +3,43 @@
 
 with FLO_IO, INT_IO, IO_EXCEPTIONS;
 function GET_FLOAT (STR : STRING) return FLOAT is
-  I : INTEGER;
   F : FLOAT;
-  L : NATURAL;
+  L : POSITIVE;
+  I : INTEGER;
+  STR_LEN : NATURAL;
 begin
+  -- Locate last significant character of STR
+  STR_LEN := 0;
+  for J in reverse STR'RANGE loop
+    if STR(J) /= ' ' then
+      STR_LEN := J + 1 - STR'FIRST;
+      exit;
+    end if;
+  end loop;
+  if STR_LEN = 0 then
+    raise CONSTRAINT_ERROR;
+  end if; 
+
+  TRY_TO_CONVERT:
   begin
-    -- Try to read float
-    FLO_IO.GET (STR, F, L);
-    return F;
+    -- Float format
+    FLO_IO.GET(STR, F, L);
   exception
     when IO_EXCEPTIONS.DATA_ERROR =>
-      -- Try to read int
-      INT_IO.GET (STR, I, L);
-      return FLOAT (I);
-  end;
+       -- Int format
+      INT_IO.GET(STR, I, L);
+      F := FLOAT(I);
+  end TRY_TO_CONVERT;
+
+
+  if L /= STR'LAST then
+    raise CONSTRAINT_ERROR;
+  else
+    return F;
+  end if;
+
+exception
+  when others =>
+    raise CONSTRAINT_ERROR;
 end GET_FLOAT;
+
