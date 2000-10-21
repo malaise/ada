9e81ee9f0446e130014197f416ef17ac56560d4d
Pascal MALAISE <malaise@magic.fr>
2000-10-21 09:09:17 +0000

    Add cost computation

--- usr/fl/fl.adb
+++ usr/fl/fl.adb
@@ -1,53 +1,100 @@
 with TEXT_IO;
-
+with ARGUMENT, NORMAL, MY_IO, GET_FLOAT;
 with FL_TIME, FL_GET;
 
 procedure FL is
 
-  package INT_IO is new TEXT_IO.INTEGER_IO (INTEGER);
-
   TT, T : FL_TIME.TIME_TYPE;
-  STR_2 : STRING (1..2);
   MAX_HOUR_DIG : constant := 9; -- FL_TIME.HOURS_RANGE'WIDTH;
+  WITH_COST : BOOLEAN;
+  COST, TMP_COST : FLOAT;
   use FL_TIME;
 begin
+  if ARGUMENT.GET_NBRE_ARG = 1 and then
+     ARGUMENT.GET_PARAMETER = "-c" then
+    WITH_COST := TRUE;
+  elsif ARGUMENT.GET_NBRE_ARG = 0 then
+    WITH_COST := FALSE;
+  else
+    TEXT_IO.PUT_LINE ("USAGE: " & ARGUMENT.GET_PROGRAM_NAME
+                    & " [ -c ]");
+    return;
+  end if;
+    
   TEXT_IO.PUT_LINE ("      HOURS and MINUTES additions.");
   TEXT_IO.PUT_LINE ("      ----------------------------");
   TEXT_IO.PUT_LINE (" Syntax of time is [-]hhhhhhh[.mm] (Return);");
   TEXT_IO.PUT_LINE (" Enter 'C'to clear, 'X' or 'Q' to exit.");
   TEXT_IO.NEW_LINE (2);
 
-
-
+  -- Initialise
   TT := (TRUE, 0, 0);
+  COST := 0.0;
+  TMP_COST := 0.0;
+
   loop
+    -- Display
     TEXT_IO.PUT ("--> ");
 
     if TT.POSITIV then
-      TEXT_IO.PUT (' '); 
+      TEXT_IO.PUT (' ');
     else
-      TEXT_IO.PUT ('-'); 
+      TEXT_IO.PUT ('-');
     end if;
-
-    INT_IO.PUT (INTEGER(TT.HOURS), MAX_HOUR_DIG + 1);
+    TEXT_IO.PUT (NORMAL(INTEGER(TT.HOURS), MAX_HOUR_DIG + 1));
     TEXT_IO.PUT ('.');
-    INT_IO.PUT (STR_2, INTEGER(TT.MINUTES));
-    if STR_2(1) = ' ' then STR_2(1) := '0'; end if;
-    TEXT_IO.PUT_LINE (STR_2);
+    TEXT_IO.PUT (NORMAL(INTEGER(TT.MINUTES), 2, GAP => '0'));
+    if WITH_COST then
+       TEXT_IO.PUT ("     This cost: ");
+       MY_IO.PUT(TMP_COST, AFT => 2, EXP => 0);
+       TEXT_IO.PUT ("  Total cost: ");
+       MY_IO.PUT_LINE(COST, AFT => 2, EXP => 0);
+    else
+      TEXT_IO.NEW_LINE;
+    end if;
+      
 
+    -- Get
     begin
       T := FL_GET.GET_TIME;
     exception
       when FL_GET.ERROR =>
         T := (TRUE, 0, 0);
+        TMP_COST := 0.0;
         TEXT_IO.PUT_LINE (ASCII.BEL & "Error.");
       when FL_GET.CLEAR =>
         T := (TRUE, 0, 0);
         TT := (TRUE, 0, 0);
+        TMP_COST := 0.0;
+        COST := 0.0;
       when FL_GET.QUIT =>
         exit;
     end;
 
+    if WITH_COST and then T /= (TRUE, 0, 0) then
+      -- Get hourly cost of aircraft
+      declare
+        STR : STRING(1 .. 8);
+        LEN : NATURAL;
+      begin
+        TEXT_IO.PUT(">>");
+        TEXT_IO.GET_LINE(STR, LEN);
+        TMP_COST := GET_FLOAT.GET_FLOAT(STR(1 .. LEN));
+      exception
+        when others =>
+          T := (TRUE, 0, 0);
+          TMP_COST := 0.0;
+          TEXT_IO.PUT_LINE (ASCII.BEL & "Error.");
+      end;
+      -- Cost of the flight
+      TMP_COST := TMP_COST * FLOAT(T.HOURS)
+                + TMP_COST * FLOAT(T.MINUTES) / 60.0;
+      if not T.POSITIV then
+        TMP_COST := -TMP_COST;
+      end if;
+    end if;
+
+    -- Add / substract
     declare
       ST : constant FL_TIME.TIME_TYPE := TT;
     begin
@@ -60,6 +107,7 @@ begin
         TT := ST;
         TEXT_IO.PUT_LINE (ASCII.BEL & "Overflow.");
     end;
+    COST := COST + TMP_COST;
 
   end loop;
 
