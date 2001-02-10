8b0c4bcad2be181d2b985099da5ddabcacb807e8
Pascal MALAISE <malaise@magic.fr>
2001-02-10 07:56:32 +0000

    Display the state machine. Improve the state machine.

--- usr/tests/t_state_machine.adb
+++ usr/tests/t_state_machine.adb
@@ -32,36 +32,73 @@ procedure T_STATE_MACHINE is
     return EVENT;
   end GET_EVENT;
 
+
+  procedure PUT_TRANSITION (TRANSITION : in TRANSITION_REC) is
+    procedure PUTS (STR : in STRING) is
+      STRMAX : STRING(1 .. 8) := (others => ' ');
+    begin
+      if STR'LENGTH <= STRMAX'LENGTH then
+        STRMAX(1 .. STR'LENGTH) := STR;
+        MY_IO.PUT(STRMAX);
+      else
+        MY_IO.PUT(STR);
+      end if;
+    end PUTS;
+  begin
+    PUTS(STATE_LIST'IMAGE(TRANSITION.ORIGINAL_STATE));
+    MY_IO.PUT(" -- ");
+    PUTS(EVENT_LIST'IMAGE(TRANSITION.EVENT));
+    MY_IO.PUT(" -> ");
+    PUTS(STATE_LIST'IMAGE(TRANSITION.DESTINATION_STATE));
+    MY_IO.NEW_LINE;
+  end PUT_TRANSITION;
+
   procedure DISPLAY_TRANSITION (PREV_STATE : in STATE_LIST;
                                 EVENT : in EVENT_LIST;
                                 NEW_STATE : in STATE_LIST) is
   begin
-    MY_IO.PUT_LINE (STATE_LIST'IMAGE(PREV_STATE) & " -- " & EVENT_LIST'IMAGE(EVENT)
-                  & " -> " &STATE_LIST'IMAGE(NEW_STATE));
-  end DISPLAY_TRANSITION;
+    if NEW_STATE /= PREV_STATE then
+      PUT_TRANSITION( (PREV_STATE, EVENT, NEW_STATE) );
+    end if;
+  end DISPLAY_TRANSITION; 
+  
+
+  procedure MY_ADD_TRANSITION (TRANSITION : in TRANSITION_REC) is
+  begin
+    MSM.ADD_TRANSITION(TRANSITION);
+    PUT_TRANSITION(TRANSITION);
+  end MY_ADD_TRANSITION;
+
 begin
 
+  MY_IO.PUT_LINE("State machine definition:");
   -- Declare state machine
-  ADD_TRANSITION ( (UNKNOWN,  DETACH,    DETACHED) );
-  ADD_TRANSITION ( (UNKNOWN,  START,     STARTING) );
-  ADD_TRANSITION ( (UNKNOWN,  TRUE,      STARTING) );
-  ADD_TRANSITION ( (STARTING, DETACH,    DETACHED) );
-  ADD_TRANSITION ( (STARTING, START,     STARTING) );
-  ADD_TRANSITION ( (STARTING, FAILURE,   FAILED)   );
-  ADD_TRANSITION ( (STARTING, SUCCESS,   OK)       );
-  ADD_TRANSITION ( (FAILED,   DETACH,    DETACHED) );
-  ADD_TRANSITION ( (FAILED,   START,     STARTING) );
-  ADD_TRANSITION ( (OK,       DETACH,    DETACHED) );
-  ADD_TRANSITION ( (OK,       START,     STARTING) );
-  ADD_TRANSITION ( (OK,       FAILURE,   FAILED)   );
-  ADD_TRANSITION ( (DETACHED, ATTACH,    UNKNOWN)  );
---ADD_TRANSITION ( (OK,       TRUE,      FAILED)   );
---ADD_TRANSITION ( (FAILED,   TRUE,      OK)       );
-  ADD_TRANSITION ( (UNKNOWN,  DEFAULT,   ERROR)    );
-  ADD_TRANSITION ( (STARTING, DEFAULT,   ERROR)    );
-  ADD_TRANSITION ( (FAILED,   DEFAULT,   ERROR)    );
-  ADD_TRANSITION ( (OK,       DEFAULT,   ERROR)    );
-  ADD_TRANSITION ( (ERROR,    DETACH,    DETACHED) );
+  MY_ADD_TRANSITION ( (UNKNOWN,  DETACH,    DETACHED) );
+  MY_ADD_TRANSITION ( (UNKNOWN,  START,     STARTING) );
+  MY_ADD_TRANSITION ( (UNKNOWN,  TRUE,      STARTING) );
+  MY_ADD_TRANSITION ( (UNKNOWN,  ATTACH,    UNKNOWN) );
+  MY_ADD_TRANSITION ( (UNKNOWN,  DEFAULT,   ERROR)    );
+  MY_ADD_TRANSITION ( (STARTING, DETACH,    DETACHED) );
+  MY_ADD_TRANSITION ( (STARTING, START,     STARTING) );
+  MY_ADD_TRANSITION ( (STARTING, FAILURE,   FAILED)   );
+  MY_ADD_TRANSITION ( (STARTING, SUCCESS,   OK)       );
+  MY_ADD_TRANSITION ( (STARTING, ATTACH,    STARTING) );
+  MY_ADD_TRANSITION ( (STARTING, DEFAULT,   ERROR)    );
+  MY_ADD_TRANSITION ( (FAILED,   DETACH,    DETACHED) );
+  MY_ADD_TRANSITION ( (FAILED,   START,     STARTING) );
+  MY_ADD_TRANSITION ( (FAILED,   ATTACH,    FAILED)   );
+  MY_ADD_TRANSITION ( (FAILED,   DEFAULT,   ERROR)    );
+  MY_ADD_TRANSITION ( (OK,       DETACH,    DETACHED) );
+  MY_ADD_TRANSITION ( (OK,       FAILURE,   FAILED)   );
+  MY_ADD_TRANSITION ( (OK,       ATTACH,    OK)       );
+  MY_ADD_TRANSITION ( (OK,       DEFAULT,   ERROR)    );
+  MY_ADD_TRANSITION ( (DETACHED, ATTACH,    UNKNOWN)  );
+  MY_ADD_TRANSITION ( (ERROR,    DETACH,    DETACHED) );
+-- For true_loop detection
+--MY_ADD_TRANSITION ( (OK,       TRUE,      FAILED)   );
+--MY_ADD_TRANSITION ( (FAILED,   TRUE,      OK)       );
+  MY_IO.PUT_LINE("End of state machine definition.");
+  MY_IO.NEW_LINE;
   END_DECLARATION;
 
   MY_IO.PUT_LINE ("Initial state : " & STATE_LIST'IMAGE(CURRENT_STATE));
@@ -73,7 +110,7 @@ begin
     VALID_EVENT := TRUE;
     case CUR_STATE is
       when FAILED =>
-        MY_IO.PUT_LINE (" test program --> setting state to unknown");
+        MY_IO.PUT_LINE (" test program : setting state to unknown");
         SET_STATE (UNKNOWN);
       when others =>
         begin 
@@ -84,6 +121,8 @@ begin
         end;
         if VALID_EVENT then
           NEW_EVENT (EVENT);
+        else
+          MY_IO.PUT_LINE (" ???");
         end if;
     end case;
   end loop;
