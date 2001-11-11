a5a83008b320e4203dbbfbe22deaca8222e91ff1
Pascal MALAISE <malaise@magic.fr>
2001-11-11 15:44:08 +0000

    Display piece letter.

--- usr/chess/image.adb
+++ usr/chess/image.adb
@@ -1,72 +1,82 @@
-with Normal, Lower_Str;
-with Space, Pieces;
-function Image (Action : Game.Action_Rec;
-                Result : Game.Move_Status_List) return String is
-  -- Result
-  Str : String (1 .. 9) := (others => ' ');
-  First : Positive := 1;
+with Normal, Lower_Str, Text_Handler;
+with Space.Board;
 
-  function Image (Square : in Space.Square_Coordinate) return  String is
+package body Image is
+
+  function Piece_Image (Piece_Kind : Pieces.Piece_Kind_List) return String is
+    use type Pieces.Piece_Kind_List;
   begin
-    return Lower_Str (Space.Col_Range'Image(Square.Col))
-                   & Normal(Integer(Square.Row), 1);
-  end Image;
+    if Piece_Kind = Pieces.Knight then
+      return "N";
+    elsif Piece_Kind = Pieces.Pawn then
+      return "";
+    else
+      return Pieces.Promotion_Piece_List'Image(Piece_Kind)(1 .. 1);
+    end if;
+  end Piece_Image;
 
-  function Image (Kind : Pieces.Promotion_Piece_List) return Character is
+  function Move_Image (Action : Game.Action_Rec;
+                       Result : Game.Move_Status_List) return Move_Str is
+    -- Acting piece
+    Piece : constant Pieces.Piece_Kind_List
+          := Pieces.Id_Of (Space.Board.Piece_At (Action.To.Dest).all).Kind;
+    -- Result
+    Res : Text_Handler.Text(10);
+    Str : Move_Str := (others => ' ');
+
+    function Image (Square : in Space.Square_Coordinate) return  String is
+    begin
+      return Lower_Str (Space.Col_Range'Image(Square.Col))
+                     & Normal(Integer(Square.Row), 1);
+    end Image;
+
+
+    use type Space.Col_Range;
+    use type Pieces.Action_Kind_List;
   begin
-    return Pieces.Promotion_Piece_List'Image(Kind)(1);
-  end Image;
-
-  use type Space.Col_Range;
-  use type Pieces.Action_Kind_List;
-begin
-  case Action.To.Kind is
-    when Pieces.Move =>
-      Str(1 .. 5) := Image (Action.From) & "-" & Image (Action.To.Dest);
-      First := 6;
-    when Pieces.Take =>
-      Str(1 .. 5) := Image (Action.From) & "X" & Image (Action.To.Dest);
-      First := 6;
-    when Pieces.Take_En_Passant =>
-      Str(1 .. 7) := Image (Action.From) & "X" & Image (Action.To.Dest) & "ep";
-      First := 8;
-    when Pieces.Castle =>
-      if Action.To.Rook_From.Col = Space.H then
-        -- Small castle
-        Str(1 .. 3) := "o-o";
-        First := 4;
-      else
-        -- Big castle
-        Str(1 .. 5) := "o-o-o";
-        First := 6;
-      end if;
-    when Pieces.Promote =>
-      Str(1 .. 7) := Image (Action.From) & "-" & Image (Action.To.Dest)
-                   & "=" & Image(Action.To.New_Piece);
-      First := 8;
-    when Pieces.Take_And_Promote =>
-      Str(1 .. 7) := Image (Action.From) & "X" & Image (Action.To.Dest)
-                   & "=" & Image(Action.To.New_Piece);
-      First := 8;
-    when Pieces.Cover =>
-      Str(1 .. 5) := "Cover";
-      First := 6;
-  end case;
-  -- Check, checkmate, stalemate
-  case Result is
-    when Game.Nok =>
-      Str (First .. First + 1) := "Nk";
-    when Game.Ok =>
-      null;
-    when Game.Check =>
-      Str (First .. First) := "+";
-    when Game.Stalemate =>
-      Str (First .. First + 1) := "==";
-    when Game.CheckMate =>
-      Str (First .. First + 1) := "++";
-  end case;
-  return Str;
+    Text_Handler.Set (Res, Piece_Image (Piece));
+    case Action.To.Kind is
+      when Pieces.Move =>
+        Text_Handler.Append (Res, Image (Action.From) & "-" & Image (Action.To.Dest) );
+      when Pieces.Take =>
+        Text_Handler.Append (Res, Image (Action.From) & "X" & Image (Action.To.Dest) );
+      when Pieces.Take_En_Passant =>
+        Text_Handler.Append (Res, Image (Action.From) & "X" & Image (Action.To.Dest) & "ep");
+      when Pieces.Castle =>
+        -- Overwrite piece
+        if Action.To.Rook_From.Col = Space.H then
+          -- Small castle
+          Text_Handler.Set (Res, "o-o");
+        else
+          -- Big castle
+           Text_Handler.Set (Res, "o-o-o");
+        end if;
+      when Pieces.Promote =>
+        -- Overwrite piece
+        Text_Handler.Set (Res, Image (Action.From) & "-" & Image (Action.To.Dest)
+                               & "=" & Piece_Image (Action.To.New_Piece) );
+      when Pieces.Take_And_Promote =>
+        -- Overwrite piece
+        Text_Handler.Set (Res, Image (Action.From) & "X" & Image (Action.To.Dest)
+                          & "=" & Piece_Image(Action.To.New_Piece) );
+      when Pieces.Cover =>
+        Text_Handler.Set (Res, "Cover");
+    end case;
+    -- Check, checkmate, stalemate
+    case Result is
+      when Game.Nok =>
+        Text_Handler.Append (Res, "Nk");
+      when Game.Ok =>
+        null;
+      when Game.Check =>
+        Text_Handler.Append (Res, "+");
+      when Game.Stalemate =>
+        Text_Handler.Append (Res, "==");
+      when Game.CheckMate =>
+        Text_Handler.Append (Res, "++");
+    end case;
+    Str (1 .. Text_Handler.Length(Res)) := Text_Handler.Value (Res);
+    return Str;
+  end Move_Image;
+        
 end Image;
-      
-
-
--- usr/chess/image.ads
+++ usr/chess/image.ads
@@ -1,17 +1,27 @@
-with Game;
--- From the action and the result (Cr being column and row)
--- Move      -> Cr-Cr
--- Take      -> CrXCr
--- EnPassant -> CrXCrep
--- Castle    -> o-o
---           or o-o-o
--- Promotion -> Cr-Cr=K    (K is one letter: kind of new piece)
---           or CrXCr=K
--- Plus extra:
--- Check        +
--- Checkmate    ++
--- Stalemate    ==
--- -- Which make 9 characters max
-function Image (Action : Game.Action_Rec;
-                Result : Game.Move_Status_List) return String;
+with Pieces, Game;
+package Image is
+  -- From the action and the result (Cr being column and row)
+  -- Piece character (none for Pawn), or R, N, B, Q, K
+  --  got from the To.Dest square of the action (except promotion)
+  -- Then
+  -- Move      -> Cr-Cr
+  -- Take      -> CrXCr
+  -- EnPassant -> CrXCrep
+  -- Castle    -> o-o
+  --           or o-o-o
+  -- Promotion -> Cr-Cr=K    (K is one letter: kind of new piece)
+  --           or CrXCr=K
+  -- Plus extra:
+  -- Check        +
+  -- Checkmate    ++
+  -- Stalemate    ==
+  -- -- Which make 10 characters max
+  subtype Move_Str is String (1 .. 10);
+  function Move_Image (Action : Game.Action_Rec;
+                       Result : Game.Move_Status_List) return Move_Str;
+
+  -- Piece character (none for Pawn), or R, N, B, Q, K
+  function Piece_Image (Piece_Kind : in Pieces.Piece_Kind_List) return String;
+
+end Image;
 
