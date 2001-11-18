e8fb7a03313481bed5678869bb4c1a72544ee59d
Pascal MALAISE <malaise@magic.fr>
2001-11-18 22:24:35 +0000

    nbres_c -> c_nbres

--- usr/navig/nav_data-add.adb
+++ usr/navig/nav_data-add.adb
@@ -1,21 +1,18 @@
 -- When 1 vector is unknown compute it by addition or substraction of
 --  the 2 others
-with NBRES_C;
+with C_NBRES;
 separate (NAV_DATA)
 function ADD (X, Y : NAV_TYPES.T_VECTOR) return NAV_TYPES.T_VECTOR is
-  CX, CY, CR : NBRES_C.COMPLEX;
+  CX, CY, CR : C_NBRES.COMPLEX;
 begin
-  CX := NBRES_C.CREATE_COMPLEX(NBRES_C.REAL(X.SPEED),
-                               NBRES_C.DEGREE(TO_REAL(X.ANGLE)) );
-  CY := NBRES_C.CREATE_COMPLEX(NBRES_C.REAL(Y.SPEED),
-                               NBRES_C.DEGREE(TO_REAL(Y.ANGLE)) );
-  CR := NBRES_C."+" (CX, CY);
+  CX := C_NBRES.CREATE_COMPLEX(C_NBRES.REAL(X.SPEED),
+                               C_NBRES.DEGREE(TO_REAL(X.ANGLE)) );
+  CY := C_NBRES.CREATE_COMPLEX(C_NBRES.REAL(Y.SPEED),
+                               C_NBRES.DEGREE(TO_REAL(Y.ANGLE)) );
+  CR := C_NBRES."+" (CX, CY);
 
   return (
-   SPEED => NAV_TYPES.T_SPEED(NBRES_C.MODULE(CR)),
-   ANGLE => TO_ANGLE(REAL(NBRES_C.ANGLE_DEGREE(CR))) );
+   SPEED => NAV_TYPES.T_SPEED(C_NBRES.MODULE(CR)),
+   ANGLE => TO_ANGLE(REAL(C_NBRES.ANGLE_DEGREE(CR))) );
 end ADD;
-
-
-
 
