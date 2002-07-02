091ddc5ceef09997cc05a4acc03da0670ccf82b8
Pascal MALAISE <malaise@magic.fr>
2002-07-02 19:04:28 +0000

    Update comments.

--- reposit/dynamic_list.ads
+++ reposit/dynamic_list.ads
@@ -7,35 +7,35 @@ package Dynamic_List is
   -- descriptor of the list
   type List_Type is limited private;
 
-  -- for READ and MODIFY to set new position
+  -- for Read and Modify to set new position
   type Movement is (Next, Prev, Current);
 
-  -- for INSERT, GET and DELETE, to set new position
-  -- for MOVE_TO and SEARCH to set direction of move
+  -- for Insert, Get and Delete, to set new position
+  -- for Move_To and Search to set direction of move
   subtype Direction is Movement range Next .. Prev;
 
-  -- for GET_POSITION
+  -- for Get_Position
   type Reference is (From_First, From_Last);
 
 
-  -- All calls except IS_EMPTY and LIST_LENGTH may raise
-  -- EMPTY_LIST if the list is empty
+  -- All calls except Is_Empty and List_Length may raise
+  -- Empty_List if the list is empty
 
   -- read the current item then moves to another item
-  -- may raise NOT_IN_LIST (no read nor movement done)
+  -- may raise Not_In_List (no read nor movement done)
   procedure Read (List : in out List_Type;
                   Item : out Element_Type;
                   Move : in Movement := Next);
 
   -- modify the current item then moves to another item
-  -- may raise NOT_IN_LIST (no movement done)
+  -- may raise Not_In_List (no movement done)
   procedure Modify (List : in out List_Type;
                     Item : in Element_Type;
                     Move : in Movement := Next);
 
   -- insert a new item after or before the current item
   --  the new item becomes then the current item
-  -- may raise FULL_LIST (no more memory)
+  -- may raise Full_List (no more memory)
   procedure Insert (List  : in out List_Type;
                     Item  : in Element_Type;
                     Where : in Direction := Next);
@@ -43,14 +43,15 @@ package Dynamic_List is
   -- read and delete the current item
   --  the current item is then the next or the previous item in the list
   --  except when deleting last item (no movement done!)
-  -- may raise NOT_IN_LIST (no get nor movement done)
+  -- may raise Not_In_List (no get nor movement done)
   procedure Get (List : in out List_Type;
                  Item : out Element_Type;
                  Move : in Direction := Next);
 
   -- suppress the current element from the list
   --  the current item is then the next or the previous item in the list
-  -- may raise NOT_IN_LIST (no deletion nor movement done)
+  -- may raise Not_In_List (no deletion nor movement done)
+  -- does not raise Empty_List when deleting the last item
   procedure Delete (List : in out List_Type; Move : in Direction := Next);
 
   -- delete the full list
@@ -59,22 +60,23 @@ package Dynamic_List is
      Deallocate : in Boolean := True);
 
   -- set the current element to number elements before or after
-  --  if FROM_CURRENT is FALSE, then counting is from the first
-  --  item in the list (NEXT), or the last (PREV).
-  -- may raise NOT_IN_LIST (no movement done)
-  --  example MOVE_TO (LIST, NEXT, 1, FALSE) goes to SECOND element
+  --  if From_Current is False, then counting is from the first
+  --  item in the list (Next), or the last (Prev).
+  -- may raise Not_In_List (no movement done)
+  --  examples Move_To (List, Next, 1, False) goes to SECOND element
+  --           Move_To (List, Next, 0, False) goes to FIRST element
   procedure Move_To (List         : in out List_Type;
                      Where        : in Direction := Next;
                      Number       : in Natural := 1;
                      From_Current : in Boolean := True);
 
   -- permute 2 elements
-  --  if FROM_CURRENT is TRUE,  then numbers of elements are relative from
+  --  if From_CUrrent is True,  then numbers of elements are relative from
   --   current
-  --  if FROM_CURRENT is FALSE, then counting is from the first
-  --   item in the list (NEXT), or the last (PREV).
-  -- may raise NOT_IN_LIST (no movement done)
-  --  example PERMUTE (LIST, 0, 1, NEXT, FALSE) permutes 1st and 2nd elements
+  --  if From_Current is False, then counting is from the first
+  --   item in the list (Next), or the last (Prev).
+  -- may raise Not_In_List (no movement done)
+  --  example Permute (List, 0, 1, Next, False) permutes 1st and 2nd elements
   procedure Permute (List         : in out List_Type;
                      Number1      : in Natural;
                      Number2      : in Natural;
@@ -88,8 +90,8 @@ package Dynamic_List is
   function List_Length (List : List_Type) return Natural;
 
   -- get position from first or last item in list
-  -- For first item of list, GET_POSITION returns 1
-  -- may raise EMPTY_LIST
+  -- For first item of list, Get_Position returns 1
+  -- may raise Empty_List
   function Get_Position (List : List_Type;
                          From : Reference := From_First) return Positive;
 
@@ -100,20 +102,20 @@ package Dynamic_List is
   function Is_Modified (List : List_Type) return Boolean;
   procedure Modification_Ack (List : in out List_Type);
 
-  -- Copy the VAL list to TO list
-  -- CARE: Risk of side effect because LIST_TYPE only is duplicated
+  -- Copy the Val list to To list
+  -- CARE: Risk of side effect because List_Type only is duplicated
   procedure Assign (To : in out List_Type; Val : in List_Type);
 
   generic
     with function Equal (El1, El2 : Element_Type) return Boolean is "=";
 
-  -- search from the nth occurence of an iten EQUAL to the provided one
-  -- starts from : same as MOVE_TO
-  -- may raise NOT_IN_LIST if the given element is not found
+  -- search from the nth occurence of an item Equal to the provided one
+  -- starts from : same as Move_To
+  -- may raise Not_In_List if the given element is not found
   --  also if empty list
   --  (position not changed) otherwise, the current position is set to
   --  the item found
-  -- If OCCURENCE is 1 and current/first/last matches, then it is selected
+  -- If Occurence is 1 and current/first/last matches, then it is selected
   procedure Search (List         : in out List_Type;
                     Item         : in Element_Type;
                     Where        : in Direction := Next;
@@ -122,8 +124,8 @@ package Dynamic_List is
 
   generic
     -- Comparison function for sorting
-    -- WARNING : LESS_TAHN must be strict
-    --  (i.e. LESS_THAN(EL1, EL1) = FALSE)
+    -- WARNING : Less_Than must be strict
+    --  (i.e. Less_Than(El1, El1) = False)
     with function Less_Than (El1, El2 : Element_Type) return Boolean;
 
   -- sort all the list in crescent order
