cadd33da90d21e3a973c15af07fda164a86f6b79
Pascal MALAISE <malaise@magic.fr>
2002-10-15 17:22:30 +0000

    Put time.

--- usr/dictio/dictio_debug.adb
+++ usr/dictio/dictio_debug.adb
@@ -1,5 +1,6 @@
-with Ada.Text_Io;
-with Sys_Calls; use Sys_Calls;
+with Ada.Text_Io, Ada.Calendar;
+with Sys_Calls, Normal, Day_Mng;
+use Sys_Calls;
 package body Debug is
   Val : String (1 .. 1);
   Set, Trunc : Boolean;
@@ -37,7 +38,21 @@ package body Debug is
   end Init;
 
   procedure Put (Str : in String) is
+    Year : Ada.Calendar.Year_Number;
+    Month : Ada.Calendar.Month_Number;
+    Day : Ada.Calendar.Day_Number;
+    Dur : Ada.Calendar.Day_Duration;
+    Hour : Day_Mng.T_Hours;
+    Minute : Day_Mng.T_Minutes;
+    Second : Day_Mng.T_Seconds;
+    Millisec : Day_Mng.T_Millisec;
   begin
+    Ada.Calendar.Split (Ada.Calendar.Clock, Year, Month, Day, Dur);
+    Day_Mng.Split (Dur, Hour, Minute, Second, Millisec);
+    Ada.Text_Io.Put (Normal (Hour, 2, Gap => '0') & ":"
+                   & Normal (Minute, 2, Gap => '0') & ":"
+                   & Normal (Second, 2, Gap => '0') & "."
+                   & Normal (Millisec, 3, Gap => '0') & " ");
     Ada.Text_Io.Put_Line (Str);
   end Put;
 
