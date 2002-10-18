3881ee0b1d9f25e288a3f44d4bda5b0014ae0420
Pascal MALAISE <malaise@magic.fr>
2002-10-18 17:06:05 +0000

    Replace haevy list of timers by data (event) in timer.

--- usr/dictio/status.adb
+++ usr/dictio/status.adb
@@ -1,4 +1,4 @@
-with Timers, Dynamic_List;
+with Timers;
 with Debug;
 package body Status is
 
@@ -6,49 +6,28 @@ package body Status is
   Stable_Status : Stable_Status_List := Dead;
   Status_Cb : New_Status_Callback := null;
 
-  -- List of pending statuses
-  type Pending_Rec is record
-    Tid : Timers.Timer_Id;
-    Status : Status_List;
-  end record;
-  function Tid_Match (R1, R2 : Pending_Rec) return Boolean is
-    use type Timers.Timer_Id;
-  begin
-    return R1.Tid = R2.Tid;
-  end Tid_Match;
-  package Pending_List_Mng is new Dynamic_List(Pending_Rec);
-  Pending_List : Pending_List_Mng.List_Type;
-  procedure Search_Tid is new Pending_List_Mng.Search (Tid_Match);
-
-  function Timer_Cb (Id : Timers.Timer_Id) return Boolean is
-    Rec : Pending_Rec;
+  function Timer_Cb (Id : Timers.Timer_Id;
+                     Data : Timers.Timer_Data) return Boolean is
     Prev_Status : constant Status_List := Current_Status;
+    New_Status : Status_List;
   begin
-    -- Look for status in pending list
-    Rec.Tid := Id;
+
     begin
-      Search_Tid (Pending_List, Rec, From_Current => False);
+      New_Status := Status_List'Val(Data);
     exception
-      when Pending_List_Mng.Not_In_List =>
+      when others =>
         if Debug.Level_Array(Debug.Status) then
-          Debug.Put ("Status: Timer " & Timers.Image (Id)
-                   & " not found in pending list");
+          Debug.Put ("Status: Cb with invalid status " & Data'Img);
         end if;
         return False;
     end;
-    -- Read status and delete rec
-    if Pending_List_Mng.Get_Position (Pending_List) = 1 then
-      Pending_List_Mng.Get (Pending_List, Rec, Pending_List_Mng.Next);
-    else
-      Pending_List_Mng.Get (Pending_List, Rec, Pending_List_Mng.Prev);
-    end if;
 
     -- Handle status change
-    if Current_Status /= Dead and then Rec.Status /= Current_Status then
+    if Current_Status /= Dead and then New_Status /= Current_Status then
       if Debug.Level_Array(Debug.Status) then
-        Debug.Put ("Status: " & Prev_Status'Img & " -> " & Rec.Status'Img);
+        Debug.Put ("Status: " & Prev_Status'Img & " -> " & New_Status'Img);
       end if;
-      Current_Status := Rec.Status;
+      Current_Status := New_Status;
       case Current_Status is
         when Starting | Init | Dead =>
           Stable_Status := Dead;
@@ -68,15 +47,9 @@ package body Status is
     Tid : Timers.Timer_Id;
     T : Timers.Delay_Rec;
   begin
-    -- Create immediate timer (so we go back to main loop)
+    -- Create immediate timer with status (so we go back to main loop)
     T.Delay_Seconds := 0.0;
-    Tid := Timers.Create (T, Timer_Cb'Access);
-    
-    -- Append pending status for expiration
-    if not Pending_List_Mng.Is_Empty (Pending_List) then
-      Pending_List_Mng.Move_To (Pending_List, Pending_List_Mng.Prev, 0, False);
-    end if;
-    Pending_List_Mng.Insert (Pending_List, (Tid, Status));
+    Tid := Timers.Create (T, Timer_Cb'Access, Status_List'Pos(Status));
   end Set;
 
   function Get return Status_List is
