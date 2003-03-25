9a928d48a367698d670d575cf590e9e5bbe58b34
Pascal MALAISE <malaise@magic.fr>
2003-03-25 18:15:59 +0000

    Parse host name/addr and port name/num.

---
+++ reposit/ip_addr.adb
@@ -0,0 +1,97 @@
+with Socket, Text_Handler;
+package body Ip_Addr is
+
+  -- Return -1 if convestion impossible or greater than Max
+  function To_Natural (Str : String) return Integer is
+    Num : Natural;
+    Error : constant Integer := -1;
+  begin
+    begin
+      Num := Natural'Value (Str);
+    exception
+      when others =>
+        return Error;
+    end;
+    return Num;
+  end To_Natural;
+
+  -- If Addr is "xxx.yyy.zzz.ttt" where each is between 0 and 255 then
+  --   return the Tcp_Util.Remote_Host (Tcp_Util.Host_Id_Spec)
+  -- Else
+  --   return the Tcp_Util.Remote_Host (Tcp_Util.Host_Name_Spec)
+  -- End if
+  function Parse (Addr : String) return Tcp_Util.Remote_Host is
+    Txt : Text_Handler.Text (Tcp_Util.Max_Host_Name_Len);
+    Dots : array (1 .. 3) of Natural;
+    Ip_Addr : Socket.Ip_Address;
+    Result : Tcp_Util.Remote_Host;
+  begin
+    -- Try to parse Host_Id
+    begin
+      Text_Handler.Set (Txt, Addr);
+      -- 3 and only three dots, not contiguous
+      if Text_Handler.Locate (Txt, '.', 4) /= 0 then
+        raise Parse_Error;
+      end if;
+      Dots(1) := Text_Handler.Locate (Txt, '.', 1);
+      Dots(2) := Text_Handler.Locate (Txt, '.', 2);
+      Dots(3) := Text_Handler.Locate (Txt, '.', 3);
+      if Dots(1) = 1 or else Dots(2) = Dots(1)+1 or else
+         Dots(3) = Dots(2)+1 or else Dots(3) = Text_Handler.Length(Txt) then
+        raise Parse_Error;
+      end if;
+      
+      -- 4 bytes
+      Ip_Addr.A := Socket.Byte(To_Natural(
+            Text_Handler.Value(Txt)(1 .. Dots(1)-1)));
+      Ip_Addr.B := Socket.Byte(To_Natural(
+            Text_Handler.Value(Txt)(Dots(1)+1 .. Dots(2)-1)));
+      Ip_Addr.C := Socket.Byte(To_Natural(
+            Text_Handler.Value(Txt)(Dots(2)+1 .. Dots(3)-1)));
+      Ip_Addr.D := Socket.Byte(To_Natural(
+            Text_Handler.Value(Txt)(Dots(3)+1 .. Text_Handler.Length(Txt))));
+      return (Kind => Tcp_Util.Host_Id_Spec,
+              Id   => Socket.Addr2Id (Ip_Addr) );
+    exception
+      when others =>
+        null;
+    end;
+
+    -- Try to parse Host_Name
+    Result.Name (1 .. Addr'Length) := Addr;
+    return Result;
+
+  exception
+    when others =>
+      raise Parse_Error;
+
+  end Parse;
+
+  -- If Port is a num between 0 and 65535 then
+  --   return the Tcp_Util.Remote_Port (Tcp_Util.Port_Num_Spec)
+  -- Else
+  --   return the Tcp_Util.Remote_Port (Tcp_Util.Port_Name_Spec)
+  -- End if
+  function Parse (Port : String) return Tcp_Util.Remote_Port is
+    Result : Tcp_Util.Remote_Port;
+  begin
+    -- Try to parse Port_Num
+    begin
+      return (Kind => Tcp_Util.Port_Num_Spec,
+              Num  => Tcp_Util.Port_Num(To_Natural (Port)));
+    exception
+      when others =>
+        null;
+    end;
+
+    -- Try to parse Port_Name
+    Result.Name (1 .. Port'Length) := Port;
+    return Result;
+
+  exception
+    when others =>
+      raise Parse_Error;
+  end Parse;
+
+end Ip_Addr;
+
---
+++ reposit/ip_addr.ads
@@ -0,0 +1,22 @@
+with Tcp_Util;
+package Ip_Addr is
+
+  -- If Addr is "xxx.yyy.zzz.ttt" where each is between 0 and 255 then
+  --   return the Tcp_Util.Remote_Host (Tcp_Util.Host_Id_Spec)
+  -- Else
+  --   return the Tcp_Util.Remote_Host (Tcp_Util.Host_Name_Spec)
+  -- End if
+  function Parse (Addr : String) return Tcp_Util.Remote_Host;
+
+
+  -- If Port is a num between 0 and 65535 then
+  --   return the Tcp_Util.Remote_Port (Tcp_Util.Port_Num_Spec)
+  -- Else
+  --   return the Tcp_Util.Remote_Port (Tcp_Util.Port_Name_Spec)
+  -- End if
+  function Parse (Port : String) return Tcp_Util.Remote_Port;
+
+  Parse_Error : exception;
+
+end Ip_Addr;
+
