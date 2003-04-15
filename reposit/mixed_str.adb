9d679cae8f704b6fa473b81786ac4d1420d07441
Pascal MALAISE <malaise@magic.fr>
2003-04-15 17:39:23 +0000

    *** empty log message ***

---
+++ reposit/mixed_str.adb
@@ -0,0 +1,19 @@
+with Upper_Char, Lower_Char;
+function Mixed_Str (Str : String) return String is
+  Str_Loc : String (Str'Range) := Str;
+  Prev_Separator : Boolean := True;
+begin
+
+  for I in Str_Loc'Range loop
+    if Prev_Separator and then Str_Loc(I) in 'a' .. 'z' then
+      Str_Loc(I) := Upper_Char (Str_Loc(I));
+    elsif not Prev_Separator and then Str_Loc(I) in  'A' .. 'Z' then
+      Str_Loc(I) := Lower_Char (Str_Loc(I));
+    end if;
+    Prev_Separator := Str_Loc(I) = '_' or else Str_Loc(I) = '.';
+  end loop;
+
+  return Str_Loc;
+
+end Mixed_Str;
+
