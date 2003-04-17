08527ba7c9bc80c13e34771c4c176a9953751548
Pascal MALAISE <malaise@magic.fr>
2003-04-17 17:22:46 +0000

    Get_Line -> Argument

--- usr/tests/t_num_match.adb
+++ usr/tests/t_num_match.adb
@@ -1,26 +1,44 @@
 with Ada.Text_Io;
-with Num_Match, My_Io;
+with Argument, Num_Match;
 
 procedure T_Num_Match is
+
+  procedure Usage is
+  begin
+    Ada.Text_Io.Put_Line ("Usage: " & Argument.Get_Program_Name
+       & " <Natural> [ <Criteria_String> ]");
+  end Usage;
 
   Str : String(1 .. 256);
   Len : Natural;
   Num : Natural;
+  Res : Boolean;
 
 begin
 
-  Ada.Text_Io.Put ("Num: ");
-  My_Io.Get (Num);
-   Ada.Text_Io.skip_Line;
-
-  Ada.Text_Io.Put ("Criteria: ");
-  Ada.Text_Io.Get_Line (Str, Len);
-
-  if Num_Match (Num, Str(1 .. Len)) then
-    Ada.Text_Io.Put (Num'Img & " matches");
+  if Argument.Get_Nbre_Arg = 1 then
+    Num := Natural'Value (Argument.Get_Parameter);
+    Len := 0;
+  elsif Argument.Get_Nbre_Arg = 2 then
+    Num := Natural'Value (Argument.Get_Parameter (Occurence => 1));
+    Argument.Get_Parameter (Str, Len, 2);
   else
-    Ada.Text_Io.Put (Num'Img & " does not match");
+    Usage;
+    return;
   end if;
 
+  Res := Num_Match (Num, Str(1 .. Len));
+
+  Ada.Text_Io.Put (Num'Img);
+  if Num_Match (Num, Str(1 .. Len)) then
+    Ada.Text_Io.Put (" matches");
+  else
+    Ada.Text_Io.Put (" does not match");
+  end if;
+  Ada.Text_Io.Put_Line (" >" & Str(1 .. Len) & "<");
+
+exception
+  when Argument.Argument_Not_Found | Argument.Argument_Too_Long =>
+    Usage;
 end T_Num_Match;
 
