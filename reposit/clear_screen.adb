81adfd01fa3c9588313735e33798cc3c405302ca
Pascal MALAISE <malaise@magic.fr>
2003-05-23 16:34:22 +0000

    Sys_Calls.Foo_Stdin -> Sys_Calls.Foo(Fd).

--- reposit/clear_screen.adb
+++ reposit/clear_screen.adb
@@ -3,7 +3,7 @@ procedure Clear_Screen is
   Command : constant String := "clear";
   Res : Boolean;
 begin
-  Res := Sys_Calls.Set_Stdin_Attr (Sys_Calls.Canonical);
+  Res := Sys_Calls.Set_Tty_Attr (Sys_Calls.Stdin, Sys_Calls.Canonical);
   Res := Sys_Calls.Call_System (Command) = 0;
 end Clear_Screen;
 
