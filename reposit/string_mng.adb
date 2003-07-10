a4e67b25fd5da67b51e4cb6f5cd5cd0ebc5b1529
Pascal MALAISE <malaise@magic.fr>
2003-07-10 18:21:30 +0000

    Add Locate.

--- reposit/string_mng.adb
+++ reposit/string_mng.adb
@@ -1,6 +1,6 @@
 package body String_Mng is
 
-  -- Parces spaces and tabs (ASCII.HT) from the head/tail of a string
+  -- Parces spaces and tabs (Ascii.Ht) from the head/tail of a string
   -- Returns the position of the first/last character or 0 if
   --  all the string is spaces or tabs (or empty)
   function Parse_Spaces (Str : String; From_Head : Boolean := True)
@@ -28,20 +28,20 @@ package body String_Mng is
   end Parse_Spaces;
 
 
-  -- Puts a string STR in a string of fixed length LEN.
-  -- If STR is shorter than LEN, it is aligned at right or left and padded
-  -- If STR is longer  than LEN, it's head ot tail is truncated
+  -- Puts a string Str in a string of fixed length Len.
+  -- If Str is shorter than Len, it is aligned at right or left and padded
+  -- If Str is longer  than Len, it's head ot tail is truncated
 
-  -- STR : STRING to put in the returned string
-  -- LEN : Number of characters of the returned string
-  -- ALIGN_LEFT : If string is shorter than LEN characters,
-  --     align it at left or at right (not LEFT) and fill with GAP,
-  -- GAP : When string is shorter than len, fill empty positions with GAP
-  -- TRUNC_HEAD : If string is longer than LEN characters, trunc it's head
+  -- Str : StrING to put in the returned string
+  -- Len : Number of characters of the returned string
+  -- Align_Left : If string is shorter than Len characters,
+  --     align it at left or at right (not Align_Left) and fill with Gap,
+  -- Gap : When string is shorter than len, fill empty positions with Gap
+  -- Trunc_Head : If string is longer than Len characters, trunc it's head
   --     or its tail
-  -- SHOW_TRUNC : When string is longer than LEN, if SHOW_TRUNC is set,
-  --         then STR is truncated to LEN-2 and starts (TRUNC_HEAD) with " >"
-  --         or ends (not TRUNC_HEAD) with " <"
+  -- Show_Trunc : When string is longer than Len, if Show_Trunc is set,
+  --         then Str is truncated to Len-2 and starts (Trunc_Head) with " >"
+  --         or ends (not Trunc_Head) with " <"
   function Procuste (Str : String; Len : Positive;
            Align_Left : Boolean := True; Gap : Character := ' ';
            Trunc_Head : Boolean := True; Show_Trunc : Boolean := True)
@@ -50,7 +50,7 @@ package body String_Mng is
     S : String (1 .. Len);
   begin
     if L < Len then
-      -- STR is shorter than LEN: Pad
+      -- Str is shorter than Len: Pad
       if Align_Left then
         -- Copy L characters at left and pad
         S(1 .. L) := Str;
@@ -61,30 +61,52 @@ package body String_Mng is
         S(1 .. Len-L) := (others => Gap);
       end if;
     elsif L > Len then
-      -- STR is larger than LEN: Trunc
+      -- Str is larger than Len: Trunc
       if Trunc_Head then
         if Show_Trunc and then Len >= 2 then
-          -- Copy "> " then LEN-2 last characters of STR
+          -- Copy "> " then Len-2 last characters of Str
           S := "> " & Str(Str'Last-Len+1+2 .. Str'Last);
         else
-          -- Copy LEN last characters of STR
+          -- Copy Len last characters of Str
           S := Str(Str'Last-Len+1 .. Str'Last);
         end if;
       else
         if Show_Trunc and then Len >= 2 then
-          -- Copy LEN-2 first characters of STR then " <"
+          -- Copy Len-2 first characters of Str then " <"
           S := Str(Str'First .. Str'First+Len-1-2) & " <";
         else
-          -- Copy LEN first characters of STR
+          -- Copy Len first characters of Str
           S := Str(Str'First .. Str'First+Len-1);
         end if;
       end if;
     else
-      -- STR is as LEN characters: copy
+      -- Str is as Len characters: copy
       S := Str;
     end if;
     return S;
   end Procuste;
+
+  -- Locate Nth occurence of a fragment within a string
+  -- Returns index of fragment start
+  --  or 0 if not found or if Within or Fragment is empty
+  function Locate (Within : String;
+                   Fragment : String;
+                   Occurence : Positive := 1) return Natural is
+    Found_Occurence : Natural := 0;
+  begin
+    if Within'Length = 0 or else Fragment'Length = 0 then
+      return 0;
+    end if;
+    for I in 1 .. Within'Length - Fragment'Length + 1 loop
+      if Within(I .. I+Fragment'Length-1) = Fragment then
+        Found_Occurence := Found_Occurence + 1;
+        if Found_Occurence = Occurence then
+          return I;
+        end if;
+      end if;
+    end loop;
+    return 0;
+  end Locate;
 
 end String_Mng;
 
--- reposit/string_mng.ads
+++ reposit/string_mng.ads
@@ -1,29 +1,36 @@
 package String_Mng is
 
-  -- Parces spaces and tabs (ASCII.HT) from the head/tail of a string
+  -- Parces spaces and tabs (Ascii.Ht) from the head/tail of a string
   -- Returns the position of the first/last character or 0 if
   --  all the string is spaces or tabs (or empty)
   function Parse_Spaces (Str : String; From_Head : Boolean := True)
                         return Natural;
 
-  -- Puts a string STR in a string of fixed length LEN.
-  -- If STR is shorter than LEN, it is aligned at right or left and padded
-  -- If STR is longer  than LEN, it's head ot tail is truncated
+  -- Puts a string str in a string of fixed length Len.
+  -- If Str is shorter than Len, it is aligned at right or left and padded
+  -- If Str is longer  than Len, it's head ot tail is truncated
 
-  -- STR : STRING to put in the returned string
-  -- LEN : Number of characters of the returned string
-  -- ALIGN_LEFT : If string is shorter than LEN characters,
-  --     align it at left or at right (not LEFT) and fill with GAP,
-  -- GAP : When string is shorter than len, fill empty positions with GAP
-  -- TRUNC_HEAD : If string is longer than LEN characters, trunc it's head
+  -- Str : String to put in the returned string
+  -- Len : Number of characters of the returned string
+  -- Align_Left : If string is shorter than Len characters,
+  --     align it at left or at right (not Align_Left) and fill with Gap,
+  -- Gap : When string is shorter than Len, fill empty positions with Gap
+  -- Trunc_Head : If string is longer than Len characters, trunc it's head
   --     or its tail
-  -- SHOW_TRUNC : When string is longer than LEN, if SHOW_TRUNC is set,
-  --         then STR is truncated to LEN-2 and starts (TRUNC_HEAD) with " >"
-  --         or ends (not TRUNC_HEAD) with " <"
+  -- Show_Trunc : When string is longer than Len, if Show_Trunc is set,
+  --         then STR is truncated to Len-2 and starts (Trunc_Head) with " >"
+  --         or ends (not Trunc_Head) with " <"
   function Procuste (Str : String; Len : Positive;
            Align_Left : Boolean := True; Gap : Character := ' ';
            Trunc_Head : Boolean := True; Show_Trunc : Boolean := True)
            return String;
+
+  -- Locate Nth occurence of a fragment within a string
+  -- Returns index of fragment start
+  --  or 0 if not found or if Within or Fragment is empty
+  function Locate (Within : String;
+                   Fragment : String;
+                   Occurence : Positive := 1) return Natural;
 
 end String_Mng;
 
