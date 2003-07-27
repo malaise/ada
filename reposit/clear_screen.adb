caec8a75de6a9274db17fd313517be29d05d5da4
Pascal MALAISE <malaise@magic.fr>
2003-07-27 08:42:34 +0000

    Use console.

--- reposit/clear_screen.adb
+++ reposit/clear_screen.adb
@@ -1,9 +1,8 @@
-with Sys_Calls;
+with Sys_Calls, Console;
 procedure Clear_Screen is
-  Command : constant String := "clear";
   Res : Boolean;
 begin
   Res := Sys_Calls.Set_Tty_Attr (Sys_Calls.Stdin, Sys_Calls.Canonical);
-  Res := Sys_Calls.Call_System (Command) = 0;
+  Console.Clear;
 end Clear_Screen;
 
