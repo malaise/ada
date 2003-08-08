030b92fd104805e5d056338cee4c6624363e85ec
Pascal MALAISE <malaise@magic.fr>
2003-08-08 16:39:21 +0000

    Change Create and Delete into Set and Del. Add Is_Set, Copy and Image.

--- reposit/parser.adb
+++ reposit/parser.adb
@@ -1,15 +1,30 @@
---  with function Is_Separator (C : Character) return Boolean;
 with Ada.Unchecked_Deallocation;
 package body Parser is
 
   Empty : constant String := "";
 
+  -- Is iterator created and not deleted
+  function Is_Set (Iter : Iterator) return Boolean is
+  begin
+    return Iter.Acc /= null;
+  end Is_Set;
+
+  -- Clear iterator if needed
+  procedure Free is new Ada.Unchecked_Deallocation(Object => Iter_Rec,
+                                                   Name   => Iter_Rec_Access);
+  procedure Clear (Iter : in out Iterator) is
+  begin
+    if Is_Set (Iter) then
+      Free (Iter.Acc);
+    end if;
+  end Clear;
+
   -- Initialise an iterator with the string to parse and the criteria
   -- May raise Constraint_Error if the String is too
   --   long (see Text_Handler.Max_Len_Range)
-  procedure Create (Str : in String;
-                    Is_Sep : in Separing_Function;
-                    Iter : out Iterator) is
+  procedure Set (Iter : in out Iterator;
+                 Str  : in String;
+                 Is_Sep : in Separing_Function) is
   begin
     if Str'Length > Text_Handler.Max_Len_Range'Last then
       raise Constraint_Error;
@@ -25,41 +40,50 @@ package body Parser is
       when others =>
         raise Constraint_Error;
     end;
+    Clear (Iter);
     Iter.Acc := new Iter_Rec'(Str'Length, Str, Str'First, Is_Sep, Parsing,
                     Init_Rec.First, Init_Rec.Last, Init_Rec.Sep);
-  end Create;
+  end Set;
+
+
+  -- Copy an iterator
+  procedure Copy (Dest_Iter : in out Iterator; Src_Iter : in Iterator) is
+  begin
+    Clear (Dest_Iter);
+    if Is_Set (Src_Iter) then
+      Dest_Iter.Acc := new Iter_Rec'(Src_Iter.Acc.all);
+    end if;
+  end Copy;
 
   -- Check iterator validity
   procedure Check (Iter : Iterator) is
   begin
-    if Iter.Acc = null then
+    if not Is_Set (Iter) then
       raise Constraint_Error;
     end if;
   end Check;
   
   -- Destroy the iterator
   -- May raise Constraint_Error if iterator has not been created
-  procedure Free is new Ada.Unchecked_Deallocation(Object => Iter_Rec,
-                                                   Name   => Iter_Rec_Access);
 
-  procedure Delete (Iter : in out Iterator) is
+  procedure Del (Iter : in out Iterator) is
   begin
     Check (Iter);
-    Free (Iter.Acc);
-  end Delete;
+    Clear (Iter);
+  end Del;
 
   -- Automatic garbage collection
   procedure Finalize (Iter : in out Iterator) is
   begin
-    if Iter.Acc /= null then
-      Free (Iter.Acc);
+    if Is_Set (Iter) then
+      Del (Iter);
     end if;
   end Finalize;
 
   -- Reset the iterator, setting it in the same state than at creation
   -- The separing function may be changed
   -- May raise Constraint_Error if iterator has not been created or is deleted
-  procedure Reset (Iter : in out Iterator;
+  procedure Reset (Iter : in Iterator;
                    Is_Sep : in Separing_Function := null) is
   begin
     Check (Iter);
@@ -164,29 +188,63 @@ package body Parser is
     return Iter.Acc.Str(Iter.Acc.First .. Iter.Acc.Last);
   end Current_Word;
 
+  -- Reset and parse first word
+  function First_Word (Iter : Iterator) return String is
+  begin
+    Reset (Iter);
+    return Next_Word (Iter);
+  end First_Word;
 
   -- Return the indexes of current word
   -- 1 .. 0 if parsing not started or finished
   -- May raise Constraint_Error if iterator has not been created
-  function First_Index (Iter : Iterator) return Positive is
+  function First_Index (Iter : Iterator; Normalize : Boolean := True)
+                       return Positive is
   begin
     Check (Iter);
     if Iter.Acc.First > Iter.Acc.Last then
       return 1;
+    elsif Normalize then
+      return Iter.Acc.First;
     else
       return Iter.Acc.First - 1 + Iter.Acc.Start;
     end if;
   end First_Index;
 
-  function Last_Index  (Iter : Iterator) return Natural is
+  function Last_Index  (Iter : Iterator; Normalize : Boolean := True)
+                       return Natural is
   begin
     Check (Iter);
     if Iter.Acc.First > Iter.Acc.Last then
       return 0;
+    elsif Normalize then
+      return Iter.Acc.Last;
     else
       return Iter.Acc.Last - 1 + Iter.Acc.Start;
     end if;
   end Last_Index;
+
+  -- Return the string Str with which the iterator was created
+  -- The Str provided to Create may not have been from 1 to N
+  -- If not Normalized, the string returned has the same range as Str
+  --  otherwise they are from 1 to N
+  -- May raise Constraint_Error if iterator does not exist
+  function Image (Iter : Iterator; Normalize : Boolean := True)
+                 return  String is
+  begin
+    Check (Iter);
+    if Normalize then
+      return  Iter.Acc.Str(1 .. Iter.Acc.Len);
+    else
+      -- Return a string from Start to N
+      declare
+        Str : constant String (Iter.Acc.Start .. Iter.Acc.Start+Iter.Acc.Len-1)
+            := Iter.Acc.Str(1 .. Iter.Acc.Len);
+      begin
+        return Str;
+      end;
+    end if;
+  end Image;
 
 end Parser;
 
--- reposit/parser.ads
+++ reposit/parser.ads
@@ -2,57 +2,84 @@ with Ada.Finalization;
 with Text_Handler;
 package Parser is
 
+  -- The function to define separating characters
   type Separing_Function is
        access function (C : Character) return Boolean;
 
+  -- Access key
   type Iterator is limited private;
 
-  -- Initialise an iterator with the string to parse and the criteria
+  -- Initialise the iterator Iter with the string to parse and the criteria.
+  -- Previous content of Iter is overwritten.
   -- May raise Constraint_Error if Is_Sep is null or invalid
-  --  or if the String is too long (see Text_Handler.Max_Len_Range)
-  procedure Create (Str : in String;
-                    Is_Sep : in Separing_Function;
-                    Iter : out Iterator);
+  --  or if Str is too long (see Text_Handler.Max_Len_Range).
+  procedure Set (Iter : in out Iterator;
+                 Str : in String;
+                 Is_Sep : in Separing_Function);
 
-  -- Destroy the iterator
-  -- May raise Constraint_Error if iterator has not been created or is deleted
-  procedure Delete (Iter : in out Iterator);
+  -- Is the iterator Iter set?
+  function Is_Set (Iter : Iterator) return Boolean;
 
-  -- Reset the iterator, setting it in the same state than at creation
-  -- The separing function may be changed
-  -- May raise Constraint_Error if iterator has not been created or is deleted
-  procedure Reset (Iter : in out Iterator;
+  -- Delete the content of of the iterator Iter (which becomes not set).
+  -- May raise Constraint_Error if Iter is not set.
+  procedure Del (Iter : in out Iterator);
+
+  -- Copy the iterator Src_Iter to Dest_Iter.
+  -- Previous content of Dest_Iter is overwritten.
+  -- Src_Iter may not be set, which deletes Dest_Iter.
+  procedure Copy (Dest_Iter : in out Iterator; Src_Iter : in Iterator);
+
+  -- Reset iterator Iter, setting it in the same state than at creation.
+  -- The separing function may be changed at this occasion.
+  -- May raise Constraint_Error if Iter is not set.
+  procedure Reset (Iter : in Iterator;
                    Is_Sep : in Separing_Function := null);
 
-  -- Parse first then next word of the string
-  -- Parsing ends by returning empty string
-  -- May raise Constraint_Error if iterator has not been created
+  -- Parse first then next word of the string.
+  -- End of parsing is signaled by returning an empty string.
+  -- May raise Constraint_Error if Iter is not set.
   function Next_Word (Iter : Iterator) return String;
   procedure Next_Word (Iter : in Iterator);
 
   -- Return the separating characters previously found when looking
-  --   for Next_Word
-  -- Just after Creation, returns empty string
-  -- After Next_Word returns a string
-  --  - empty string if the word was the first and at string_start
-  --  - the separators from previous_word or string_start
-  -- After Next_Word returns empty string
-  --  - empty string if the string is empty
-  --  - the separators from previous_word or string
-  -- May raise Constraint_Error if iterator has not been created
+  --   for Next_Word.
+  -- Just after Creation, returns an empty string.
+  -- After Next_Word has returned a string:
+  --  - an empty string if the word was the first and at string_start,
+  --  - the separators from previous_word or string_start.
+  -- After Next_Word has returned empty string:
+  --  - an empty string if the string is empty,
+  --  - the separators from previous_word or string:
+  -- May raise Constraint_Error if Iter is not set.
   function Prev_Separators (Iter : Iterator) return String;
 
-  -- Return current word
-  -- Empty string if parsing is not started or finished
-  -- May raise Constraint_Error if iterator has not been created
+  -- Return current word.
+  --  or an empty string if parsing is not started or finished.
+  -- May raise Constraint_Error if Iter is not set.
   function Current_Word (Iter : Iterator) return String;
 
-  -- Return the indexes of current word
-  -- CAUTION: these are indexes in the original Str provided to Create
-  -- Returns 1 .. 0 if parsing is not started or finished
-  -- May raise Constraint_Error if iterator has not been created
-  function First_Index (Iter : Iterator) return Positive;
-  function Last_Index  (Iter : Iterator) return Natural;
+  -- Reset Iter and parse first word.
+  -- May raise Constraint_Error if Iter is not set.
+  function First_Word (Iter : Iterator) return String;
+
+  -- Return the indexes of current word.
+  -- The Str provided to Set may not have been from 1 to N.
+  -- If not Normalized, the indexes are in this Str,
+  --  otherwise they are from 1 to N.
+  -- Returns 1 .. 0 if parsing is not started or finished.
+  -- May raise Constraint_Error if Iter is not set.
+  function First_Index (Iter : Iterator; Normalize : Boolean := True)
+                       return Positive;
+  function Last_Index  (Iter : Iterator; Normalize : Boolean := True)
+                       return Natural;
+
+  -- Return the string Str to which the iterator was set.
+  -- The Str provided to Set may not have been from 1 to N.
+  -- If not Normalized, the string returned has the same indexes as Str,
+  --  otherwise it is from 1 to N.
+  -- May raise Constraint_Error if Iter is not set.
+  function Image (Iter : Iterator; Normalize : Boolean := True)
+                 return  String;
 
 private
 
@@ -85,7 +112,5 @@ private
   --  Free Acc
   procedure Finalize (Iter : in out Iterator);
 
-
 end Parser;
-
 
