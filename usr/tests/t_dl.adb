96eabc46ab1080a6ff7019eb7bad78eaec5e82af
Pascal MALAISE <malaise@magic.fr>
2003-08-12 18:02:39 +0000

    Test Iterate.

--- usr/tests/t_dl.adb
+++ usr/tests/t_dl.adb
@@ -3,7 +3,7 @@ with Dynamic_List, Normal, Rnd;
 procedure T_Dl is
   package My_List is new Dynamic_List(Element_Type => Integer);
   procedure My_Search is new My_List.Search;   -- ("=" of Integer)
-  procedure My_Safe_Search is new My_List.Safe_Search;   -- ("=" of Integer)
+  procedure My_Unsafe_Search is new My_List.Unsafe_Search;
   procedure My_Sort is new My_List.Sort("<");  -- ("<" of Integer)
 
   List : My_List.List_Type;
@@ -16,6 +16,29 @@ procedure T_Dl is
     Text_Io.Put (Normal (I, 2, Gap => '0') & ' ');
     if New_Line then Text_Io.New_Line; end if;
   end Put;
+
+  procedure Iteration (Current : in out Integer;
+                       Go_On   : in out Boolean) is
+  begin
+    Put (Current);
+    if Current rem 2 = 0 then
+      Text_Io.Put (" is odd");
+    else
+      Text_Io.Put (" is even");
+    end if;
+    if Current > 10 then
+      Text_Io.Put_Line (" and stopping iteration.");
+      begin
+        My_List.Read (List, Item);
+      exception
+        when My_List.In_Callback =>
+          Text_Io.Put_Line ("IN CALLBACK");
+      end;
+      Go_On := False;
+    else
+      Text_Io.New_Line;
+    end if;
+  end Iteration;
 
   procedure Dump is
     Pos : Natural;
@@ -97,7 +120,7 @@ begin
   -- permute 1st and 4th elements, then search 3 from last
   Text_Io.Put_Line("Permute 1st and 4th elements, then search 3 from last");
   My_List.Permute (List, 0, 3, My_List.Next, False);
-  My_Search (List, 3, My_List.Prev, 1, My_List.Absolute);
+  My_Unsafe_Search (List, 3, My_List.Prev, 1, My_List.Absolute);
 
   -- get pos from first and current item
   Text_Io.Put("Get current pos from first: ");
@@ -114,7 +137,7 @@ begin
 
   -- search 50 from first
   Text_Io.Put_Line("Seach 50 from first");
-  My_Safe_Search (List, Found, 50, From => My_List.Absolute);
+  My_Search (List, Found, 50, From => My_List.Absolute);
   if not Found then
     Text_Io.Put_Line ("NOT FOUND");
     -- This is not normal. Abort.
@@ -122,7 +145,7 @@ begin
   end if;
   -- search 50 from current, skipping it
   Text_Io.Put_Line("Seach 50, skipping current");
-  My_Safe_Search (List, Found, 50, From => My_List.Skip_Current);
+  My_Search (List, Found, 50, From => My_List.Skip_Current);
   if not Found then
     Text_Io.Put_Line ("NOT FOUND");
   end if;
@@ -157,6 +180,10 @@ begin
   Text_Io.Put("Current item, stay: ");
   My_List.Read(List, Item, My_List.Current);
   Put(Item, True);
+
+  Text_Io.Put_Line ("Iteration");
+  My_List.Iterate (List, null, 1, My_List.Next, My_List.Absolute, 
+                   Iteration'Unrestricted_Access);
 
   Text_Io.Put_Line("Delete fully the list");
   My_List.Delete_List (List);
