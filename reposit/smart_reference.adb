300fbb60f9685f434dd52e64d61010ec0a2e040a
Pascal MALAISE <malaise@magic.fr>
2005-01-21 18:22:07 +0000

    Smart pointer (automatic garbage collector).

---
+++ reposit/smart_reference.adb
@@ -0,0 +1,75 @@
+with Ada.Text_Io;
+with Unchecked_Deallocation;
+package body Smart_Reference is
+
+  Debug : Boolean := False;
+  procedure Trace (Str : in String) is
+  begin
+    if Debug then
+      Ada.Text_Io.Put_Line(Str);
+    end if;
+  end Trace;
+
+  procedure Free is new Unchecked_Deallocation (Object_Box,
+                                                Object_Box_Access);
+
+  -- Decrement reference to object and free object if no more reference
+  procedure Decrement_Ref (Ref : in out Handle) is
+  begin
+    if Ref.Box_Access /= null then
+      Ref.Box_Access.Nb_Access := Ref.Box_Access.Nb_Access - 1;
+      if Ref.Box_Access.Nb_Access = 0 then
+        Trace("Free");
+        Free(Ref.Box_Access);
+      end if;
+    end if;
+  end Decrement_Ref;
+
+  -- Increment reference to object
+  procedure Increment_Ref (Ref : in Handle) is
+  begin
+    Ref.Box_Access.Nb_Access := Ref.Box_Access.Nb_Access + 1;
+  end Increment_Ref;
+
+  -- Init Nb_Access to 1
+  procedure Initialize (Ref : in out Handle) is
+  begin
+    Trace("Initialization");
+  end Initialize;
+
+  -- Decrement Nb_Access and garbage collect when last
+  procedure Finalize (Ref : in out Handle) is
+  begin
+    Trace("Finalization");
+    Decrement_Ref(Ref);
+  end Finalize;
+
+  procedure Set (Reference : in out Handle; Init : in Object) is
+  begin
+    Decrement_Ref(Reference);
+    Reference.Box_Access := new Object_Box;
+    Set (Reference.Box_Access.Obj, Init);
+    Increment_Ref(Reference);
+  end Set;
+
+  -- Copy handle
+  procedure Set (Dest : in out Handle; Val : in Handle) is
+  begin
+    Decrement_Ref(Dest);
+    Dest.Box_Access := Val.Box_Access;
+    Increment_Ref(Dest);
+  end Set;
+
+  procedure Dereference (Reference : in out Handle; Val : in out Object) is
+  begin
+    Trace("Dereference");
+    Set (Val, Reference.Box_Access.Obj);
+    Trace("Dereferenced");
+  end Dereference;
+  function  Dereference (Reference : Handle) return Object is
+  begin
+    return Reference.Box_Access.Obj;
+  end Dereference;
+
+end Smart_Reference;
+
---
+++ reposit/smart_reference.ads
@@ -0,0 +1,35 @@
+with Ada.Finalization;
+generic
+  type Object is limited private;
+  with procedure Set (Dest : in out Object; Val : in Object);
+package Smart_Reference is
+
+  type Handle is limited private;
+
+  -- Initialise a Handle to an object 
+  procedure Set (Reference : in out Handle; Init : in Object);
+  -- Copy handle
+  procedure Set (Dest : in out Handle; Val : in Handle);
+
+  -- Get handled object
+  function  Dereference (Reference : Handle) return Object;
+  procedure Dereference (Reference : in out Handle; Val : in out Object);
+
+private
+  type Object_Access is access Object;
+
+  type Object_Box is record
+    Obj : Object;
+    Nb_Access : Natural := 0;
+  end record;
+  type Object_Box_Access is access Object_Box;
+
+  type Handle is new Ada.Finalization.Controlled with record
+    Box_Access : Object_Box_Access;
+  end record;
+  procedure Initialize (Ref : in out Handle);
+  procedure Finalize (Ref : in out Handle);
+
+
+end Smart_Reference;
+
