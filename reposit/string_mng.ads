70ca9cc10243c71aed285eae9ef248669e2bdfb9
Pascal MALAISE <malaise@magic.fr>
2005-11-18 18:08:56 +0000

    In Eval_Variables, replace getenv by Resolv callback.

--- reposit/string_mng.ads
+++ reposit/string_mng.ads
@@ -5,7 +5,7 @@ package String_Mng is
   -- Returns the position of the first/last character or 0 if
   --  all the string is spaces or tabs (or empty)
   function Parse_Spaces (Str : String; From_Head : Boolean := True)
-                        return Natural;
+           return Natural;
 
   -- Puts a string str in a string of fixed length Len.
   -- If Str is shorter than Len, it is aligned at right or left and padded
@@ -20,9 +20,12 @@ package String_Mng is
   -- Show_Trunc : When string is longer than Len, if Show_Trunc is set,
   --         then Str is truncated to Len-2 and starts (Trunc_Head) with "> "
   --         or ends (not Trunc_Head) with " <"
-  function Procuste (Str : String; Len : Positive;
-           Align_Left : Boolean := True; Gap : Character := ' ';
-           Trunc_Head : Boolean := True; Show_Trunc : Boolean := True)
+  function Procuste (Str : String;
+                     Len : Positive;
+                     Align_Left : Boolean := True;
+                     Gap : Character := ' ';
+                     Trunc_Head : Boolean := True;
+                     Show_Trunc : Boolean := True)
            return String;
 
   -- Locate Nth occurence of a fragment within a string
@@ -30,7 +33,8 @@ package String_Mng is
   --  or 0 if not found or if Within or Fragment is empty
   function Locate (Within : String;
                    Fragment : String;
-                   Occurence : Positive := 1) return Natural;
+                   Occurence : Positive := 1)
+           return Natural;
 
   -- Remove Nb_Char characters from the string From at index At_Index.
   -- If Gap is No_Gap, then the string is shorten by Nb_Char, either
@@ -42,9 +46,12 @@ package String_Mng is
   -- Return the remaining string.
   -- Raises Constraint_Error if At_Index is not within From'First .. From'Last.
   No_Gap : constant Character :=  Ada.Characters.Latin_1.Nul;
-  function Remove (From : String; At_Index : Positive; Nb_Char : Natural;
+  function Remove (From : String;
+                   At_Index : Positive;
+                   Nb_Char : Natural;
                    Shift_Left : Boolean := True;
-                   Gap : Character := No_Gap) return String;
+                   Gap : Character := No_Gap)
+           return String;
 
   -- If To_Right is True, extract Nb_Char characters of From from At_Index
   -- If To_Right is False, extract Nb_Char characters of From up to At_Index
@@ -52,21 +59,28 @@ package String_Mng is
   -- Raises Constraint_Error
   --  if At_Index+Nb_Char-1 is more than From'Last  (when To_Right is True)
   --  or At_Index-Nb_Char+1 is less than From'First (when To_Right is False).
-  function Slice (From : String; At_Index : Positive; Nb_Char : Natural;
-                  To_Right : Boolean := True) return String;
+  function Slice (From : String;
+                  At_Index : Positive;
+                  Nb_Char : Natural;
+                  To_Right : Boolean := True)
+           return String;
 
   -- Extract the Nb_Char first (if Head is set to True) or last characters
   --   (if Head is set to False) of From string.
   -- Return the remaining string.
-  function Cut (From : String; Nb_Char : Natural;
-                Head : Boolean := True) return String;
+  function Cut (From : String;
+                Nb_Char : Natural;
+                Head : Boolean := True)
+           return String;
 
   -- Extract the Nb_Char first (if Head is set to True) or last characters
   --    (if Head is set to False) of From string.
   -- Return the extracted substring.
   -- Raises Constraint_Error if Nb_Char is more than From'Length.
-  function Extract (From : String; Nb_Char : Natural;
-                    Head : Boolean := True) return String;
+  function Extract (From : String;
+                    Nb_Char : Natural;
+                    Head : Boolean := True)
+           return String;
 
   -- Swap the characters of string
   -- Example: ABCD -> DCBA
@@ -75,9 +89,15 @@ package String_Mng is
   -- Remove any multiple occurence of a character from string.
   -- Check from head or tail and return string.
   -- Example: ABCAD, Head -> ABCD  and  ABCAD, Tail -> BCAD
-  function Unique (From : String; From_Head : Boolean := True) return String;
+  function Unique (From : String;
+                   From_Head : Boolean := True)
+           return String;
 
-  -- Replace recursively all variables by their values in environ (getenv).
+  -- This function returns the value of a variable. It may raise exceptions
+  --  (that will be propagated by Eval_Variables).
+  type Resolv_Access is access function (Variable_Name : String) return String;
+  -- Replace recursively all variables by their values provided by the
+  --  Resolv callback.
   -- A variable name is identified because it is within delimiters (strings).
   -- Start and stop delimiters must be non empty and different (e.g. "(" and ")",
   --  or "${" and "}"), otherwise Inv_Delimiter is raised.
@@ -85,12 +105,13 @@ package String_Mng is
   -- Delimiter number must match (as many stop as start and in consistent
   --  sequence e.g. {}}{ s forbidden), otherwise the exception
   --  Delimiter_Mismatch is raised.
-  -- When a variable is not defined in environ, either it is expanded to
-  --  an empty string or the exception No_Variable is raised.
+  -- If no callback is set (Resolv = null) then variables are replaced by
+  --  empty strings.
   function Eval_Variables (Str : String;
                            Start_Delimiter, Stop_Delimiter : in String;
-                           Raise_No_Var : in Boolean := False) return String;
-  Inv_Delimiter, Delimiter_Mismatch, No_Variable : exception;
+                           Resolv : Resolv_Access)
+           return String;
+  Inv_Delimiter, Delimiter_Mismatch : exception;
 
 end String_Mng;
 
