c52b249165a3a8123d487110ea0ecc874001da75
Pascal MALAISE <malaise@magic.fr>
2005-12-01 21:07:35 +0000

    Text_Line supports In and Out modes.

--- usr/tests/t_text_line.adb
+++ usr/tests/t_text_line.adb
@@ -1,24 +1,25 @@
-with Ada.Text_Io;
 with Sys_Calls, Argument, Text_Line;
 procedure T_Text_Line is
 
   procedure Do_One (File_Name : in String) is
     Fd : Sys_Calls.File_Desc;
-    File : Text_Line.File_Type;
+    In_File, Out_File : Text_Line.File_Type;
   begin
     Fd := Sys_Calls.Open (File_Name, Sys_Calls.In_File);
-    Text_Line.Open (File, Fd);
+    Text_Line.Open (In_File, Text_Line.In_File, Fd);
+    Text_Line.Open (Out_File, Text_Line.Out_File, Sys_Calls.Stdout);
     loop
       declare
-        Str : constant String := Text_Line.Get (File);
+        Str : constant String := Text_Line.Get (In_File);
       begin
         exit when Str = "";
-        Ada.Text_Io.Put (Str);
+        Text_Line.Put (Out_File, Str);
       end;
     end loop;
-    Ada.Text_Io.Put_Line ("<<EOF>>");
-    Text_Line.Close (File);
+    Text_Line.Put_Line (Out_File, "<<EOF>>");
+    Text_Line.Close (In_File);
     Sys_Calls.Close (Fd);
+    Text_Line.Close (Out_File);
   exception
     when Sys_Calls.Name_Error =>
       Sys_Calls.Put_Line_Error ("Name error on " & File_Name & ".");
