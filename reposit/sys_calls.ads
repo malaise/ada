8c3df26fad0e56a97c10476550ebbe7407b090aa
Pascal MALAISE <malaise@magic.fr>
2006-01-05 18:11:02 +0000

    Add comment that read/write may raise System_Error en EOF...

--- reposit/sys_calls.ads
+++ reposit/sys_calls.ads
@@ -116,6 +116,7 @@ package Sys_Calls is
                            Status : out Get_Status_List;
                            C      : out Character);
   -- Read / write on File_Desc
+  -- May raise System_Error;
   function Read  (Fd : File_Desc; Buffer : System.Address; Nbytes : Positive)
            return Natural;
   function Write (Fd : File_Desc; Buffer : System.Address; Nbytes : Positive)
