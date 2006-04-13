f38145ce188069256e4d25dc9e2a7a55065b3f62
Pascal MALAISE <malaise@magic.fr>
2006-04-13 17:46:21 +0000

    Fix characters. Improve display.

--- usr/tests/t_utf8.adb
+++ usr/tests/t_utf8.adb
@@ -1,35 +1,58 @@
 with Ada.Text_Io;
-with Utf_8, Rnd;
+with My_Io, Utf_8, Rnd;
 procedure T_Utf8 is
+
+  procedure Put (N : Natural) is
+  begin
+    My_Io.Put (N, Base => 16);
+  end Put;
 
   Str : constant String := "aàâeéèêëiîïoôuùü";
   Ucodes : constant array (Positive range <>) of Utf_8.Unicode_Number
-         := (16#0061#, 16#C3A0#, 16#C3A2#, 
-             16#0065#, 16#C3A9#, 16#C3A8#, 16#C3AA#, 16#C3AB#,
-             16#0069#, 16#C3AE#, 16#C3AF#,
-             16#006F#, 16#C3B4#,
-             16#0075#, 16#C3B9#, 16#C3BC#);
+         := (16#61#, 16#E0#, 16#E2#, 
+             16#65#, 16#E9#, 16#E8#, 16#EA#, 16#EB#,
+             16#69#, 16#EE#, 16#EF#,
+             16#6F#, 16#F4#,
+             16#75#, 16#F9#, 16#FC#);
 
   U1, U2 : Utf_8.Unicode_Number;
 begin
   Ada.Text_Io.Put_Line (Str);
   for I in Ucodes'Range loop
-    Ada.Text_Io.Put (Utf_8.Encode(Ucodes(I)));
+    declare
+      Ustr : constant String := Utf_8.Encode(Ucodes(I));
+    begin
+      Ada.Text_Io.Put (Ustr);
+      for J in Ustr'Range loop
+        Put (Character'Pos (UStr(J)));
+      end loop;
+    end;
+    Ada.Text_Io.New_Line;
   end loop;
   Ada.Text_Io.New_Line;
 
+  Rnd.Randomize;
   loop
     U1 := Rnd.Int_Random (Utf_8.Unicode_Number'First,
                           Utf_8.Unicode_Number'Last);
+    Put (U1);
+    Ada.Text_Io.Put (" -> ");
     declare
       Str : constant Utf_8.Sequence := Utf_8.Encode (U1);
     begin
+      for I in Str'Range loop
+        Put (Character'Pos (Str(I)));
+      end loop;
+      Ada.Text_Io.Put (" -> ");
+
       U2 := Utf_8.Decode (Str);
+      Put (U2);
+      
       if U1 /= U2 then
-        Ada.Text_Io.Put_Line ("Bug:" & U1'Img & " /= " & U2'Img);
+        Ada.Text_Io.Put_Line (" Bug");
         exit;
       else
-        Ada.Text_Io.Put_Line ("OK:" & U1'Img);
+        Ada.Text_Io.Put_Line (" OK");
       end if;
     end;
   end loop;
