dff42c54e27b06f93183aac1f9166cd44a823a26
Pascal MALAISE <malaise@magic.fr>
2006-06-30 18:11:30 +0000

    Use Arbitrary.Factors.

--- usr/misc/prime.adb
+++ usr/misc/prime.adb
@@ -7,15 +7,14 @@
 --        prime -lcm <n1> <n2>     lowest common multiple
 
 with Ada.Text_Io;
-with Argument, Dynamic_List, Arbitrary;
+with Argument, Dynamic_List, Arbitrary, Arbitrary.Factors;
 with Prime_List;
 procedure Prime is
   use type Arbitrary.Number;
   use Prime_List;
 
   -- Lists of prime factors
-  package Pldm is new Dynamic_List(Prime_Positive);
-  package Plm renames Pldm.Dyn_List;
+  package Plm renames Arbitrary.Factors.Nb_List_Mng;
   procedure Search is new Plm.Search("=");
 
   -- What should we do
@@ -27,12 +26,6 @@ procedure Prime is
 
   -- Lists of prime factors
   L1, L2, Lr : Plm.List_Type;
-
-  -- Does a factor appear in both lists
-  Match : Boolean;
-
-  -- Boolean for call to delete
-  End_Of_List : Boolean;
 
   -- Help
   procedure Usage is
@@ -70,16 +63,8 @@ procedure Prime is
     Ada.Text_Io.New_Line;
   end Put_Line;
 
-  -- Rewind a list
-  procedure Rewind (L : in out Plm.List_Type) is
-  begin
-    if not Plm.Is_Empty (L) then
-      Plm.Rewind (L);
-    end if;
-  end Rewind;
-
-  Zero : constant Prime_Number := Prime_List.Zero;
-  One  : constant Prime_Positive := Prime_List.One;
+  Zero : constant Prime_Number := Arbitrary.Zero;
+  One  : constant Prime_Positive := Arbitrary.One;
 
   -- Set a positive number from string (for arg parsing)
   function  Prime_Positive_Value (Str : String) return Prime_Positive is
@@ -92,34 +77,6 @@ procedure Prime is
     return R;
   end Prime_Positive_Value;
 
-  -- Decompose N in prime numbers.
-  procedure Decompose (N : in Prime_Positive; L : in out Plm.List_Type) is
-    C, T : Prime_Positive;
-  begin
-    C := N;
-    -- Start after 1
-    if C = One then
-      Plm.Insert (L, One);
-    else
-      T := Prime_List.Next;
-      T := Prime_List.Next;
-      loop
-        if C rem T = Zero then
-          -- Insert this factor and try again with it
-          Plm.Insert (L, T);
-          C := C / T;
-          exit when C = One;
-        else
-          -- Try next factor
-          T := Prime_List.Next;
-        end if;
-      end loop;
-    end if;
-    -- Rewind lists
-    Rewind (L);
-    Prime_List.Rewind;
-  end Decompose;
-
   -- Put list from current
   procedure Put_List (L : in out Plm.List_Type) is
     T : Prime_Positive;
@@ -131,33 +88,6 @@ procedure Prime is
       Plm.Move_To (L);
     end loop;
   end Put_List;
-
-  -- Delete current
-  procedure Delete (L : in out Plm.List_Type; End_Of_List : out Boolean) is
-    Done : Boolean;
-  begin
-    Plm.Delete (L, Plm.Next, Done);
-    End_Of_List := not Done;
-  end Delete;
-
-  -- Mustiply numbers of list from current
-  function Multiply (L : in Plm.List_Type) return Prime_Number is
-    S, T : Prime_Number;
-    Lt : Plm.List_Type;
-  begin
-    S := One;
-    Plm.Assign (Lt, L);
-    loop
-      Plm.Read (Lt, T, Plm.Current);
-      S := S * T;
-      exit when not Plm.Check_Move (Lt);
-      Plm.Move_To (Lt);
-    end loop;
-    return S;
-  exception
-    when Plm.Empty_List =>
-      return S;
-  end Multiply;
 
 begin
 
@@ -253,51 +183,25 @@ begin
 
     when Factors =>
       -- Decompose N1 in prime factors
-      Decompose (N1, L1);
+      Arbitrary.Factors.Decompose (N1, L1);
       Put_List (L1);
     when Hcd | Lcm =>
       -- Decompose N1 and N2 in prime factors
-      Decompose (N1, L1);
-      Decompose (N2, L2);
+      Arbitrary.Factors.Decompose (N1, L1);
+      Arbitrary.Factors.Decompose (N2, L2);
 
       -- Add to Lr any prime factor common to L1 and L2
       --  and remove it from from L1 and L2
-      loop
-        -- Next factor of N1
-        begin
-          Plm.Read (L1, N1, Plm.Current);
-        exception
-          when Plm.Empty_List =>
-            exit;
-        end;
-        -- Find in factors of N2
-        Search (L2, Match, N1, From => Plm.Absolute);
-
-        if Match then
-          -- Found. Add it to Lr and remove it from L1 and L2
-          Plm.Insert (Lr, N1);
-          Delete (L2, End_Of_List);
-          Delete (L1, End_Of_List);
-          -- End of L1?
-          exit when End_Of_List;
-        else
-          -- Not found next of L1
-          exit when not Plm.Check_Move (L1);
-          Plm.Move_To (L1);
-        end if;
-      end loop;
-
-      -- Rewind
-      Rewind (L1);
-      Rewind (L2);
-      Rewind (Lr);
+      Arbitrary.Factors.Extract_Common (L1, L2, Lr);
 
       if Mode = Hcd then
         -- Put multiplication of Lr
-        Put_Line (Multiply (Lr));
+        Put_Line (Arbitrary.Factors.Multiply (Lr));
       else
         -- Put multiplication of Lr * L1 * L2
-        Put_Line (Multiply (Lr) * Multiply (L1) * Multiply (L2));
+        Put_Line (Arbitrary.Factors.Multiply (Lr)
+                * Arbitrary.Factors.Multiply (L1)
+                * Arbitrary.Factors.Multiply (L2));
       end if;
 
   end case;
