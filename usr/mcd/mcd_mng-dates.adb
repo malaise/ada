a3ac47b5c3bacfef0893fc33346228ef7d612867
Pascal MALAISE <malaise@magic.fr>
2006-10-03 16:51:12 +0000

    Change reference date to 1960, handle exceptions in Date_To_Time.

--- usr/mcd/mcd_mng-dates.adb
+++ usr/mcd/mcd_mng-dates.adb
@@ -14,7 +14,7 @@ package body Dates is
 
   -- Reference time
   Ref_Cal_Time : constant Ada.Calendar.Time
-               := Ada.Calendar.Time_Of (1970, 01, 01, 0.0);
+               := Ada.Calendar.Time_Of (1960, 01, 01, 0.0);
 
 
   function Clock return Item_Rec is
@@ -192,6 +192,9 @@ package body Dates is
 
 
     return Res_Time;
+  exception
+    when Constraint_Error | Ada.Calendar.Time_Error =>
+      raise Compute_Error;
   end Date_To_Time;
 
 end Dates;
