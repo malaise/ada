9b7e39438e2888d44ddf82ba1c09f2b6953ac433
Pascal MALAISE <malaise@magic.fr>
2006-11-10 18:26:02 +0000

    Test number of points vs Space and Lem widths.

--- usr/lem/moon.adb
+++ usr/lem/moon.adb
@@ -1,25 +1,38 @@
-with Rnd;
+with My_Math, Rnd;
+with Lem;
 package body Moon is
 
   -- type Ground_Array is array (Positive range <>) of Space.Position_Rec;
-  -- Number of points
-  Nb_Points : constant := 51;
+  use type Space.Position_Range;
+  -- Number of points on the ground
+  Nb_Points : constant := 25;
   subtype Point_Range is Positive range 1 .. Nb_Points;
+
   -- Distance on X between 2 points
   Delta_Point : constant Space.X_Range
-              := Space."/" (Space.X_Max,Space.Position_Range (Nb_Points - 1));
+              := Space.X_Max / Space.Position_Range (Nb_Points - 1);
 
   -- The ground array
   Ground : Ground_Array (Point_Range);
 
   -- Init a new moon
   procedure Init is
-    -- Index of the central point of the landing site (2 .. 50)
+    -- Index of the central point of the landing site (2 .. Last-2)
+    --  Last-2 because Last-1 to Last may not be wide enough for LEM
     subtype Landing_Range is Point_Range
-            range Point_Range'First + 1 .. Point_Range'Last - 1;
+            range Point_Range'First + 1 .. Point_Range'Last - 2;
     Index_Landing : Point_Range;
     use type Space.X_Range;
   begin
+    -- Raise anonymous (un-catchable) exception
+    --  if LEM cannot land between 3 points
+    declare
+      Delta_Point_Lem_Width_Error : exception;
+    begin
+      if Delta_Point * 2.0 < Lem.Width then
+        raise Delta_Point_Lem_Width_Error;
+      end if;
+    end;
     -- Set random height for all points
     for I in Point_Range loop
       Ground(I).X_Pos := Space.X_Range (I - 1) * Delta_Point;
