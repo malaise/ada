d83b99a72bc7c566c0de96394426a419a4f51957
Pascal MALAISE <malaise@magic.fr>
2006-11-15 18:16:37 +0000

    Clean interfaces. Handle inputs.

--- usr/lem/game.adb
+++ usr/lem/game.adb
@@ -1,52 +1,115 @@
 with Ada.Text_Io;
 with Event_Mng;
-with Screen, Flight, Moon, Lem;
+with Screen, Flight, Moon, Lem, Debug;
 package body Game is
 
   function Play_One return Boolean is
     Flight_Status : Flight.Status_Rec;
-    Prev_Pos, New_Pos : Screen.Lem_Position;
+    Get_Status : Screen.Got_List;
+    use type Lem.Thrust_Range;
+    X_Thrust_Increment : constant Lem.X_Thrust_Range
+                       := Lem.Max_X_Thrust / 2;
+    Y_Thrust_Increment : constant Lem.Y_Thrust_Range
+                       := Lem.Max_Y_Thrust / 10;
+    Y_Thrust : Lem.Y_Thrust_Range;
     use type Flight.Status_List;
   begin
     -- Init Moon ground
     Moon.Init;
     -- Init Lem at a valid position
-    Prev_Pos := Screen.No_Position;
-    New_Pos := (Set => True, Pos => Flight.Get_Init_Position);
-    Lem.Init (New_Pos.Pos);
+    Lem.Init (Flight.Get_Init_Position);
+
     -- Init screen
-    Screen.Init (Moon.Get_Ground);
-    Screen.Update (Prev_Pos, New_Pos,
-                   Lem.Get_Speed, Lem.Get_Y_Thrust, Lem.Get_Fuel);
-    Prev_Pos := New_Pos;
+    Screen.Init;
+    Screen.Update (Lem.Get_Position, Lem.Get_Speed);
+
+    -- Play
     loop
-      -- Get Lem characteristics and put
-      New_Pos.Pos := Lem.Get_Position;
-      Screen.Update (Prev_Pos, New_Pos, Lem.Get_Speed,
-                     Lem.Get_Y_Thrust, Lem.Get_Fuel);
-      Prev_Pos := New_Pos;
       -- Get flying status
       Flight_Status := Flight.Get_Status;
       exit when Flight_Status.Status /= Flight.Flying;
-      -- Wait a bit
-      exit when Event_Mng.Wait (100);
+      -- Get Lem characteristics and put
+      Screen.Update (Flight_Status.Pos, Flight_Status.Speed);
+      -- Get a key or wait a bit
+      Get_Status := Screen.Get_Key (0.1);
+      -- Handle key
+      case Get_Status is
+        when Screen.Right_Key =>
+          -- Push right
+          Lem.Set_X_Thrust (-X_Thrust_Increment);
+        when Screen.Left_Key =>
+          -- Push left
+          Lem.Set_X_Thrust (X_Thrust_Increment);
+        when Screen.Up_Key =>
+          -- Push less, down to 0
+          Y_Thrust := Lem.Get_Y_Thrust;
+          if Y_Thrust > Y_Thrust_Increment then
+            Y_Thrust := Y_Thrust - Y_Thrust_Increment;
+          else
+            Y_Thrust := 0;
+          end if;
+          Lem.Set_Y_Thrust (Y_Thrust);
+        when Screen.Down_Key =>
+          -- Push more, up to max
+          Y_Thrust := Lem.Get_Y_Thrust;
+          if Y_Thrust < Lem.Max_Y_Thrust - Y_Thrust_Increment then
+            Y_Thrust := Y_Thrust + Y_Thrust_Increment;
+          else
+            Y_Thrust := Lem.Max_Y_Thrust;
+          end if;
+          Lem.Set_Y_Thrust (Y_Thrust);
+        when Screen.Other_Key =>
+          -- Ignore any other key
+          null;
+        when Screen.Break =>
+          -- Abort
+          Screen.Close;
+          return False;
+        when Screen.Timeout =>
+          -- Re-loop
+          null;
+        when Screen.Refresh =>
+          -- Refresh screen
+          Screen.Refresh;
+      end case;
     end loop;
-    -- Stop all
+
+    -- Game is ended
+    -- Stop the LEM
     Lem.Stop;
-    if Flight_Status.Status = Flight.Lost then
-      -- Lem lost: hide it
-      New_Pos := Screen.No_Position;
-    else
-      -- Landed or crashed: show it
-      New_Pos := Prev_Pos;
-    end if;
-    Screen.Update (New_Pos, Prev_Pos,
-                   Speed => (0.0, 0.0),
-                   Y_Thrust => 0,
-                   Fuel => Lem.Get_Fuel);
-Ada.Text_Io.Put_Line (Flight_Status.Status'Img);
-delay 10.0;
-    return True;
+
+    -- Last display and get key
+    loop
+      if Flight_Status.Status = Flight.Lost then
+        -- Lem lost: hide it
+        Screen.Delete (Flight_Status.Speed);
+      else
+        -- Landed or crashed: show it
+        Screen.Update (Flight_Status.Pos, Flight_Status.Speed);
+      end if;
+      Screen.Put_End (Flight_Status.Status);
+
+      Get_Status := Screen.Get_Key (-1.0);
+      case Get_Status is
+        when Screen.Break =>
+          -- Abort
+          Screen.Close;
+          return False;
+        when Screen.Refresh =>
+          -- Refresh screen
+          Screen.Refresh;
+        when Screen.Timeout =>
+          -- Should not occure
+          null;
+        when Screen.Other_Key =>
+          -- Any other key: go on
+          return True;
+        when others =>
+          -- Arrows (remaining events): ignore
+          null;
+      end case;
+    end loop;
+
   end Play_One;
 
 end Game;
