a59b96509bbcea79e0f8f0560f78823b5068180f
Pascal MALAISE <malaise@magic.fr>
2006-11-15 18:11:38 +0000

    Clean interfaces. Add Get_Key.

--- usr/lem/screen.adb
+++ usr/lem/screen.adb
@@ -1,4 +1,5 @@
-with Con_Io, My_Math;
+with My_Math;
+with Space, Moon;
 package body Screen is
 
   -- First and Last X on screen for space
@@ -37,11 +38,39 @@ package body Screen is
             Y => Y_To_Screen (Position.Y_Pos) );
   end To_Screen;
 
-  -- Screen position gauge names (thrust, speeds and fuel)
-  Th, Vs, Fu, Hs : Coordinate_Rec;
+  -- Screen position of gauge names (thrust, speeds and fuel)
+  Thn, Vsn, Fun, Hsn : Coordinate_Rec;
+
+  -- Gauges definition
+  Gauge_Size : constant := 5;
+  -- Thrust
+  Thx : Con_Io.Graphics.X_Range;
+  Thymin, Thymax : Con_Io.Graphics.Y_Range;
+  Thfactor : My_Math.Real;
+  -- Vertical speed
+  use type Lem.Speed_Range;
+  Max_Vert_Speed : constant Lem.Speed_Range := 10.0 * Flight.Max_Verti_Speed;
+  Vsx : Con_Io.Graphics.X_Range;
+  Vsymin, Vsymax, Vsymid : Con_Io.Graphics.Y_Range;
+  Vsfactor : My_Math.Real;
+  -- Fuel
+  Fuxmin, Fuxmax : Con_Io.Graphics.X_Range;
+  Fuy : Con_Io.Graphics.Y_Range;
+  Fufactor : My_Math.Real;
+  -- Horizontal speed
+  Max_Hori_Speed : constant Lem.Speed_Range := 10.0 * Flight.Max_Horiz_Speed;
+  Hsxmin, Hsxmax, Hsxmid : Con_Io.Graphics.X_Range;
+  Hsy : Con_Io.Graphics.Y_Range;
+  Hsfactor : My_Math.Real;
+
+  -- Close (definitively)
+  procedure Close is
+  begin
+    Con_Io.Destroy;
+  end Close;
 
   -- Reset screen. Display titles and moon ground
-  procedure Init (Ground : in Moon.Ground_Array) is
+  procedure Init is
     use Con_Io.Graphics;
     use type My_Math.Real;
   begin
@@ -59,16 +88,40 @@ package body Screen is
     X_Factor := My_Math.Real (Last_X - First_X) / My_Math.Real (Space.X_Max);
     Y_Factor := My_Math.Real (Last_Y - First_Y) / My_Math.Real (Space.Y_Max);
     -- Compute position of gauge names
-    Th := (Last_X + 2, First_Y + 1);
-    Vs := (Th.X + Con_Io.Graphics.Font_width * 3, Th.Y);
-    Fu := (First_X + 1, First_Y - 2 - Con_Io.Graphics.Font_Height);
-    Hs := (Fu.X, Fu.Y - Con_Io.Graphics.Font_Height);
+    Thn := (Last_X + 2, First_Y + 1);
+    Vsn := (Thn.X + Con_Io.Graphics.Font_Width * 3 - 4, Thn.Y);
+    Fun := (First_X + 1, First_Y - Con_Io.Graphics.Font_Height);
+    Hsn := (Fun.X, Fun.Y - Con_Io.Graphics.Font_Height + 3);
+    -- Compute position and factor of gauges
+    -- Thrust
+    Thx := Thn.X + 6;
+    Thymin := Thn.Y +  Con_Io.Graphics.Font_Height;
+    Thymax := Last_Y;
+    Thfactor := My_Math.Real(Thymax - Thymin) / My_Math.Real(Lem.Max_Y_Thrust);
+    -- Vert speed
+    Vsx := Vsn.X + 6;
+    Vsymin := Thymin;
+    Vsymax := Thymax;
+    Vsymid := (Vsymin + Vsymax) / 2;
+    Vsfactor := My_Math.Real(Vsymax - Vsymid) / My_Math.Real(Max_Vert_Speed);
+    -- Fuel
+    Fuxmin := Fun.X + Con_Io.Graphics.Font_Width * 4;
+    Fuxmax := Last_X;
+    Fuy := Fun.Y + 1;
+    Fufactor := My_Math.Real(Fuxmax - Fuxmin) / My_Math.Real(Lem.Max_Fuel);
+    -- Horizontal speed
+    Hsxmin := Hsn.X + Con_Io.Graphics.Font_Width * 4;
+    Hsxmax := Last_X;
+    Hsxmid := (Hsxmin + Hsxmax) / 2;
+    Hsy := Hsn.Y + 1;
+    Hsfactor := My_Math.Real(Hsxmax - Hsxmid) / My_Math.Real(Max_Hori_Speed);
     -- Draw ground
-    Refresh (Ground);
+    Refresh;
   end Init;
 
   -- Put "constant" info
-  procedure Refresh (Ground : in Moon.Ground_Array) is
+  procedure Refresh is
+    Ground : constant Moon.Ground_Array := Moon.Get_Ground;
     J : Positive;
     Screen_Point : Coordinate_Rec;
     Screen_Ground : Con_Io.Natural_Array (1 .. (Ground'Length + 2) * 2);
@@ -95,86 +148,202 @@ package body Screen is
     J := J + 1;
     Screen_Ground(J) := First_Y;
     J := J + 1;
-    -- Put ground
     Con_Io.Set_Foreground (Con_Io.Light_Gray);
     Con_Io.Graphics.Fill_Area (Screen_Ground);
     -- Frame
-    Con_Io.Set_Foreground (Con_Io.Red);
+    Con_Io.Set_Foreground (Con_Io.Blue);
     Con_Io.Graphics.Draw_Rectangle (First_X - 1, First_Y - 1,
                                     Last_X + 1, Last_Y + 1);
-    -- Letters
-    Con_Io.Graphics.Put ("Th", Th.X, Th.Y);
-    Con_Io.Graphics.Put ("Vs", Vs.X, Vs.Y);
-    Con_Io.Graphics.Put ("Fu:", Fu.X, Fu.Y);
-    Con_Io.Graphics.Put ("Hs:", Hs.X, Hs.Y);
+    -- Gauge Letters
+    Con_Io.Graphics.Put ("Th", Thn.X, Thn.Y);
+    Con_Io.Graphics.Put ("Vs", Vsn.X, Vsn.Y);
+    Con_Io.Graphics.Put ("Fu:", Fun.X, Fun.Y);
+    Con_Io.Graphics.Put ("Hs:", Hsn.X, Hsn.Y);
+    -- Done
+    Con_Io.Flush;
   end Refresh;
 
-  -- Update lem and its speed and Y_thrust
-  procedure Update (Prev_Pos, New_Pos : in Lem_Position;
-                    Speed : in Lem.Speed_Rec;
-                    Y_Thrust : in Lem.Y_Thrust_Range;
-                    Fuel     : in Lem.Fuel_Range) is
-    procedure Draw_Lem (Pos : Space.Position_Rec) is
-      -- Lower left and upper right of LEM
-      -- Bottom and top of left and right feet
-      Ll, Ur, Blf, Tlf, Brf, Trf : Coordinate_Rec;
-      --   +--------+
-      --   |        |
-      --   +---+----+
-      --     /    \
-      --    /      \
-      use type My_Math.Real;
-      Height : constant Con_Io.Graphics.Y_Range
-             := Integer(My_Math.Trunc(My_Math.Real(Lem.Width) / 2.0 * Y_Factor));
-      use type Space.Position_Range;
-    begin
-      -- Compute Lower left and Upper right corners (Pos is LEM center)
-      Ll := To_Screen ( (Pos.X_Pos - Lem.Width / 2.0,
-                         Pos.Y_Pos) );
-      Ur := (X => X_To_Screen (Pos.X_Pos + Lem.Width / 2.0),
-             Y => Ll.Y + Height);
-      -- Compute left and right legs bottom and top
-      Blf := (X => Ll.X,
-              Y => Ll.Y - Height);
-      Brf := (X => Ur.X,
-              Y => Blf.Y);
-      Tlf := (X => X_To_Screen (Pos.X_Pos - Lem.Width / 4.0),
-              Y => Ll.Y);
-      Trf := (X => X_To_Screen (Pos.X_Pos + Lem.Width / 4.0),
-              Y => Ll.Y); 
-      -- Fill body
-      Con_Io.Graphics.Fill_Rectangle (Ll.X, Ll.Y, Ur.X, Ur.Y);
-      -- Draw legs
-      Con_Io.Graphics.Draw_Line (Tlf.X, Tlf.Y, Blf.X, Blf.Y);
-      Con_Io.Graphics.Draw_Line (Tlf.X + 1, Tlf.Y, Blf.X + 1, Blf.Y);
-      Con_Io.Graphics.Draw_Line (Trf.X, Trf.Y, Brf.X, Brf.Y);
-      Con_Io.Graphics.Draw_Line (Trf.X - 1, Trf.Y, Brf.X - 1, Brf.Y);
-    end Draw_Lem;
+  -- Lem position
+  type Lem_Position (Set : Boolean := False) is record
+    case Set is
+      when True => Pos : Space.Position_Rec;
+      when False => null;
+    end case;
+  end record;
+  No_Pos : constant Lem_Position := (Set => False);
+
+  -- Previous Lem position to rease on update/delete
+  Prev_Pos : Lem_Position := No_Pos;
+
+  -- Draw/hide the LEM
+  procedure Draw_Lem (Pos : Space.Position_Rec) is
+    -- Lower left and upper right of LEM
+    -- Bottom and top of left and right feet
+    Ll, Ur, Blf, Tlf, Brf, Trf : Coordinate_Rec;
+    --   +--------+
+    --   |        |
+    --   +---+----+
+    --     /    \
+    --    /      \
+    use type My_Math.Real;
+    Height : constant Con_Io.Graphics.Y_Range
+           := Integer(My_Math.Trunc(My_Math.Real(Lem.Width) / 2.0 * Y_Factor));
+    use type Space.Position_Range;
+  begin
+    -- Compute Lower left and Upper right corners (Pos is LEM center)
+    Ll := To_Screen ( (Pos.X_Pos - Lem.Width / 2.0,
+                       Pos.Y_Pos) );
+    Ur := (X => X_To_Screen (Pos.X_Pos + Lem.Width / 2.0),
+           Y => Ll.Y + Height);
+    -- Compute left and right legs bottom and top
+    Blf := (X => Ll.X,
+            Y => Ll.Y - Height);
+    Brf := (X => Ur.X,
+            Y => Blf.Y);
+    Tlf := (X => X_To_Screen (Pos.X_Pos - Lem.Width / 4.0),
+            Y => Ll.Y);
+    Trf := (X => X_To_Screen (Pos.X_Pos + Lem.Width / 4.0),
+            Y => Ll.Y);
+    -- Fill body
+    Con_Io.Graphics.Fill_Rectangle (Ll.X, Ll.Y, Ur.X, Ur.Y);
+    -- Draw legs
+    Con_Io.Graphics.Draw_Line (Tlf.X, Tlf.Y, Blf.X, Blf.Y);
+    Con_Io.Graphics.Draw_Line (Tlf.X + 1, Tlf.Y, Blf.X + 1, Blf.Y);
+    Con_Io.Graphics.Draw_Line (Trf.X, Trf.Y, Brf.X, Brf.Y);
+    Con_Io.Graphics.Draw_Line (Trf.X - 1, Trf.Y, Brf.X - 1, Brf.Y);
+  end Draw_Lem;
+
+  -- Update (hide then draw) the gauges
+  procedure Put_Gauges (New_Speed : in Lem.Speed_Rec) is
+    Y_Thrust : constant Lem.Y_Thrust_Range := Lem.Get_Y_Thrust;
+    Fuel     : constant Lem.Fuel_Range := Lem.Get_Fuel;
+    Thrust_Size : Natural;
+    Vspeed : Lem.Speed_Range;
+    Vspeed_Size : Integer;
+    Fuel_Size : Natural;
+    Hspeed : Lem.Speed_Range;
+    Hspeed_Size : Integer;
+    use type My_Math.Real, Lem.Speed_Range, Lem.Mass_Range;
+  begin
+    -- Thrust gauge
+    Con_Io.Set_Foreground (Con_Io.Get_Background);
+    Con_Io.Graphics.Fill_Rectangle (Thx, Thymin, Thx + Gauge_Size, Thymax);
+    Thrust_Size := Natural (My_Math.Trunc(
+                   My_Math.Real(Y_Thrust) * Thfactor));
+    Con_Io.Set_Foreground (Con_Io.Blue);
+    Con_Io.Graphics.Fill_Rectangle (Thx, Thymin, Thx + Gauge_Size,
+                                    Thymin + Thrust_Size);
+    -- Vertical speed
+    Con_Io.Set_Foreground (Con_Io.Get_Background);
+    Con_Io.Graphics.Fill_Rectangle (Vsx, Vsymin, Vsx + Gauge_Size, Vsymax);
+    Vspeed := New_Speed.Y_Speed;
+    if Vspeed > Max_Vert_Speed then
+      Vspeed := Max_Vert_Speed;
+    elsif Vspeed < -Max_Vert_Speed then
+      Vspeed := -Max_Vert_Speed;
+    end if;
+    Con_Io.Set_Foreground (Con_Io.Blue);
+    Con_Io.Graphics.Draw_Line (Vsx - 3, Vsymid, Vsx + Gauge_Size + 3, Vsymid);
+    Vspeed_Size := Integer (My_Math.Trunc(
+                   My_Math.Real(Vspeed) * Vsfactor));
+    if Vspeed < - Flight.Max_Verti_Speed then
+      Con_Io.Set_Foreground (Con_Io.Red);
+    end if;
+    Con_Io.Graphics.Fill_Rectangle (Vsx, Vsymid, Vsx + Gauge_Size,
+                                    Vsymid + Vspeed_Size);
+    -- Fuel
+    Con_Io.Set_Foreground (Con_Io.Get_Background);
+    Con_Io.Graphics.Fill_Rectangle (Fuxmin, Fuy, Fuxmax, Fuy + Gauge_Size);
+    Fuel_Size := Natural (My_Math.Trunc(
+                   My_Math.Real(Fuel) * Fufactor));
+    if Fuel < Lem.Max_Fuel / 10.0 then
+      Con_Io.Set_Foreground (Con_Io.Red);
+    else
+      Con_Io.Set_Foreground (Con_Io.Blue);
+    end if;
+    Con_Io.Graphics.Fill_Rectangle (Fuxmin, Fuy,
+                                    Fuxmin + Fuel_Size, Fuy + Gauge_Size);
+    -- Horizontal speed
+    Con_Io.Set_Foreground (Con_Io.Get_Background);
+    Con_Io.Graphics.Fill_Rectangle (Hsxmin, Hsy, Hsxmax, Hsy + Gauge_Size);
+    Hspeed := New_Speed.X_Speed;
+    if Hspeed > Max_Hori_Speed then
+      Hspeed := Max_Hori_Speed;
+    elsif Hspeed < -Max_Hori_Speed then
+      Hspeed := -Max_Hori_Speed;
+    end if;
+    Con_Io.Set_Foreground (Con_Io.Blue);
+    Con_Io.Graphics.Draw_Line (Hsxmid, Hsy - 3, Hsxmid, Hsy + Gauge_Size + 3);
+    Hspeed_Size := Integer (My_Math.Trunc(
+                   My_Math.Real(Hspeed) * Hsfactor));
+    if abs Hspeed > Flight.Max_Horiz_Speed then
+      Con_Io.Set_Foreground (Con_Io.Red);
+    end if;
+    Con_Io.Graphics.Fill_Rectangle (Hsxmid, Hsy, Hsxmid + Hspeed_Size,
+                                    Hsy + Gauge_Size);
+  end Put_Gauges;
+
+  -- Update lem and show the gauges
+  procedure Update (New_Pos : in Lem.Position_Rec;
+                    New_Speed : in Lem.Speed_Rec) is
+
   begin
     if Prev_Pos.Set then
       -- Hide prev pos
       Con_Io.Set_Foreground (Con_Io.Get_Background);
       Draw_Lem (Prev_Pos.Pos);
     end if;
-    if New_Pos.Set then
-      -- Show prev pos
-      Con_Io.Set_Foreground (Con_Io.Cyan);
-      Draw_Lem (New_Pos.Pos);
-    end if;
+    -- Show new pos
+    Con_Io.Set_Foreground (Con_Io.Cyan);
+    Draw_Lem (New_Pos);
+    -- Save pos
+    Prev_Pos := (True, New_Pos);
     -- Show Y thrust, speeds and fuel
-    -- @@@
+    Put_Gauges (New_Speed);
     Con_Io.Flush;
   end Update;
 
+  -- Delete lem and show the gauges
+  procedure Delete (New_Speed : in Lem.Speed_Rec) is
+  begin
+    if Prev_Pos.Set then
+      -- Hide prev pos
+      Con_Io.Set_Foreground (Con_Io.Get_Background);
+      Draw_Lem (Prev_Pos.Pos);
+    end if;
+    Prev_Pos := No_Pos;
+    -- Show Y thrust, speeds and fuel
+    Put_Gauges (New_Speed);
+    Con_Io.Flush;
+  end Delete;
 
   -- Put game end
   -- subtype End_Reason_List is Flight.Status_List
   --                            range Flight.Landed .. Flight.Lost;
   procedure Put_End (Reason : in End_Reason_List) is
+    use type Flight.Status_List;
+    X_Text : constant Con_Io.Graphics.X_Range := 250;
+    Y_Text : constant Con_Io.Graphics.Y_Range := 250;
   begin
-    -- @@@
-    null;
+    
+    case Reason is
+      when Flight.Landed =>
+        Con_Io.Set_Foreground (Con_Io.Light_Green);
+        Con_Io.Graphics.Put ("You landed the LEM safely", X_Text, Y_Text);
+      when Flight.Lost =>
+        Con_Io.Set_Foreground (Con_Io.Magenta);
+        Con_Io.Graphics.Put ("You lost the LEM", X_Text, Y_Text);
+      when Flight.Crashed =>
+        Con_Io.Set_Foreground (Con_Io.Magenta);
+        Con_Io.Graphics.Put ("You crashed the LEM", X_Text, Y_Text);
+    end case;
+    Con_Io.Set_Foreground (Con_Io.Light_Gray);
+    Con_Io.Graphics.Put ("Hit any key or Escape", X_Text, Y_Text - 25);
+    Con_Io.Flush;
   end Put_End;
+
+  -- Get a key
+  function Get_Key (Wait : in Duration) return Got_List is separate;
 
 end Screen;
 
--- usr/lem/screen.ads
+++ usr/lem/screen.ads
@@ -1,31 +1,32 @@
-with Space, Moon, Flight, Lem;
+with Con_Io;
+with Flight, Lem;
 package Screen is
 
   -- Reset screen. Display titles and moon ground
-  procedure Init (Ground : in Moon.Ground_Array);
+  procedure Init;
+
+  -- Close (definitively)
+  procedure Close;
 
   -- Redisplay moon ground, frame...
-  procedure Refresh (Ground : in Moon.Ground_Array);
+  procedure Refresh;
 
-  -- Lem position
-  type Lem_Position (Set : Boolean := True) is record
-    case Set is
-      when True => Pos : Space.Position_Rec;
-      when False => null;
-    end case;
-  end record;
-  No_Position : constant Lem_Position := (Set => False);
+  -- Update lem and the gauges
+  procedure Update (New_Pos : in Lem.Position_Rec;
+                    New_Speed : in Lem.Speed_Rec);
 
-  -- Update lem and its speed and Y_thrust
-  procedure Update (Prev_Pos, New_Pos : in Lem_Position;
-                    Speed    : in Lem.Speed_Rec;
-                    Y_Thrust : in Lem.Y_Thrust_Range;
-                    Fuel     : in Lem.Fuel_Range);
+  -- Delete lem and show the gauges
+  procedure Delete (New_Speed : in Lem.Speed_Rec);
 
   -- Put game end
   subtype End_Reason_List is Flight.Status_List
                              range Flight.Landed .. Flight.Lost;
   procedure Put_End (Reason : in End_Reason_List);
+
+  -- Get a key
+  type Got_List is (Right_Key, Left_Key, Up_Key, Down_Key, Break,
+                    Other_key, Timeout, Refresh);
+  function Get_Key (Wait : in Duration) return Got_List;
 
 end Screen;
 
