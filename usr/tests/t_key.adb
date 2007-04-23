70e95b959cca7fb1cca17a22213c6c96b62ffeba
Pascal MALAISE <malaise@magic.fr>
2007-04-23 17:24:43 +0000

    Re-created.

---
+++ usr/tests/t_key.adb
@@ -0,0 +1,33 @@
+with Ada.Wide_Text_Io;
+with Con_Io, My_Io;
+procedure T_Key is
+
+  Got : Con_Io.Get_Result;
+
+  use type Con_Io.Curs_Mvt;
+begin
+
+  Con_Io.Init;
+  Con_Io.Reset_Term;
+  Got := (Mvt => Con_Io.Refresh);
+  loop
+    if Got.Mvt = Con_Io.Refresh then
+      -- Refresh
+      Con_Io.Move;
+      Con_Io.Put_Line ("Exit with Ctrl C");
+    end if;
+    Got := Con_Io.Get;
+
+    My_Io.Put (Got.Mvt'Img);
+
+    if Got.Mvt = Con_Io.Full then
+      My_Io.Put (Integer'(Wide_Character'Pos (Got.Char)), Base => 16);
+      Ada.Wide_Text_Io.Put (" >" & Got.Char & "<");
+    elsif Got.Mvt = Con_Io.Break then
+      exit;
+    end if;
+    My_Io.New_Line;
+  end loop;
+  My_Io.New_Line;
+end T_Key;
+
