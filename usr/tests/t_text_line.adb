0fb59c82f767179cbf76bd6cf0240e6d382ff625
Pascal MALAISE <malaise@magic.fr>
2007-07-19 17:52:40 +0000

    Handle stdin if no arg.

--- usr/tests/t_text_line.adb
+++ usr/tests/t_text_line.adb
@@ -5,8 +5,12 @@ procedure T_Text_Line is
     Fd : Sys_Calls.File_Desc;
     In_File, Out_File : Text_Line.File_Type;
   begin
-    Fd := Sys_Calls.Open (File_Name, Sys_Calls.In_File);
-    Text_Line.Open (In_File, Text_Line.In_File, Fd);
+    if File_Name /= "" then
+      Fd := Sys_Calls.Open (File_Name, Sys_Calls.In_File);
+      Text_Line.Open (In_File, Text_Line.In_File, Fd);
+    else
+      Text_Line.Open (In_File, Text_Line.In_File, Sys_Calls.Stdin);
+    end if;
     Text_Line.Open (Out_File, Text_Line.Out_File, Sys_Calls.Stdout);
     loop
       declare
@@ -16,9 +20,11 @@ procedure T_Text_Line is
         Text_Line.Put (Out_File, Str);
       end;
     end loop;
-    Text_Line.Put_Line (Out_File, "<<EOF>>");
     Text_Line.Close (In_File);
-    Sys_Calls.Close (Fd);
+    if File_Name /= "" then
+      Sys_Calls.Close (Fd);
+      Text_Line.Put_Line (Out_File, "<<EOF>>");
+    end if;
     Text_Line.Close (Out_File);
   exception
     when Sys_Calls.Name_Error =>
@@ -26,8 +32,12 @@ procedure T_Text_Line is
   end Do_One;
 
 begin
-  for I in 1 .. Argument.Get_Nbre_Arg loop
-    Do_One (Argument.Get_Parameter (Occurence => I));
-  end loop;
+  if Argument.Get_Nbre_Arg = 0 then
+    Do_One ("");
+  else
+    for I in 1 .. Argument.Get_Nbre_Arg loop
+      Do_One (Argument.Get_Parameter (Occurence => I));
+    end loop;
+  end if;
 end T_Text_Line;
 
