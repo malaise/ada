0a4c1093cd04082f90af83a38bfaea8af7826f39
Pascal MALAISE <malaise@magic.fr>
2007-10-05 17:28:58 +0000

    Replace arguments -l Lan and -p Port by an address Lan:Port

--- usr/misc/udp_spy.adb
+++ usr/misc/udp_spy.adb
@@ -1,6 +1,7 @@
 with Ada.Exceptions, Ada.Text_Io, Ada.Calendar;
 with Argument, Basic_Proc, Text_Handler, Date_Image, Normal, Int_Image,
-     Upper_Str, Socket, Event_Mng, Ip_Addr, Tcp_Util;
+     Upper_Str, String_Mng, Text_Line, Sys_Calls,
+     Socket, Event_Mng, Ip_Addr, Tcp_Util;
 
 procedure Udp_Spy is
 
@@ -14,12 +15,14 @@ procedure Udp_Spy is
   use type Tcp_Util.Remote_Port_List, Tcp_Util.Remote_Host_List;
   type Dump_Kind_List is (Header, Short, Full, Binary);
   Dump_Mode : Dump_Kind_List;
+  File : Text_Line.File_Type;
   Short_Data_Len : constant Natural := 64;
 
   package Byte_Io is new Ada.Text_Io.Integer_Io (Socket.Byte);
 
   function Byte_Image is new Int_Image (Socket.Byte);
   function Port_Image is new Int_Image (Socket.Port_Num);
+  function Inte_Image is new Int_Image (Integer);
 
   function Dest_Image (S : Socket.Socket_Dscr) return String is
     Ip_Addr : constant Socket.Ip_Address
@@ -50,7 +53,6 @@ procedure Udp_Spy is
   -- Signal callback
   procedure Signal_Cb is
   begin
-    Ada.Text_Io.Put_Line (Curr_Date_Image & " aborted.");
     Sig := True;
   end Signal_Cb;
 
@@ -97,7 +99,7 @@ procedure Udp_Spy is
       end if;
       -- Flush
       if O = 16 or else I = Len then
-        Ada.Text_Io.Put_Line ("  " & Hexa_Str & "   " & Char_Str);
+        Text_Line.Put_Line (File, "  " & Hexa_Str & "   " & Char_Str);
         Hexa_Str := (others => ' ');
         Char_Str := (others => ' ');
       end if;
@@ -110,15 +112,15 @@ procedure Udp_Spy is
     Data_Len : Natural;
   begin
     if F /= Fd then
-      Basic_Proc.Put_Line_Error ("Read callback: unknown fd");
+      Basic_Proc.Put_Line_Error ("Error: read callback on unknown fd.");
       raise Program_Error;
     end if;
     My_Receive (Soc, Data, Data_Len, Set_For_Reply => True);
     -- Put header
     if Dump_Mode /= Binary then
-      Ada.Text_Io.Put (Curr_Date_Image);
-      Ada.Text_Io.Put (" got " & Normal (Data_Len, 4) & " bytes from ");
-      Ada.Text_Io.Put_Line (Dest_Image (Soc));
+      Text_Line.Put (File, Curr_Date_Image);
+      Text_Line.Put (File, " got " & Normal (Data_Len, 4) & " bytes from ");
+      Text_Line.Put_Line (File, Dest_Image (Soc));
     end if;
     -- Put data
     if Dump_Mode = Header then
@@ -126,13 +128,14 @@ procedure Udp_Spy is
     elsif Dump_Mode = Binary then
       -- Binary dump
       for I in 1 .. Data_Len loop
-        Ada.Text_Io.Put (Character'Val (Data(I)));
+        Text_Line.Put (File, Character'Val (Data(I)) & "");
       end loop;
     elsif Dump_Mode = Short and then Data_Len > Short_Data_Len then
       Dump_Data (Short_Data_Len);
     else
       Dump_Data (Data_Len);
     end if;
+    Text_Line.Flush (File);
     return False;
   exception
     when Socket.Soc_Conn_Lost =>
@@ -143,15 +146,27 @@ begin
 
   -- Parse arguments
   begin
-    if Argument.Get_Nbre_Arg /= 2
-    and then Argument.Get_Nbre_Arg /= 3 then
+    if Argument.Get_Nbre_Arg /= 1
+    and then Argument.Get_Nbre_Arg /= 2 then
       raise Arg_Error;
     end if;
 
-    Argument.Get_Parameter (Port_Name, 1, "p");
-    Argument.Get_Parameter (Host_Name, 1, "l");
+    -- Parse address <Lan>:<Port>
+    declare
+      Address : constant String
+              := Argument.Get_Parameter (Param_Key => Argument.Not_Key);
+      Index : Natural;
+    begin
+      Index := String_Mng.Locate (Address, ":");
+      Text_Handler.Set (Host_Name, Address(1 .. Index - 1));
+      Text_Handler.Set (Port_Name, Address(Index + 1 .. Address'Last));
+    exception
+      when others =>
+        raise Arg_Error;
+    end;
+
     -- Dump mode
-    if Argument.Get_Nbre_Arg = 2 then
+    if Argument.Get_Nbre_Arg = 1 then
       -- Default
       Dump_Mode := Short;
     else
@@ -170,6 +185,15 @@ begin
   exception
     when Argument.Argument_Not_Found =>
       raise Arg_Error;
+  end;
+
+  -- Open output flow
+  begin
+    Text_Line.Open (File, Text_Line.Out_File, Sys_Calls.Stdout);
+  exception
+    when others =>
+      Basic_Proc.Put_Line_Error ("Error: cannot open output flow.");
+      raise;
   end;
 
   -- Create socket, add callback
@@ -221,20 +245,29 @@ begin
     exit when Sig;
   end loop;
 
+  -- Close
+  Text_Line.Close (File);
   if Event_Mng.Fd_Callback_Set (Fd, True) then
     Event_Mng.Del_Fd_Callback (Fd, True);
     Socket.Close (Soc);
   end if;
 
+  Basic_Proc.Put_Line_Error (Curr_Date_Image & " stopped.");
+
 exception
   when Arg_Error =>
-    Basic_Proc.Put_Line_Error ("Usage: " & Argument.Get_Program_Name
-       & " <port> <lan> [ <dump_mode> ]");
-    Basic_Proc.Put_Line_Error ("  <port> ::= -p <port_name_or_num>");
-    Basic_Proc.Put_Line_Error ("  <lan>  ::= -l <lan_name_or_num> ");
+    Basic_Proc.Put_Line_Error ("Usage: "
+       & Argument.Get_Program_Name & " <address> [ <dump_mode> ]");
+    Basic_Proc.Put_Line_Error ("  <address>   ::= <lan>:<port>");
+    Basic_Proc.Put_Line_Error ("  <lan>       ::= <lan_name_or_num> ");
+    Basic_Proc.Put_Line_Error ("  <port>      ::= <port_name_or_num>");
     Basic_Proc.Put_Line_Error ("  <dump_mode> ::= -dh | -ds | -df | -db");
+    Basic_Proc.Put_Line_Error ("Dump received headers, short ("
+      & Inte_Image(Short_Data_Len) & " Bytes, default), full or binary.");
+    Basic_Proc.Set_Error_Exit_Code;
   when Error : others =>
     Basic_Proc.Put_Line_Error ("Exception: "
         & Ada.Exceptions.Exception_Name (Error));
+    Basic_Proc.Set_Error_Exit_Code;
 end Udp_Spy;
 
