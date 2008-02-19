0bcef7bc9e4c7d92b29574c2f41be68081301686
Pascal MALAISE <malaise@magic.fr>
2008-02-19 19:11:06 +0000

    Test external resolver with Environ.

--- usr/tests/t_computer.adb
+++ usr/tests/t_computer.adb
@@ -1,14 +1,30 @@
 with Ada.Text_Io;
-with Argument, Computer, String_Mng;
+with Argument, Computer, String_Mng, Environ;
 procedure T_Computer is
   procedure Usage is
   begin
     Ada.Text_Io.Put_Line ("Usage: " & Argument.Get_Program_Name
      & " [ { <name>=<value> } ] <expression>");
   end Usage;
+
+  function My_Getenv (Name : String) return String is
+  begin
+    -- Try to resolve through environ
+    if Environ.Is_Set (Name) then
+      return Environ.Getenv (Name);
+    else
+      -- Raise Any exception
+      raise Constraint_Error;
+    end if;
+  end My_Getenv;
   Loc : Natural;
   N : Integer;
 begin
+  if Argument.Get_Nbre_Arg = 0 then
+    Usage;
+    return;
+  end if;
+  Computer.External_Resolver := My_Getenv'Unrestricted_Access;
   -- All args but last are variables
   for I in 1 .. Argument.Get_Nbre_Arg - 1 loop
     declare
@@ -24,11 +40,17 @@ begin
   end loop;
 
   -- Last arg is the expression
+  -- Evaluate expression
+  Ada.Text_Io.Put_Line ("Evaluation: ");
   Ada.Text_Io.Put_Line (Computer.Eval (
          Argument.Get_Parameter(Argument.Get_Nbre_Arg)));
+  -- Try to compute expression
+  Ada.Text_Io.Put_Line ("Computation: ");
   N := Computer.Compute (Argument.Get_Parameter(Argument.Get_Nbre_Arg));
   Ada.Text_Io.Put_Line (N'Img);
 exception
+  when Computer.Invalid_Expression =>
+    Ada.Text_Io.Put_Line ("Invalid expression");
   when others =>
     Usage;
     raise;
