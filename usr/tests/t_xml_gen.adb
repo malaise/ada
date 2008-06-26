6c5813b316e9c0fb669448fda6c1e0ad08084739
Pascal MALAISE <malaise@magic.fr>
2008-06-26 17:30:10 +0000

    Use Ctx of Xml_Parser.Generator. Test Xml_Parser.Check.

--- usr/tests/t_xml_gen.adb
+++ usr/tests/t_xml_gen.adb
@@ -1,62 +1,75 @@
-with Xml_Parser.Generator;
+with Basic_Proc, Xml_Parser.Generator;
 procedure T_Xml_Gen is
-  Dscr : Xml_Parser.Ctx_Type;
+  Dscr : Xml_Parser.Generator.Ctx_Type;
   Node : Xml_Parser.Node_Type;
   New_Node : Xml_Parser.Node_Type;
+  Ok : Boolean;
   use Xml_Parser, Xml_Parser.Generator;
 begin
-  Set_Version (Dscr, 1, 1);
-  Node := Get_Prologue (Dscr);
-  Add_Comment (Dscr, Node,
+  -- Generate Tree
+  Dscr.Set_Version (1, 1);
+  Node := Dscr.Get_Prologue;
+  Dscr.Add_Comment (Node,
      " Definition of variables for test of program ""comp_vars"" ", New_Node);
-  Add_Doctype (Dscr, Node, "Variables", False, "", "variables.dtd", "",
+  Dscr.Add_Doctype (Node, "Variables", False, "", "variables.dtd", "",
                New_Node);
-  Add_Comment (Dscr, Node, " After Doctype ", New_Node);
+  Dscr.Add_Comment (Node, " After Doctype ", New_Node);
 
-  Node := Get_Root_Element (Dscr);
-  Set_Name (Dscr, Node, "Variables");
-  Add_Child (Dscr, Node, " Below root ", Xml_Parser.Comment, New_Node);
+  Node := Dscr.Get_Root_Element;
+  Dscr.Set_Name (Node, "Variables");
+  Dscr.Add_Child (Node, " Below root ", Xml_Parser.Comment, New_Node);
 
-  Add_Brother (Dscr, New_Node, "Var", Xml_Parser.Element, New_Node);
-  Add_Attribute (Dscr, New_Node, "Name", "V1");
-  Add_Attribute (Dscr, New_Node, "Type", "Int");
-  Add_Child (Dscr, New_Node, "5", Xml_Parser.Text, New_Node);
-  Node := Get_Parent (Dscr, New_Node);
+  Dscr.Add_Brother (New_Node, "Var", Xml_Parser.Element, New_Node);
+  Dscr.Add_Attribute (New_Node, "Name", "V1");
+  Dscr.Add_Attribute (New_Node, "Type", "Int");
+  Dscr.Add_Child (New_Node, "5", Xml_Parser.Text, New_Node);
+  Node := Dscr.Get_Parent (New_Node);
 
-  Add_Brother (Dscr, Node, "Var", Xml_Parser.Element, New_Node);
-  Add_Attribute (Dscr, New_Node, "Name", "PATH");
-  Add_Child (Dscr, New_Node, "${PATH}:/usr/local/bin", Xml_Parser.Text,
+  Dscr.Add_Brother (Node, "Var", Xml_Parser.Element, New_Node);
+  Dscr.Add_Attribute (New_Node, "Name", "PATH");
+  Dscr.Add_Child (New_Node, "${PATH}:/usr/local/bin", Xml_Parser.Text,
              New_Node);
-  Node := Get_Parent (Dscr, New_Node);
+  Node := Dscr.Get_Parent (New_Node);
 
-  Add_Brother (Dscr, Node, "Var", Xml_Parser.Element, New_Node);
-  Add_Attribute (Dscr, New_Node, "Name", "V2");
-  Add_Attribute (Dscr, New_Node, "Type", "Int");
-  Add_Child (Dscr, New_Node, "5*${V1}*${V1}", Xml_Parser.Text, New_Node);
-  Node := Get_Parent (Dscr, New_Node);
+  Dscr.Add_Brother (Node, "Var", Xml_Parser.Element, New_Node);
+  Dscr.Add_Attribute (New_Node, "Name", "V2");
+  Dscr.Add_Attribute (New_Node, "Type", "Int");
+  Dscr.Add_Child (New_Node, "5*${V1}*${V1}", Xml_Parser.Text, New_Node);
+  Node := Dscr.Get_Parent (New_Node);
 
-  Add_Brother (Dscr, Node, "Var", Xml_Parser.Element, New_Node);
-  Add_Attribute (Dscr, New_Node, "Name", "V3");
-  Add_Attribute (Dscr, New_Node, "Type", "Str");
-  Add_Child (Dscr, New_Node, "${V1}*${V2}", Xml_Parser.Text, New_Node);
-  Node := Get_Parent (Dscr, New_Node);
+  Dscr.Add_Brother (Node, "Var", Xml_Parser.Element, New_Node);
+  Dscr.Add_Attribute (New_Node, "Name", "V3");
+  Dscr.Add_Attribute (New_Node, "Type", "Str");
+  Dscr.Add_Child (New_Node, "${V1}*${V2}", Xml_Parser.Text, New_Node);
+  Node := Dscr.Get_Parent (New_Node);
 
-  Add_Brother (Dscr, Node, "Var", Xml_Parser.Element, New_Node);
-  Add_Attribute (Dscr, New_Node, "Name", "V2");
-  Add_Attribute (Dscr, New_Node, "Type", "Int");
-  Add_Child (Dscr, New_Node, "${V3}*2", Xml_Parser.Text, New_Node);
-  Node := Get_Parent (Dscr, New_Node);
+  Dscr.Add_Brother (Node, "Var", Xml_Parser.Element, New_Node);
+  Dscr.Add_Attribute (New_Node, "Name", "V2");
+  Dscr.Add_Attribute (New_Node, "Type", "Int");
+  Dscr.Add_Child (New_Node, "${V3}*2", Xml_Parser.Text, New_Node);
+  Node := Dscr.Get_Parent (New_Node);
 
-  Add_Brother (Dscr, Node,
-   " Comp_var will report error if variable SET_ME is not set ",
-   Xml_Parser.Comment, New_Node);
+  Dscr.Add_Brother (Node,
+       " Comp_var will report error if variable SET_ME is not set ",
+       Xml_Parser.Comment, New_Node);
 
-  Add_Brother (Dscr, New_Node, "Var", Xml_Parser.Element, New_Node);
-  Add_Attribute (Dscr, New_Node, "Name", "Fail");
-  Add_Attribute (Dscr, New_Node, "Type", "Str");
-  Add_Child (Dscr, New_Node, "${SET_ME}", Xml_Parser.Text, New_Node);
-  Node := Get_Parent (Dscr, New_Node);
+  Dscr.Add_Brother (New_Node, "Var", Xml_Parser.Element, New_Node);
+  Dscr.Add_Attribute (New_Node, "Name", "Fail");
+  Dscr.Add_Attribute (New_Node, "Type", "Str");
+  Dscr.Add_Child (New_Node, "${SET_ME}", Xml_Parser.Text, New_Node);
+  Node := Dscr.Get_Parent (New_Node);
 
-  Put (Dscr, Xml_Parser.Generator.Stdout);
+  -- Check tree
+  Dscr.Check (Ok);
+  if not Ok then
+    Basic_Proc.Put_Line_Error ("Error in tree "
+          & Dscr.Get_Parse_Error_Message);
+    Basic_Proc.Set_Error_Exit_Code;
+    Dscr.Clean;
+    return;
+  end if;
+
+  -- Display Tree
+  Dscr.Put (Xml_Parser.Generator.Stdout);
 end T_Xml_Gen;
 
