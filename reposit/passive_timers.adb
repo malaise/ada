6fc0136da7a48169deee88335044d300b6ed6c26
Pascal MALAISE <malaise@magic.fr>
2009-06-15 16:49:42 +0000

    Use a Chrono to follow Virtual_Time.

--- reposit/passive_timers.adb
+++ reposit/passive_timers.adb
@@ -1,31 +1,72 @@
 with Ada.Unchecked_Deallocation;
+with Perpet;
 package body Passive_Timers is
 
-  -- type Passive_Timer is tagged private;
-
+  procedure Free is new Ada.Unchecked_Deallocation (Timer_Rec, Timer_Access);
 
   -- Arm a passive timer with a given period
   -- Overwrites any previous setting on this timer
   -- Raises Invalid_Period if Period is <= 0.0
-  procedure Arm (Timer : in out Passive_Timer;
-                 Period : in Duration) is
-    use type Ada.Calendar.Time;
+  procedure Arm (Timer  : in out Passive_Timer;
+                 Period : in Duration;
+                 Clock  : in Virtual_Time.Clock_Access := null) is
+    use type Virtual_Time.Time, Perpet.Delta_Rec;
   begin
     if Period <= 0.0 then
       raise Invalid_Period;
     end if;
-    Timer.Acc.Next_Expiration := Ada.Calendar.Clock + Period;
+    if Timer.Acc = null then
+      Timer.Acc := new Timer_Rec;
+    end if;
+    -- (Re) initialize chrono
+    Timer.Acc.Chrono.Stop;
+    Timer.Acc.Chrono.Attach (Clock);
+    Timer.Acc.Chrono.Start;
+    -- Initialise timer
+    Timer.Acc.Next_Expiration := Perpet.To_Delta_Rec (Period);
     Timer.Acc.Period := Period;
   end Arm;
 
+  -- Stop a timer, which becomes unusable until re-armed
+  -- Timer_Stopped : exception;
+  procedure Stop (Timer : in out Passive_Timer) is
+  begin
+    if Timer.Acc = null then
+      raise Timer_Stopped;
+    end if;
+    -- Deallocate (chrono un-registers...)
+    Free (Timer.Acc);
+  end Stop;
+
+  -- Suspend a timer: pending expirations can still be retrieved
+  -- No action is timer is alread syspended
+  procedure Suspend (Timer : in out Passive_Timer) is
+  begin
+    if Timer.Acc = null then
+      raise Timer_Stopped;
+    end if;
+    Timer.Acc.Chrono.Stop;
+  end Suspend;
+
+  -- Resume a suspended a timer: new expirations are resumed
+  -- No action is timer is not syspended
+  procedure Resume (Timer : in out Passive_Timer) is
+  begin
+    if Timer.Acc = null then
+      raise Timer_Stopped;
+    end if;
+    Timer.Acc.Chrono.Stop;
+  end Resume;
 
   -- Checks if timer expiration time (Prev_Exp + Period) is reached
   -- If yes, add Period to expiration time
   function Has_Expired (Timer : in Passive_Timer) return Boolean is
-    use type Ada.Calendar.Time;
+    use type Chronos.Time_Rec;
   begin
-    if Timer.Acc.Period = Not_Started
-    or else Ada.Calendar.Clock < Timer.Acc.Next_Expiration then
+    if Timer.Acc = null then
+      raise Timer_Stopped;
+    end if;
+    if Timer.Acc.Chrono.Read < Timer.Acc.Next_Expiration then
       return False;
     else
       Timer.Acc.Next_Expiration :=
@@ -34,7 +75,6 @@ package body Passive_Timers is
     end if;
   end Has_Expired;
 
-  procedure Free is new Ada.Unchecked_Deallocation (Timer_Rec, Timer_Access);
   procedure Finalize (Timer : in out Passive_Timer) is
   begin
     Free (Timer.Acc);
--- reposit/passive_timers.ads
+++ reposit/passive_timers.ads
@@ -1,4 +1,5 @@
-with Ada.Finalization, Ada.Calendar;
+with Ada.Finalization;
+with Virtual_Time, Chronos;
 package Passive_Timers is
 
   type Passive_Timer is tagged limited private;
@@ -6,8 +7,21 @@ package Passive_Timers is
   -- Arm a passive timer with a given period
   -- Overwrites any previous setting on this timer
   -- Raises Invalid_Period if Period is <= 0.0
-  procedure Arm (Timer : in out Passive_Timer;
-                 Period : in Duration);
+  procedure Arm (Timer  : in out Passive_Timer;
+                 Period : in Duration;
+                 Clock  : in Virtual_Time.Clock_Access := null);
+
+  -- Stop a timer, which becomes unusable until re-armed
+  Timer_Stopped : exception;
+  procedure Stop (Timer : in out Passive_Timer);
+
+  -- Suspend a timer: pending expirations can still be retrieved
+  -- No action is timer is alread syspended
+  procedure Suspend (Timer : in out Passive_Timer);
+
+  -- Resume a suspended a timer: new expirations are resumed
+  -- No action is timer is not syspended
+  procedure Resume (Timer : in out Passive_Timer);
 
   -- Checks if timer expiration time (Prev_Exp + Period) is reached
   -- If yes, add Period to expiration time
@@ -16,16 +30,16 @@ package Passive_Timers is
   Invalid_Period : exception;
 
 private
-  Not_Started : constant Duration := 0.0;
   type Timer_Rec is record
-    Period : Duration := Not_Started;
-    Next_Expiration : Ada.Calendar.Time;
+    Period : Duration;
+    Next_Expiration : Chronos.Time_Rec;
+    Chrono : Chronos.Chrono_Type;
   end record;
   type Timer_Access is access Timer_Rec;
 
   -- So timer can be In for function Has_Expired
   type Passive_Timer is new Ada.Finalization.Limited_Controlled with record
-    Acc : Timer_Access := new Timer_Rec;
+    Acc : Timer_Access;
   end record;
 
   procedure Finalize (Timer : in out Passive_Timer);
