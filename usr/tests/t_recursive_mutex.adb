58eb13e7244096c30aea06d94e0c7cdd0a1f44b0
Pascal MALAISE <malaise@magic.fr>
2009-06-22 16:53:09 +0000

    Add recursive mutex and use it to protect timers

---
+++ usr/tests/t_recursive_mutex.adb
@@ -0,0 +1,74 @@
+with Ada.Text_Io;
+with Recursive_Mutex, Mixed_Str;
+procedure T_Recursive_Mutex is
+
+  M : Recursive_Mutex.Mutex;
+  B : Boolean;
+
+  task T is
+    entry Start;
+    entry Stop;
+  end T;
+
+
+  task body T is
+  begin
+    accept Start;
+    loop
+      Ada.Text_Io.Put_Line ("   Task gets mutex");
+      M.Get;
+      Ada.Text_Io.Put_Line ("   Task has got mutex");
+      delay 3.0;
+      Ada.Text_Io.Put_Line ("   Task releases mutex");
+      M.Release;
+      select
+        accept Stop;
+        exit;
+      or
+        delay 3.0;
+      end select;
+    end loop;
+  end T;
+
+begin
+  -- Get mutex twice
+  Ada.Text_Io.Put_Line ("Getting mutex");
+  M.Get;
+  Ada.Text_Io.Put_Line ("Getting mutex");
+  M.Get;
+
+  -- Start task and get mutex a third time
+  Ada.Text_Io.Put_Line ("Starting task");
+  T.Start;
+  Ada.Text_Io.Put_Line ("Getting mutex");
+  M.Get;
+  Ada.Text_Io.New_Line;
+
+  -- Release fully mutex (task takes it)
+  Ada.Text_Io.Put_Line ("Releasing fully mutex");
+  M.Release_Full;
+
+  -- Get mutex => Ko, Ko... Ok
+  loop
+    Ada.Text_Io.Put_Line ("Trying to get mutex");
+    B := M.Get (1.0);
+    Ada.Text_Io.Put_Line ("Result is " & Mixed_Str (B'Img));
+    exit when B;
+  end loop;
+  Ada.Text_Io.New_Line;
+
+  -- Get mutex a second time then release twice
+  Ada.Text_Io.Put_Line ("Getting mutex and waiting 2s");
+  M.Get;
+  delay 2.0;
+  Ada.Text_Io.Put_Line ("Releasing mutex twice");
+  M.Release;
+  M.Release;
+
+  -- The end
+  Ada.Text_Io.Put_Line ("Stopping task");
+  T.Stop;
+  Ada.Text_Io.Put_Line ("Done");
+
+end T_Recursive_Mutex;
+
