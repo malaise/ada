bed16ef891d97f8983474263f6df37d28997ae7e
Pascal MALAISE <malaise@telemaque>
2010-06-18 19:51:42 +0200

    C_Nbres reorganization of types ans operations

--- usr/navig/nav_data-add.adb
+++ usr/navig/nav_data-add.adb
@@ -3,16 +3,19 @@
 with C_Nbres;
 separate (Nav_Data)
 function Add (X, Y : Nav_Types.T_Vector) return Nav_Types.T_Vector is
-  Cx, Cy, Cr : C_Nbres.Complex;
+  Cx, Cy : C_Nbres.Complex;
+  P : C_Nbres.Polar;
 begin
-  Cx := C_Nbres.Create_Complex(C_Nbres.Real(X.Speed),
-                               C_Nbres.Degree(To_Real(X.Angle)) );
-  Cy := C_Nbres.Create_Complex(C_Nbres.Real(Y.Speed),
-                               C_Nbres.Degree(To_Real(Y.Angle)) );
-  Cr := C_Nbres."+" (Cx, Cy);
+  Cx := C_Nbres.To_Complex (C_Nbres.Create_Polar(
+                               C_Nbres.Real(X.Speed),
+                               C_Nbres.Degree(To_Real(X.Angle)) ));
+  Cy := C_Nbres.To_Complex (C_Nbres.Create_Polar(
+                               C_Nbres.Real(Y.Speed),
+                               C_Nbres.Degree(To_Real(Y.Angle)) ));
+  P := C_Nbres.To_Polar (C_Nbres."+" (Cx, Cy));
 
   return (
-   Speed => Nav_Types.T_Speed(C_Nbres.Module(Cr)),
-   Angle => To_Angle(Real(C_Nbres.Angle_Degree(Cr))) );
+   Speed => Nav_Types.T_Speed(C_Nbres.Module(P)),
+   Angle => To_Angle(Real(C_Nbres.Angle_Degree(P))) );
 end Add;
 
