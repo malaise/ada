4af998845732802a9f16f7e49fd6f180094345da
Pascal MALAISE <malaise@telemaque>
2010-07-23 18:37:30 +0200

    Add help

--- usr/tests/t_assert.adb
+++ usr/tests/t_assert.adb
@@ -1,7 +1,11 @@
+with Ada.Text_Io;
 with Assertion;
 procedure T_Assert is
 
 begin
+  Ada.Text_Io.Put_Line ("This program is normally silent");
+  Ada.Text_Io.Put_Line ("Set ENV variable ASSERT_ACTION to TRACE to see the 3 traces");
+   Ada.Text_Io.Put_Line (" or to RAISE to raise first exception.");
   Assertion.Assert (False, "Assert test 1");
   Assertion.Assert (True, "Should not appear");
   Assertion.Assert (False, "Assert test 2");
