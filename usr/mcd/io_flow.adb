a867e8fc3c3891393b8b21be8d8c20129e23ba9a
Pascal MALAISE <malaise@telemaque>
2010-08-12 19:28:36 +0200

    Udp channel tested

--- usr/mcd/io_flow.adb
+++ usr/mcd/io_flow.adb
@@ -18,16 +18,25 @@ package body Io_Flow is
   -- Data read
   Input_Data : Asu_Us;
 
+  -- Tempo data until Line Feed
+  Tmp_Data : Asu_Us;
+
   -- Udp or tcp socket
+  Host : Tcp_Util.Remote_Host;
+  Port : Tcp_Util.Remote_Port;
+  Is_Ipm : Boolean;
   Soc : Socket.Socket_Dscr;
+  function Socket_Is_Active return Boolean;
+  procedure Activate_Socket (Active : in Boolean);
+  procedure Open_Socket (And_Activate : in Boolean);
+  procedure Soc_Send is new Socket.Send (Io_Data.Message_Type);
+  procedure Soc_Receive is new Socket.Receive (Io_Data.Message_Type);
 
   -- Fifo
   Fifo_Name : Asu_Us;
   package Mcd_Fifos is new Fifos.Fifo (Io_Data.Message_Type);
   Acc_Id, Client_Id : Mcd_Fifos.Fifo_Id := Mcd_Fifos.No_Fifo;
   procedure Open_Fifo (Active : in Boolean);
-  -- Concatenated fifo messages until one lasts with Cr or Lf
-  Fifo_Data : Asu_Us;
 
   -- Async Stdin
   function Stdin_Cb (Str : in String) return Boolean;
@@ -46,10 +55,7 @@ package body Io_Flow is
   end Init_Default;
 
   procedure Init (Default : in Boolean := False) is
-    Host : Tcp_Util.Remote_Host;
-    Port : Tcp_Util.Remote_Port;
     use type Sys_Calls.File_Desc_Kind_List;
-    use type Tcp_Util.Remote_Host_List, Tcp_Util.Remote_Port_List;
   begin
     if Io_Mode /= Unknown then
       return;
@@ -86,6 +92,7 @@ package body Io_Flow is
       Async_Stdin.Put_Line_Err ("Not Implemented.");
       raise Init_Error;
       Io_Mode := Tcp;
+      return;
     end if;
 
     -- Get udp/ipm spec if set
@@ -102,15 +109,20 @@ package body Io_Flow is
           Async_Stdin.Put_Line_Err ("Invalid udp spec");
           raise Init_Error;
       end;
-      Soc.Open (Socket.Udp);
-      if Host.Kind = Tcp_Util.Host_Id_Spec
-      or else Host.Name(Host.Name'First) /= ' ' then
-        -- An address specified => Ipm
-        -- Use Set_Dest to indicate Imp address
-        Socket_Util.Set_Destination (Soc, True, Host, Port);
+      Open_Socket (True);
+      if Debug.Debug_Level_Array(Debug.Flow) then
+        Async_Stdin.Put_Err ("Flow: init on ");
+        if Is_Ipm then
+          Async_Stdin.Put_Err ("ipm LAN " &
+            Ip_Addr.Image (Socket.Id2Addr (Soc.Get_Destination_Host)) & " ");
+        else
+          Async_Stdin.Put_Err ("udp ");
+        end if;
+        Async_Stdin.Put_Line_Err ("port " &
+          Ip_Addr.Image (Soc.Get_Linked_To));
       end if;
-      Socket_Util.Link (Soc, Port);
       Io_Mode := Udp;
+      return;
     end if;
 
     -- If no arg => Stdin
@@ -144,7 +156,7 @@ package body Io_Flow is
   end Init;
 
   ----------------------------------------------------
-  -- Get data from fifo or stdin
+  -- Get data from fifo, tcp, udp or stdin (async or not)
   ----------------------------------------------------
   procedure Next_Line (Str : out Asu_Us) is
     Evt : Event_Mng.Out_Event_List;
@@ -170,11 +182,11 @@ package body Io_Flow is
         exit when Len /= 0;
       end loop;
     else
-      -- Get next data on Tty stdin or Fifo
+      -- Get next data on async stdin, socket or Fifo
       loop
         Input_Data := Asu_Null;
         if Debug.Debug_Level_Array(Debug.Flow) then
-          Async_Stdin.Put_Line_Err ("Flow: Waiting on fifo/tty");
+          Async_Stdin.Put_Line_Err ("Flow: Waiting on fifo/socket/tty");
         end if;
         Evt := Event_Mng.Wait (Event_Mng.Infinite_Ms);
 
@@ -185,7 +197,14 @@ package body Io_Flow is
         elsif Evt = Event_Mng.Signal_Event then
           -- Give up on signal
           Input_Data := Asu_Null;
+          if Debug.Debug_Level_Array(Debug.Flow) then
+            Async_Stdin.Put_Line_Err ("Flow: Got signal");
+          end if;
           exit;
+        else
+          if Debug.Debug_Level_Array(Debug.Flow) then
+            Async_Stdin.Put_Line_Err ("Flow: Got event " & Evt'Img);
+          end if;
         end if;
       end loop;
     end if;
@@ -198,6 +217,8 @@ package body Io_Flow is
         if Client_Id /= Mcd_Fifos.No_Fifo then
           Mcd_Fifos.Activate (Client_Id, True);
         end if;
+      when Udp | Tcp =>
+        Activate_Socket (True);
       when others =>
         null;
     end case;
@@ -207,26 +228,35 @@ package body Io_Flow is
   end Next_Line;
 
   ----------------------------------------------------
-  -- Put data on fifo or stdout
+  -- Pet data on fifo, tcp, udp or stdin (async or not)
   ----------------------------------------------------
-  -- Send one message
+  -- Send one message on socket or FIFO
   Message_To_Put : Io_Data.Message_Type;
   procedure Send_Message (Str : in String) is
     Active : Boolean;
     Res : Fifos.Send_Result_List;
     use type Fifos.Send_Result_List;
   begin
-    Active := Mcd_Fifos.Is_Active (Client_Id);
     Message_To_Put (1 .. Str'Length) := Str;
-    Res := Mcd_Fifos.Send (Client_Id, Message_To_Put, Str'Length);
-    if Res /= Fifos.Ok and then Res /= Fifos.Overflow then
-      Mcd_Fifos.Close (Client_Id);
-      Open_Fifo (Active);
+    if Io_Mode = Fifo then
+      Active := Mcd_Fifos.Is_Active (Client_Id);
+      Res := Mcd_Fifos.Send (Client_Id, Message_To_Put, Str'Length);
+      if Res /= Fifos.Ok and then Res /= Fifos.Overflow then
+        Mcd_Fifos.Close (Client_Id);
+        Open_Fifo (Active);
+      end if;
+    else
+      Active := Socket_Is_Active;
+      Soc_Send (Soc, Message_To_Put, Str'Length);
     end if;
   exception
     when Fifos.In_Overflow =>
       Mcd_Fifos.Close (Client_Id);
       Open_Fifo (Active);
+    when Socket.Soc_Would_Block =>
+      Activate_Socket (False);
+      Soc.Close;
+      Open_Socket (Active);
   end Send_Message;
 
   -- Put or send string
@@ -235,31 +265,36 @@ package body Io_Flow is
     use type Mcd_Fifos.Fifo_Id;
   begin
     case Io_Mode is
+      when Unknown =>
+        null;
       when Stdio_Tty | Stdio_Not_Tty =>
         -- Put on stdout (tty or not)
         Async_Stdin.Put_Out (Str);
-      when Fifo =>
-        if Client_Id /= Mcd_Fifos.No_Fifo then
-          -- Send on fifo several messages
-          F := Str'First;
-          loop
-            L := F + Io_Data.Max_Message_Len - 1;
-            if L > Str'Last then
-              L := Str'Last;
-            end if;
-            if Debug.Debug_Level_Array(Debug.Flow) then
-              Async_Stdin.Put_Line_Err ("Flow: Sending -> " & Str(F .. L));
-            end if;
-            Send_Message (Str(F .. L));
-            if Debug.Debug_Level_Array(Debug.Flow) then
-              Async_Stdin.Put_Line_Err ("Flow: Sent -> " & Str(F .. L));
-            end if;
-            exit when L = Str'Last;
-            F := L + 1;
-          end loop;
+      when Udp | Tcp | Fifo =>
+        -- Skip if channel is not active
+        if Io_Mode = Fifo and then Client_Id /= Mcd_Fifos.No_Fifo then
+          return;
+        elsif (Io_Mode = Udp or else Io_Mode = Tcp)
+        and then not Soc.Is_Open then
+          return;
         end if;
-      when others =>
-        null;
+        -- Send on Socket/Fifo several messages
+        F := Str'First;
+        loop
+          L := F + Io_Data.Max_Message_Len - 1;
+          if L > Str'Last then
+            L := Str'Last;
+          end if;
+          if Debug.Debug_Level_Array(Debug.Flow) then
+            Async_Stdin.Put_Line_Err ("Flow: Sending -> " & Str(F .. L) & "<");
+          end if;
+          Send_Message (Str(F .. L));
+          if Debug.Debug_Level_Array(Debug.Flow) then
+            Async_Stdin.Put_Line_Err ("Flow: Sent -> " & Str(F .. L) & "<");
+          end if;
+          exit when L = Str'Last;
+          F := L + 1;
+        end loop;
     end case;
   end Put;
 
@@ -278,6 +313,7 @@ package body Io_Flow is
   procedure Close is
     use type Mcd_Fifos.Fifo_Id;
   begin
+    Closing := True;
     case Io_Mode is
       when Stdio_Tty =>
         -- Reset tty blocking
@@ -289,7 +325,6 @@ package body Io_Flow is
         if Debug.Debug_Level_Array(Debug.Flow) then
           Async_Stdin.Put_Line_Err ("Flow: Closing fifo");
         end if;
-        Closing := True;
         if Client_Id /= Mcd_Fifos.No_Fifo then
           Mcd_Fifos.Close (Client_Id);
         end if;
@@ -299,17 +334,27 @@ package body Io_Flow is
         if Debug.Debug_Level_Array(Debug.Flow) then
           Async_Stdin.Put_Line_Err ("Flow: Closed fifo");
         end if;
+      when Udp =>
+        if Debug.Debug_Level_Array(Debug.Flow) then
+          Async_Stdin.Put_Line_Err ("Flow: Closing udp");
+        end if;
+        if Soc.Is_Open then
+          if Socket_Is_Active then
+            Activate_Socket (False);
+          end if;
+          Soc.Close;
+        end if;
       when others =>
         null;
     end case;
   end Close;
 
   ----------------------------------------------------
-  -- Fifo callbacks
+  -- Fifo operations
   ----------------------------------------------------
-  procedure Conn_Cb (Fifo_Name : in String;
-                     Id        : in Mcd_Fifos.Fifo_Id;
-                     Connected : in Boolean) is
+  procedure Fifo_Conn_Cb (Fifo_Name : in String;
+                          Id        : in Mcd_Fifos.Fifo_Id;
+                          Connected : in Boolean) is
     pragma Unreferenced (Fifo_Name);
     Active : Boolean;
   begin
@@ -331,27 +376,31 @@ package body Io_Flow is
         Open_Fifo (Active);
       end if;
     end if;
-  end Conn_Cb;
+  end Fifo_Conn_Cb;
 
-  procedure Rece_Cb (Id      : in Mcd_Fifos.Fifo_Id;
-                     Message : in Io_Data.Message_Type;
-                     Length  : in Fifos.Message_Length) is
+  procedure Fifo_Rece_Cb (Id      : in Mcd_Fifos.Fifo_Id;
+                          Message : in Io_Data.Message_Type;
+                          Length  : in Fifos.Message_Length) is
     pragma Unreferenced (Id);
   begin
     if Length = 0 then
       return;
     end if;
     -- Add this chunk
-    Asu.Append (Fifo_Data, Message(1 .. Length));
+    Asu.Append (Tmp_Data, Message(1 .. Length));
     if      Message(Length) = Ada.Characters.Latin_1.Cr
     or else Message(Length) = Ada.Characters.Latin_1.Lf then
       -- Validate the overall string
-      Input_Data := Fifo_Data;
-      Fifo_Data := Asu_Null;
+      Input_Data := Tmp_Data;
+      Tmp_Data := Asu_Null;
       -- Freeze fifo to prevent Input_Data to be overwritten
       Mcd_Fifos.Activate (Client_Id, False);
+      if Debug.Debug_Level_Array(Debug.Flow) then
+        Async_Stdin.Put_Line_Err ("Flow: Fifo_Rece_Cb set >"
+                             & Asu_Ts (Input_Data) & "<");
+      end if;
     end if;
-  end Rece_Cb;
+  end Fifo_Rece_Cb;
 
   procedure Open_Fifo (Active : in Boolean) is
   begin
@@ -360,8 +409,8 @@ package body Io_Flow is
     end if;
     Acc_Id := Mcd_Fifos.Open (Asu_Ts (Fifo_Name),
                               False,
-                              Conn_Cb'Access,
-                              Rece_Cb'Access,
+                              Fifo_Conn_Cb'Access,
+                              Fifo_Rece_Cb'Access,
                               null);
     if not Active then
       Mcd_Fifos.Activate (Acc_Id, False);
@@ -395,11 +444,77 @@ package body Io_Flow is
     Async_Stdin.Activate (False);
     if Debug.Debug_Level_Array(Debug.Flow) then
       Async_Stdin.Put_Line_Err ("Flow: Stdin_Cb set >"
-                           & Asu_Ts (Input_Data)
-                           & "<");
+                           & Asu_Ts (Input_Data) & "<");
     end if;
     return True;
   end Stdin_Cb;
+
+  ----------------------------------------------------
+  -- Socket operations
+  ----------------------------------------------------
+  function Socket_Rece_Cb (Fd : in Event_Mng.File_Desc; Read : in Boolean)
+                          return Boolean is
+    pragma Unreferenced (Fd, Read);
+    Message : Io_Data.Message_Type;
+    Length : Natural;
+  begin
+    Soc_Receive (Soc, Message, Length, Io_Mode = Udp, False);
+    if Length = 0 then
+      return False;
+    end if;
+    -- Add this chunk
+    Asu.Append (Tmp_Data, Message(1 .. Length));
+    if      Message(Length) = Ada.Characters.Latin_1.Cr
+    or else Message(Length) = Ada.Characters.Latin_1.Lf then
+      -- Validate the overall string
+      Input_Data := Tmp_Data;
+      Tmp_Data := Asu_Null;
+      -- Freeze fifo to prevent Input_Data to be overwritten
+      Activate_Socket (False);
+      if Debug.Debug_Level_Array(Debug.Flow) then
+        Async_Stdin.Put_Line_Err ("Flow: Socket_Rece_Cb set >"
+                             & Asu_Ts (Input_Data) & "<");
+      end if;
+      return True;
+    else
+      return False;
+    end if;
+  end Socket_Rece_Cb;
+
+  function Socket_Is_Active return Boolean is
+  begin
+    return Soc.Is_Open and then Event_Mng.Fd_Callback_Set (Soc.Get_Fd, True);
+  end Socket_Is_Active;
+
+  procedure Activate_Socket (Active : in Boolean) is
+  begin
+    if Active = Socket_Is_Active then
+      return;
+    end if;
+    if Active then
+      Event_Mng.Add_Fd_Callback (Soc.Get_Fd, True, Socket_Rece_Cb'Access);
+    else
+      Event_Mng.Del_Fd_Callback (Soc.Get_Fd, True);
+    end if;
+  end Activate_Socket;
+
+  procedure Open_Socket (And_Activate : in Boolean) is
+    use type Tcp_Util.Remote_Host_List, Tcp_Util.Remote_Port_List;
+  begin
+    Soc.Open (Socket.Udp);
+    Is_Ipm := False;
+    if Host.Kind = Tcp_Util.Host_Id_Spec
+    or else Host.Name(Host.Name'First) /= ' ' then
+      -- An address specified => Ipm
+      -- Use Set_Dest to indicate Imp address
+      Socket_Util.Set_Destination (Soc, True, Host, Port);
+      Is_Ipm := True;
+    end if;
+    Socket_Util.Link (Soc, Port);
+    if And_Activate then
+      Activate_Socket (True);
+    end if;
+  end Open_Socket;
 
 end Io_Flow;
 
