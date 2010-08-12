377aeabe3e717943bd000f5974c366def22cea44
Pascal MALAISE <malaise@telemaque>
2010-08-12 19:23:17 +0200

    General set_dest and link

---
+++ reposit/socket_util.adb
@@ -0,0 +1,73 @@
+with String_Mng;
+package body Socket_Util is
+
+  -- Set destination host/lan name or Ip address, and port name or num
+  -- Lan is significant only of Host.Kind is Tcp_Util.Host_Name_Spec
+  procedure Set_Destination (Soc  : in Socket_Dscr;
+                             Lan  : in Boolean;
+                             Host : in Tcp_Util.Remote_Host;
+                             Port : in Tcp_Util.Remote_Port) is
+  begin
+    case Host.Kind is
+      when Tcp_Util.Host_Name_Spec =>
+        case Port.Kind is
+          when Tcp_Util.Port_Name_Spec =>
+            Soc.Set_Destination_Name_And_Service (Lan,
+                  String_Mng.Strip (Host.Name), String_Mng.Strip (Port.Name));
+          when Tcp_Util.Port_Num_Spec =>
+            Soc.Set_Destination_Name_And_Port (Lan,
+                  String_Mng.Strip (Host.Name), Port.Num);
+        end case;
+      when Tcp_Util.Host_Id_Spec =>
+        case Port.Kind is
+          when Tcp_Util.Port_Name_Spec =>
+            Soc.Set_Destination_Host_And_Service (
+                   Host.Id, String_Mng.Strip (Port.Name));
+          when Tcp_Util.Port_Num_Spec =>
+            Soc.Set_Destination_Host_And_Port (
+                   Host.Id, Port.Num);
+        end case;
+    end case;
+  end Set_Destination;
+
+  -- Change destination host/lan name or Ip address
+  -- Lan is significant only of Host.Kind is Tcp_Util.Host_Name_Spec
+  procedure Change_Destination (Soc  : in Socket_Dscr;
+                                Lan  : in Boolean;
+                                Host : in Tcp_Util.Remote_Host) is
+  begin
+    case Host.Kind is
+      when Tcp_Util.Host_Name_Spec =>
+        Soc.Change_Destination_Name (Lan, String_Mng.Strip (Host.Name));
+      when Tcp_Util.Host_Id_Spec =>
+        Soc.Change_Destination_Host (Host.Id);
+    end case;
+  end Change_Destination;
+
+  -- Change destination port name or num
+  procedure Change_Destination (Soc  : in Socket_Dscr;
+                                Port : in Tcp_Util.Remote_Port) is
+  begin
+    case Port.Kind is
+      when Tcp_Util.Port_Name_Spec =>
+        Soc.Change_Destination_Service (String_Mng.Strip (Port.Name));
+      when Tcp_Util.Port_Num_Spec =>
+        Soc.Change_Destination_Port (Port.Num);
+    end case;
+  end Change_Destination;
+
+  -- Link to a port name or num
+  procedure Link (Soc  : in Socket_Dscr;
+                  Port : in Tcp_Util.Remote_Port) is
+  begin
+    case Port.Kind is
+      when Tcp_Util.Port_Name_Spec =>
+        Soc.Link_Service (String_Mng.Strip (Port.Name));
+      when Tcp_Util.Port_Num_Spec =>
+        Soc.Link_Port (Port.Num);
+    end case;
+
+  end Link;
+
+end Socket_Util;
+
---
+++ reposit/socket_util.ads
@@ -0,0 +1,28 @@
+with Socket, Tcp_Util;
+package Socket_Util is
+
+  subtype Socket_Dscr is Socket.Socket_Dscr;
+
+  -- Set destination host/lan name or Ip address, and port name or num
+  -- Lan is significant only of Host.Kind is Tcp_Util.Host_Name_Spec
+  procedure Set_Destination (Soc  : in Socket_Dscr;
+                             Lan  : in Boolean;
+                             Host : in Tcp_Util.Remote_Host;
+                             Port : in Tcp_Util.Remote_Port);
+
+  -- Change destination host/lan name or Ip address
+  -- Lan is significant only of Host.Kind is Tcp_Util.Host_Name_Spec
+  procedure Change_Destination (Soc  : in Socket_Dscr;
+                                Lan  : in Boolean;
+                                Host : in Tcp_Util.Remote_Host);
+
+  -- Change destination port name or num
+  procedure Change_Destination (Soc  : in Socket_Dscr;
+                                Port : in Tcp_Util.Remote_Port);
+
+  -- Link to a port name or num
+  procedure Link (Soc  : in Socket_Dscr;
+                  Port : in Tcp_Util.Remote_Port);
+
+end Socket_Util;
+
