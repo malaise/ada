dfd7ee36c9a355f529eb2dca8252ceb6840a9fe9
Pascal MALAISE <malaise@telemaque>
2010-08-28 17:45:46 +0200

    Extract senetences from an input flow of text

---
+++ reposit/input_buffer.adb
@@ -0,0 +1,121 @@
+-- Bufferize input flow (strings) until separator is found
+-- Report the received string (text between separators)
+with Ada.Unchecked_Deallocation;
+with String_Mng;
+package body Input_Buffer is
+
+  -- Initialise the buffer Buf with the sentence delimiter and the notifier
+  procedure Set (Buf : in out Buffer;
+                 Notifier : in Sentence_Notifier;
+                 Delimiter : in String := Line_Feed) is
+  begin
+    if Notifier = null or else Delimiter = "" then
+      raise Constraint_Error;
+    end if;
+    if Buf.Acc = null then
+      Buf.Acc := new Buffer_Rec;
+    end if;
+    Buf.Acc.Notif := Notifier;
+    Buf.Acc.Delim := Asu_Tus (Delimiter);
+    Buf.Acc.Text := Asu_Null;
+    Buf.Acc.Susp := False;
+  end Set;
+
+  -- INTERNAL: Raise Status_Error if Buf is not set
+  procedure Check_Set (Buf : in Buffer) is
+  begin
+    if not Is_Set (Buf) then
+      raise Status_Error;
+    end if;
+  end Check_Set;
+
+  -- INTERNAL: Invoque the notifier if Buf is not suspended
+  procedure Flush (Buf : in Buffer) is
+    Ind : Natural;
+    Head : Asu_Us;
+  begin
+    if Buf.Acc.Susp then
+      return;
+    end if;
+    -- Several notifs
+    loop
+      Ind := String_Mng.Locate (Asu_Ts (Buf.Acc.Text), Asu_Ts (Buf.Acc.Delim));
+      exit when Ind = 0;
+      -- Move to end of sentence (end of delim)
+      Ind := Ind + Asu.Length (Buf.Acc.Delim) - 1;
+      Head := Asu.Unbounded_Slice (Buf.Acc.Text, 1, Ind);
+      -- Del head
+      Asu.Delete (Buf.Acc.Text, 1, Ind);
+      -- Finally notify (after the delete so that exception in Notifier
+      --  don't lead to infinite loop if ignored by push/resume.)
+      Buf.Acc.Notif (Asu_Ts (Head));
+    end loop;
+  end Flush;
+
+  -- Push text in buffer Buf
+  -- Can lead the notifier to be invoqued once or several times
+  procedure Push (Buf : in Buffer; Text : in String) is
+  begin
+    Check_Set (Buf);
+    Asu.Append (Buf.Acc.Text, Text);
+    Flush (Buf);
+  end Push;
+
+  -- Get the tail of the buffer Buf (text not ending with Delimiter)
+  function Tail (Buf : Buffer) return String is
+  begin
+    Check_Set (Buf);
+    return Asu_Ts (Buf.Acc.Text);
+  end Tail;
+
+  -- Suspend the buffer Buf
+  procedure Suspend (Buf : in Buffer) is
+  begin
+    Check_Set (Buf);
+    Buf.Acc.Susp := True;
+  end Suspend;
+
+  -- Resume the buffer Buf
+  procedure Resume (Buf : in Buffer) is
+  begin
+    Check_Set (Buf);
+    Buf.Acc.Susp := False;
+    Flush (Buf);
+  end Resume;
+
+  -- Is the buffer Buf suspended?
+  function Is_Suspended (Buf : Buffer) return Boolean is
+  begin
+    Check_Set (Buf);
+    return Buf.Acc.Susp;
+  end Is_Suspended;
+
+  -- Is the buffer Buf set?
+  function Is_Set (Buf : Buffer) return Boolean is
+  begin
+    return Buf.Acc /= null and then Buf.Acc.Notif /= null;
+  end Is_Set;
+
+  -- Copy the buffer Src_Buf to Dest_Buf
+  procedure Copy (Dest_Buf : in out Buffer; Src_Buf : in Buffer) is
+  begin
+    Finalize (Dest_Buf);
+    if Is_Set (Src_Buf) then
+      Dest_Buf.Acc := new Buffer_Rec'(Src_Buf.Acc.all);
+    end if;
+  end Copy;
+
+
+  --  Free Acc
+  procedure Free is new Ada.Unchecked_Deallocation
+         (Object => Buffer_Rec,
+          Name   => Buffer_Rec_Access);
+  overriding procedure Finalize (Buf : in out Buffer) is
+  begin
+    if Is_Set (Buf) then
+      Free (Buf.Acc);
+    end if;
+  end Finalize;
+
+end Input_Buffer;
+
---
+++ reposit/input_buffer.ads
@@ -0,0 +1,95 @@
+-- Bufferize input flow (strings) until separator is found
+-- Report the received string (text between separators)
+-- This is the same as Text_Line input except that the input flow is ourself
+with Ada.Finalization;
+with As.U; use As.U;
+with Text_Line;
+package Input_Buffer is
+
+  -------------------------
+  -- GENERAL DEFINITIONS --
+  -------------------------
+  -- Default separator
+  Line_Feed : constant String := Text_Line.Line_Feed_Str;
+
+  -- Callback invoqued when a sentence has been identified in input flow
+  --  (text ending by separator)
+  type Sentence_Notifier is access procedure (Sentence : in String);
+
+  -- A buffer
+  type Buffer is tagged limited private;
+
+  ----------------------
+  -- BASIC OPERATIONS --
+  ----------------------
+  -- Initialise the buffer Buf with the sentence delimiter and the notifier
+  -- Previous content of Buf is overwritten
+  -- May raise Constraint_Error if Delimiter is empty or Notifier is null
+  procedure Set (Buf : in out Buffer;
+                 Notifier : in Sentence_Notifier;
+                 Delimiter : in String := Line_Feed);
+
+  -- Push text in buffer Buf
+  -- Can lead the notifier to be invoqued once or several times
+  -- May raise Status_Error if Buf is not set
+  procedure Push (Buf : in Buffer; Text : in String);
+
+  ----------------------
+  -- OTHER OPERATIONS --
+  ----------------------
+  -- Get the tail of the buffer Buf (text not ending with Delimiter)
+  -- Beware that there may be Delimiters in the tail if the buffer is
+  --  suspended
+  -- May raise Status_Error if Buf is not set
+  function Tail (Buf : Buffer) return String;
+
+  -- Suspend the buffer Buf
+  -- It will store the text push but not notify
+  -- No effect if already suspended
+  -- May raise Status_Error if Buf is not set
+  procedure Suspend (Buf : in Buffer);
+
+  -- Resume the buffer Buf
+  -- Can lead the notifier to be invoqued once or several times
+  -- No effect if not suspended
+  -- May raise Status_Error if Buf is not set
+  procedure Resume (Buf : in Buffer);
+
+  -- Is the buffer Buf suspended?
+  -- May raise Status_Error if Buf is not set
+  function Is_Suspended (Buf : Buffer) return Boolean;
+
+  ---------------
+  -- UTILITIES --
+  ---------------
+  -- Is the buffer Buf set?
+  function Is_Set (Buf : Buffer) return Boolean;
+
+  -- Copy the buffer Src_Buf to Dest_Buf
+  -- Previous content of Dest_Buf is overwritten
+  -- Src_Buf may not be set, which deletes Dest_Buf
+  procedure Copy (Dest_Buf : in out Buffer; Src_Buf : in Buffer);
+
+  Status_Error : exception;
+private
+
+  type Buffer_Rec is record
+    Notif : Sentence_Notifier := null;
+    Delim : Asu_Us;
+    Text : Asu_Us;
+    Susp : Boolean := False;
+  end record;
+
+  Default_Rec : Buffer_Rec;
+  Init_Rec : constant Buffer_Rec := Default_Rec;
+
+  type Buffer_Rec_Access is access Buffer_Rec;
+  type Buffer is limited new Ada.Finalization.Limited_Controlled with record
+    Acc : Buffer_Rec_Access := null;
+  end record;
+
+   --  Free Acc
+  overriding procedure Finalize (Buf : in out Buffer);
+
+end Input_Buffer;
+
