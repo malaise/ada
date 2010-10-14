255b43466e74aa965def7231b0032fc5f99eca51
Pascal MALAISE <malaise@telemaque>
2010-10-14 08:20:23 +0200

    Re-organize variables

--- usr/tcpchat/variables.adb
+++ usr/tcpchat/variables.adb
@@ -1,6 +1,8 @@
-with Computer, Environ;
+with Computer, Environ, Regular_Expressions;
 with Error;
 package body Variables is
+
+  Ext_Ref : constant Character := '$';
 
   -- Dummy variable resolver for check
   function Dummy (Name : String) return String is
@@ -11,11 +13,26 @@ package body Variables is
   -- Real ENV variable resolver for expansion
   function Getenv (Name : String) return String is
   begin
-    return Environ.Getenv_If_Set (Name);
-  exception
-    when Environ.Name_Error =>
-      Error ("ENV variable " & Name & " not found");
+    -- Name must start with '$'
+    if Name = "" then
+      Error ("Empty ENV variable");
       raise Expand_Error;
+    end if;
+    if Name(Name'First) /= Ext_Ref then
+      Error ("Internal variable " & Name & " not found");
+      raise Expand_Error;
+    end if;
+    -- Get ENV variable
+    declare
+      Str : constant String
+          := Name (Integer'Succ(Name'First) .. Name'Last);
+    begin
+      return Environ.Getenv_If_Set (Str);
+    exception
+      when Environ.Name_Error =>
+        Error ("ENV variable " & Str & " not found");
+        raise Expand_Error;
+    end;
   end Getenv;
 
   -- Reset all variables
@@ -27,20 +44,34 @@ package body Variables is
   -- Set a variable
   procedure Set (Name, Value : in Asu_Us) is
   begin
+    -- Must not be empty, start by '$' or be a number
+    if Asu_Is_Null (Name)
+    or else Asu.Element (Name, 1) = Ext_Ref
+    or else Regular_Expressions.Match ("[0-9]+", Asu_Ts (Name), True) then
+      Error ("Invalid variable name" & Asu_Ts (Name));
+      raise Invalid_Name;
+    end if;
     Computer.Set (Asu_Ts (Name), Asu_Ts (Value),
                   Modifiable => True, Persistent => True);
   exception
     when Computer.Invalid_Variable =>
+      Error ("Invalid variable name" & Asu_Ts (Name));
       raise Invalid_Name;
   end Set;
 
   -- Set a volatile variable : Clean all volatile variables
   procedure Set_Volatile (Name, Value : in Asu_Us) is
-begin
+  begin
+    -- Must be a number
+    if not Regular_Expressions.Match ("[0-9]+", Asu_Ts (Name), True) then
+      Error ("Invalid volatile variable name" & Asu_Ts (Name));
+      raise Invalid_Name;
+    end if;
     Computer.Set (Asu_Ts (Name), Asu_Ts (Value),
                   Modifiable => True, Persistent => False);
   exception
     when Computer.Invalid_Variable =>
+      Error ("Invalid volatile variable name" & Asu_Ts (Name));
       raise Invalid_Name;
   end Set_Volatile;
 
