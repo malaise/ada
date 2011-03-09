f1b01e354b62c4ed99039284795753eee5758c80
Pascal MALAISE <malaise@magic.fr>
2011-03-09 18:59:09 +0100

    Use Text_Line

--- usr/code/t_vigenere.adb
+++ usr/code/t_vigenere.adb
@@ -1,19 +1,20 @@
-with Ada.Text_Io;
-with Argument;
+with Argument, Basic_Proc, Text_Line, Sys_Calls, As.U;
 with Vigenere;
-
 procedure T_Vigenere is
 
   Code : Boolean;
+  Flow : Text_Line.File_Type;
+  Str : As.U.Asu_Us;
 
   procedure Usage is
   begin
-    Ada.Text_Io.Put_Line ("Usage: " & Argument.Get_Program_Name
-                        & "  -c | -d     <key> <text>");
+    Basic_Proc.Put_Line_Output ("Usage: " & Argument.Get_Program_Name
+                        & "  -c | -d     <key>");
   end;
 
 begin
-  if Argument.Get_Nbre_Arg /= 3 then
+  -- Check and get mode
+  if Argument.Get_Nbre_Arg /= 2 then
     Usage;
     return;
   end if;
@@ -26,22 +27,38 @@ begin
     return;
   end if;
 
+  -- Open input flow
+  Flow.Open (Text_Line.In_File, Sys_Calls.Stdin);
+
+  -- Get input text
+  loop
+    declare
+       Line : constant As.U.Asu_Us := Flow.Get;
+    begin
+      exit when Line.Is_Null;
+      Str.Append (Line);
+    end;
+  end loop;
+  Flow.Close;
+  if Str.Is_Null then
+    return;
+  end if;
+
   declare
-    Str : String := Argument.Get_Parameter (Occurence => 3);
-    Lstr : Vigenere.Long_String (1 .. Str'Length);
+    Lstr : Vigenere.Long_String(1 .. Vigenere.Long_Positive (Str.Length));
   begin
-    for I in Str'Range loop
-      Lstr(Vigenere.Long_Positive(I)) := Str(I);
+    for I in 1 .. Str.Length loop
+      Lstr(Vigenere.Long_Positive(I)) := Str.Element(I);
     end loop;
     if Code then
       Vigenere.Encode (Argument.Get_Parameter(Occurence => 2), Lstr);
     else
       Vigenere.Decode (Argument.Get_Parameter(Occurence => 2), Lstr);
     end if;
-    for I in Str'Range loop
-      Str(I) := Lstr(Vigenere.Long_Positive(I));
+    for I in 1 .. Str.Length loop
+      Str.Replace_Element(I, Lstr(Vigenere.Long_Positive(I)));
     end loop;
-    Ada.Text_Io.Put (Str);
+    Basic_Proc.Put_Output (Str.Image);
   end;
 
 end T_Vigenere;
