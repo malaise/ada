646ba3fdb3e16272960b3bbfeefc821d5746c4e4
Pascal MALAISE <malaise@telemaque>
2011-07-04 19:19:46 +0200

    Improve help on Regmatch

--- usr/mcd/mcd_parser.adb
+++ usr/mcd/mcd_parser.adb
@@ -178,7 +178,7 @@ package body Mcd_Parser is
    Strprog  => (Nosy, "push A converted to program   ", False),
    Strof    => (Nosy, "push formated string of A     ", False),
    Normal   => (Nosy, "push normalised string of A   ", False),
-   Regmatch => (Nosy, "push if B matches regex A     ", True),
+   Regmatch => (Nosy, "push where B matches regex A  ", True),
 
    Clock    => (Nosy, "push current time             ", False),
    Dateof   => (Nosy, "int -> YYyy/mm/dd-hh:mm:ss.mmm", False),
