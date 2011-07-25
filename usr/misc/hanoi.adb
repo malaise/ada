b0992ffd1d1ffb237bb37def440cb56996e205d0
Pascal MALAISE <malaise@telemaque>
2011-07-25 20:15:24 +0200

    Improve comments and translate to english

--- usr/misc/hanoi.adb
+++ usr/misc/hanoi.adb
@@ -1,35 +1,33 @@
 with Ada.Text_Io, Ada.Calendar;
 with My_Io; use My_Io;
--- Simulation du jeu des tours de hanoi
+-- Simulation of the game of Hanoi towers
 
 procedure Hanoi is
 
   type Typ_Support is (A, B, C);
-  Origine, Destination : Typ_Support;
-  package Support_Io is
-    new Ada.Text_Io.Enumeration_Io(Typ_Support);
-  Supports_Identiques : exception;
+  Origin, Destination : Typ_Support;
+  package Support_Io is new Ada.Text_Io.Enumeration_Io(Typ_Support);
+  Same_Supports : exception;
 
-  subtype Typ_Numero_De_Disque is Positive;
-  Nombre_De_Disques      : Typ_Numero_De_Disque;
+  subtype Typ_No_Disk is Positive;
+  Nb_Disks : Typ_No_Disk;
 
-  Nombre_De_Deplacements : Long_Long_Integer;
-  Compteur_Overflow      : exception;
+  Nb_Moves : Long_Long_Integer;
+  Overflow_Count : exception;
 
-  Reponse                : Character;
-  Trace                  : Boolean;
+  Response : Character;
+  Trace : Boolean;
 
-  Temps                  : Ada.Calendar.Time;
-  Seconde                : Ada.Calendar.Day_Duration;
-  package Duration_Io is
-    new Ada.Text_Io.Fixed_Io(Duration);
+  Time : Ada.Calendar.Time;
+  Seconds : Ada.Calendar.Day_Duration;
+  package Duration_Io is new Ada.Text_Io.Fixed_Io(Duration);
 
-  function Determine_Intermediaire (
-   Premier_Support, Deuxieme_Support : Typ_Support) return Typ_Support is
+  function Find_Third (First_Suport, Second_Support : Typ_Support)
+           return Typ_Support is
   begin
-    case Premier_Support is
+    case First_Suport is
       when A =>
-        case Deuxieme_Support is
+        case Second_Support is
           when B =>
             return C;
           when C =>
@@ -38,7 +36,7 @@ procedure Hanoi is
             null;
         end case;
       when B =>
-        case Deuxieme_Support is
+        case Second_Support is
           when C =>
             return A;
           when A =>
@@ -47,7 +45,7 @@ procedure Hanoi is
             null;
         end case;
       when C =>
-        case Deuxieme_Support is
+        case Second_Support is
           when A =>
             return B;
           when B =>
@@ -56,145 +54,142 @@ procedure Hanoi is
             null;
         end case;
     end case;
-    Put_Line("PROBLEME: Deux supports identiques.");
-    raise Supports_Identiques;
-  end Determine_Intermediaire;
+    Put_Line("PROBLEM: Two axes are identical.");
+    raise Same_Supports;
+  end Find_Third;
 
-  procedure Deplacer (
-   Origine, Intermediaire, Destination : in Typ_Support;
-   Numero_De_Disque                    : in Typ_Numero_De_Disque) is
+  procedure Move (
+   Origin, Third, Destination : in Typ_Support;
+   No_Disk : in Typ_No_Disk) is
   begin
     begin
-      Deplacer(Origine, Destination, Intermediaire, Typ_Numero_De_Disque'Pred(
-        Numero_De_Disque));
+      Move(Origin, Destination, Third, Typ_No_Disk'Pred(No_Disk));
     exception
       when Constraint_Error =>
         null;
     end;
 
     if Trace then
-      Put("Deplacer le disque ");
-      Put(Numero_De_Disque);
-      Put(" de ");
-      Support_Io.Put(Origine);
-      Put(" vers ");
+      Put("Move the disk ");
+      Put(No_Disk);
+      Put(" from ");
+      Support_Io.Put(Origin);
+      Put(" to ");
       Support_Io.Put(Destination);
       Put_Line(".");
     end if;
 
     begin
-      Nombre_De_Deplacements := Long_Long_Integer'Succ(Nombre_De_Deplacements);
+      Nb_Moves := Long_Long_Integer'Succ(Nb_Moves);
     exception
       when others =>
-        raise Compteur_Overflow;
+        raise Overflow_Count;
     end;
 
     begin
-      Deplacer(Intermediaire, Origine, Destination, Typ_Numero_De_Disque'Pred(
-        Numero_De_Disque));
+      Move(Third, Origin, Destination, Typ_No_Disk'Pred(No_Disk));
     exception
       when Constraint_Error =>
         null;
     end;
 
-  end Deplacer;
+  end Move;
 
-begin -- hanoi
-  Put_Line("Probleme des tours de HANOI:");
+begin -- Hanoi
+  Put_Line("Problem of the Hanoi towers:");
   loop
     New_Line;
 
     loop
       begin
-        Put("Entrez le support de depart (a, b, c) ? ");
-        Support_Io.Get(Origine);
+        Put("Enter the origin axis (a, b, c) ? ");
+        Support_Io.Get(Origin);
         New_Line;
         exit;
       exception
         when others =>
           Skip_Line;
-          Put_Line("ERREUR de saisie, recommencez.");
+          Put_Line("ERROR of input, try again.");
       end;
     end loop;
 
     loop
       begin
-        Put("Entrez le support d'arrivee (a, b, c) ? ");
+        Put("Enter the destination axis (a, b, c) ? ");
         Support_Io.Get(Destination);
-        if (Destination = Origine) then
-          raise Supports_Identiques;
+        if (Destination = Origin) then
+          raise Same_Supports;
         end if;
         New_Line;
         exit;
       exception
-        when Supports_Identiques =>
+        when Same_Supports =>
           Skip_Line;
-          Put_Line("ERREUR, les deux supports sont identiques. Recommencez.");
+          Put_Line("ERROR, both axes are equal, try again.");
         when others =>
           Skip_Line;
-          Put_Line("ERREUR de saisie, recommencez.");
+          Put_Line("ERREUR of input, try again.");
       end;
     end loop;
 
     loop
       begin
-        Put("Entrez le nombre de disques (au moins 1) ? ");
-        Get(Nombre_De_Disques);
+        Put("Enter the number of disks (at least 1) ? ");
+        Get(Nb_Disks);
         New_Line;
         exit;
       exception
         when others =>
           Skip_Line;
-          Put_Line("ERREUR, recommencez.");
+          Put_Line("ERROR, try again.");
       end;
     end loop;
 
     loop
       begin
-        Put("Voulez-vous une trace detaillee (O/N) ? ");
-        Get(Reponse);
-        if (Reponse /= 'o') and then (Reponse /= 'O') and then
-           (Reponse /= 'n') and then (Reponse /= 'N') then
+        Put("Do you want a detailed trace (Y/N) ? ");
+        Get(Response);
+        if (Response /= 'y') and then (Response /= 'Y') and then
+           (Response /= 'n') and then (Response /= 'N') then
           raise Constraint_Error;
         end if;
         New_Line;
-        Trace := ((Reponse = 'o') or else (Reponse = 'O'));
+        Trace := ((Response = 'y') or else (Response = 'Y'));
         exit;
       exception
         when others =>
           Skip_Line;
-          Put_Line("ERREUR, recommencez.");
+          Put_Line("ERROR, try again.");
       end;
     end loop;
 
     New_Line;
 
     begin
-      Nombre_De_Deplacements := 0;
-      Temps := Ada.Calendar.Clock;
-      Deplacer(Origine, Determine_Intermediaire(Origine, Destination),
-        Destination, Nombre_De_Disques);
-      Seconde := Ada.Calendar."-"(Ada.Calendar.Clock, Temps);
+      Nb_Moves := 0;
+      Time := Ada.Calendar.Clock;
+      Move (Origin, Find_Third(Origin, Destination), Destination, Nb_Disks);
+      Seconds := Ada.Calendar."-"(Ada.Calendar.Clock, Time);
 
       New_Line;
-      Put("Transfert de ");
-      Put(Nombre_De_Disques);
-      Put(" disques de ");
-      Support_Io.Put(Origine);
-      Put(" vers ");
+      Put("Transfer from ");
+      Put(Nb_Disks);
+      Put(" disks from ");
+      Support_Io.Put(Origin);
+      Put(" to ");
       Support_Io.Put(Destination);
-      Put_Line(" .");
+      Put_Line(".");
 
-      Put("Operation effectuee en ");
-      Put(Nombre_De_Deplacements);
-      Put_Line(" deplacements ");
-      Put(" et en ");
-      Duration_Io.Put(Seconde);
-      Put_Line(" secondes.");
+      Put("Operation done in ");
+      Put(Nb_Moves);
+      Put_Line(" moves ");
+      Put(" and in ");
+      Duration_Io.Put(Seconds);
+      Put_Line(" seconds.");
 
     exception
-      when Compteur_Overflow =>
-        Put("Depassement de la capacite du compteur. Mettez moins de disques.")
+      when Overflow_Count =>
+        Put("Overflow of the counter of movements. try with less disks.")
           ;
     end;
 
