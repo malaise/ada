22ac53ff161a969d5be191a4d4204360bd6304bb
Pascal MALAISE <malaise@telemaque>
2011-11-15 19:30:18 +0100

    Use Async Get to allow line edition

--- usr/mmind/mmind_asc.adb
+++ usr/mmind/mmind_asc.adb
@@ -1,12 +1,11 @@
-with Basic_Proc, Normal, Rnd;
-
+with Ada.Characters.Latin_1;
+with Normal, Rnd, Async_Stdin;
 with Common, Response;
-
 procedure Mmind_Asc is
 begin
   Common.Set_Level_To_Stored;
 
-  Basic_Proc.Put_Line_Output ("Find "
+  Async_Stdin.Put_Line_Out ("Find "
     & Normal(Integer(Common.Get_Stored_Level), 1)
     & " digits from 1 to 8.");
 
@@ -14,9 +13,7 @@ begin
   Response.New_Code;
 
   declare
-    Buffer : String(1 .. 80);
-    Str : String(1 .. Integer(Common.Get_Stored_Level));
-    Len : Natural;
+    Len : constant Integer := Integer(Common.Get_Stored_Level);
     Prop : Response.Color_Rec(Common.Get_Stored_Level);
     Rep : Response.Response_Rec;
   begin
@@ -26,13 +23,17 @@ begin
 
       Get:
       loop
+        Async_Stdin.Put_Out (Normal (I, 2) & "? ");
+        Async_Stdin.Overwrite;
+        declare
+          -- Allow up to 80 of input line
+          Str : constant String := Async_Stdin.Get_Line (80, 5);
         begin
-          Basic_Proc.Put_Output(Normal(I, 2) & "? ");
-          Basic_Proc.Get_Line(Buffer, Len);
-          if Len /= Str'Length then
+          if Str'Length /= Len  + 1
+          or else Str(Str'Length) /= Ada.Characters.Latin_1.Lf then
+            -- Validated input line must have 3/4/5 chars
             raise Constraint_Error;
           end if;
-          Str := Buffer(1 .. Len);
           for I in 1 .. Len loop
             Prop.Color(Common.Level_Range(I))
                  := Common.Eff_Color_Range'Value(Str(I) & "");
@@ -45,22 +46,31 @@ begin
       end loop Get;
 
       Rep := Response.Respond(Prop);
-      Str := (others => ' ');
-      Basic_Proc.Put_Output(" -> " & Str & " ");
-      Basic_Proc.Put_Line_Output(Normal(Rep.Placed_Ok,1) & Normal(Rep.Colors_Ok,1));
+      declare
+        Pad : constant String (1 .. Len) := (others => ' ');
+      begin
+        Async_Stdin.Put_Out (" -> " & Pad & " ");
+      end;
+      Async_Stdin.Put_Line_Out (Normal (Rep.Placed_Ok, 1)
+                                 & Normal (Rep.Colors_Ok,1));
 
       exit Play when Rep.Placed_Ok = Len;
 
     end loop Play;
 
     Prop := Response.Get_Code;
-    Basic_Proc.Put_Output(" => ");
+    Async_Stdin.Put_Out (" => ");
     for I in 1 .. Len loop
-      Basic_Proc.Put_Output(Normal(Integer(Prop.Color(Common.Level_Range(I))), 1));
+      Async_Stdin.Put_Out (Normal (Integer(Prop.Color(
+          Common.Level_Range(I))), 1));
     end loop;
-    Basic_Proc.New_Line_Output;
+    Async_Stdin.New_Line_Out;
 
   end;
 
+exception
+  when others =>
+    Async_Stdin.Set_Async;
+    raise;
 end Mmind_Asc;
 
