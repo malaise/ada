1660e3535048a29d3b6260f4fae28fcf9caf93d2
Pascal MALAISE <malaise@telemaque>
2012-02-18 11:02:04 +0100

    Ensure minimum slopes.

--- usr/lem/moon.adb
+++ usr/lem/moon.adb
@@ -15,17 +15,35 @@ package body Moon is
   -- The ground array
   Ground : Ground_Array (Point_Range);
 
+  -- Add Ground(Of) so that
+  --     abs (Ground(Of).Y_Pos - Ground(Ref).Y_Pos) < Y_Min_Delta
+  -- and Y_Ground_Min <= Ground(Of).Y_Pos <= Y_Ground_Max
+  procedure Add_Ground (Of_Index, Ref_Index : in Point_Range) is
+    -- Minimum thickness of ground
+    Y_Ground_Min : constant Space.Y_Range := 2.0;
+    -- Minimum delta between 2 points (before levelling of the landing site)
+    Y_Min_Delta : constant Space.Y_Range := 5.0;
+    Ref_Y : constant Space.Y_Range := Ground(Ref_Index).Y_Pos;
+  begin
+    loop
+      Ground(Of_Index).Y_Pos := Space.Y_Range (Rnd.Float_Random (
+          Float(Space.Y_Range'First+ Y_Ground_Min), Float(Y_Ground_Max)));
+      exit when abs (Ground(Of_Index).Y_Pos - Ref_Y) >= Y_Min_Delta;
+    end loop;
+  end Add_Ground;
+
   -- Init a new moon
   procedure Init is
-    -- Index of landing array
+    -- Normal or hard mode
+    Hard_Level : Boolean;
+    -- Index of landing site
     -- If hard, ground is flat from I-1 to I, else from I-1 to I+1
     Index_Landing : Point_Range;
     -- First and last possible index for landing site (depending on hard)
-    First_Index : constant Point_Range := Point_Range'Succ (Point_Range'First);
-    Last_Index : Point_Range := Point_Range'Pred (Point_Range'Last);
+    -- Then indexes of landing site
+    First_Index : Point_Range := Point_Range'First + 2;
+    Last_Index : Point_Range := Point_Range'Last - 1;
 
-    Y_Ground_Min : constant Space.Y_Range := 2.0;
-    Hard_Level : Boolean;
   begin
     -- Check if Hard_Level (-h argument)
     begin
@@ -41,38 +59,48 @@ package body Moon is
         Hard_Level := False;
     end;
     -- Raise anonymous (un-catchable) exception
-    --  if LEM cannot land between 2 (hard) or 3 (normal) points
+    --  if LEM cannot land between 2 points
     declare
       Delta_Point_Lem_Width_Error : exception;
-      Nb_Consec : Space.X_Range;
     begin
-      if Hard_Level then
-        Nb_Consec := 1.0;
-      else
-        Nb_Consec := 2.0;
-        Last_Index := Last_Index - 1;
-      end if;
       if Delta_Point * Nb_Consec < Lem.Width then
         raise Delta_Point_Lem_Width_Error;
       end if;
     end;
-    -- Set random height for all points
+
+    -- Set X of each point, ensure last point has X = last X_Range
     for I in Point_Range loop
       Ground(I).X_Pos := Space.X_Range (I - 1) * Delta_Point;
-      Ground(I).Y_Pos := Space.Y_Range (Rnd.Float_Random (
-        Float(Space.Y_Range'First + Y_Ground_Min),
-        Float(Y_Ground_Max)));
     end loop;
-    -- Ensure last point has X = last X_Range
     Ground(Point_Range'Last).X_Pos := Space.X_Range'Last;
 
     -- Set index of landing point
-    Index_Landing := Rnd.Int_Random (First_Index, Last_Index);
-    -- Level the landing site. 2 consecutive points if hard, 3 if normal.
-    Ground(Index_Landing - 1).Y_Pos := Ground(Index_Landing).Y_Pos;
     if not Hard_Level then
-      Ground(Index_Landing + 1).Y_Pos := Ground(Index_Landing).Y_Pos;
+      Last_Index := Last_Index - 1;
     end if;
+    Index_Landing := Rnd.Int_Random (First_Index, Last_Index);
+
+    -- Set random landing altitude
+    Ground(Index_Landing).Y_Pos := Space.Y_Range'First;
+    Add_Ground (Index_Landing, Index_Landing);
+
+    -- Level the landing site. 2 consecutive points if hard, 3 if normal.
+    First_Index := Index_Landing - 1;
+    Ground(First_Index).Y_Pos := Ground(Index_Landing).Y_Pos;
+    Last_Index := Index_Landing;
+    if not Hard_Level then
+      Last_Index := Last_Index + 1;
+      Ground(Last_Index).Y_Pos := Ground(Index_Landing).Y_Pos;
+    end if;
+
+    -- Add points around landing array
+    for I in reverse Point_Range'First .. First_Index - 1 loop
+      Add_Ground (I, I + 1);
+    end loop;
+    for I in Last_Index + 1 .. Point_Range'Last loop
+      Add_Ground (I, I - 1);
+    end loop;
+
   end Init;
 
   -- The array of points defining the ground
