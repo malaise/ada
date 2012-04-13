8e777454421e6ad82bdf3f48fb6c64892fa63e30
Pascal MALAISE <malaise@telemaque>
2012-04-13 18:47:40 +0200

    Enrich type Any

--- reposit/any_def.adb
+++ reposit/any_def.adb
@@ -1,18 +1,24 @@
-with Int_Image, Float_Image, Mixed_Str;
+with Int_Image, Float_Image, Mixed_Str, Address_Ops;
 package body Any_Def is
 
+  function Integer_Image is new Int_Image (Integer);
   function Inte_Image is new Int_Image (My_Math.Inte);
+  function Flo_Image is new Float_Image (Float);
   function Real_Image is new Float_Image (My_Math.Real);
 
 
   function Image (A : Any) return String is
   begin
     case A.Kind is
-      when None_Kind => return "";
-      when Bool_Kind => return Mixed_Str (A.Bool'Img);
-      when Inte_Kind => return Inte_Image (A.Inte);
-      when Real_Kind => return Real_Image (A.Real);
-      when Str_Kind  => return A.Str.Image;
+      when None_Kind    => return "";
+      when Bool_Kind    => return Mixed_Str (A.Bool'Img);
+      when Integer_Kind => return Integer_Image (A.Int);
+      when Inte_Kind    => return Inte_Image (A.Inte);
+      when Real_Kind    => return Real_Image (A.Real);
+      when Float_Kind   => return Flo_Image (A.Flo);
+      when Str_Kind     => return A.Str.Image;
+      when Address_Kind => return Address_Ops.Image (A.Address);
+      when Access_Kind  => return "->" & Image(A.Acc.all);
     end case;
   end Image;
 
--- reposit/any_def.ads
+++ reposit/any_def.ads
@@ -1,18 +1,32 @@
+with System;
 with As.U, My_Math;
 package Any_Def is
 
   -- An "any" is a multi-type container
-  -- An "any" can be empty, a boolean, integer, real or string
-  type Any_Kind_List is (None_Kind, Bool_Kind, Inte_Kind, Real_Kind, Str_Kind);
+  -- An "any" can be empty, a boolean, integer, real, string, address
+  --  or access to an "any"
+  type Any_Kind_List is (None_Kind,
+                         Bool_Kind,
+                         Inte_Kind, Integer_Kind,
+                         Real_Kind, Float_Kind,
+                         Str_Kind,
+                         Address_Kind,
+                         Access_Kind);
 
+  type Any (Kind : Any_Kind_List := None_Kind);
+  type Any_Access is access all Any;
   -- Content of an "any"
   type Any (Kind : Any_Kind_List := None_Kind) is record
     case Kind is
-      when None_Kind => null;
-      when Bool_Kind => Bool : Boolean;
-      when Inte_Kind => Inte : My_Math.Inte;
-      when Real_Kind => Real : My_Math.Real;
-      when Str_Kind  => Str  : As.U.Asu_Us;
+      when None_Kind    => null;
+      when Bool_Kind    => Bool    : Boolean;
+      when Integer_Kind => Int     : Integer;
+      when Inte_Kind    => Inte    : My_Math.Inte;
+      when Real_Kind    => Real    : My_Math.Real;
+      when Float_Kind   => Flo     : Float;
+      when Str_Kind     => Str     : As.U.Asu_Us;
+      when Address_Kind => Address : System.Address;
+      when Access_Kind  => Acc     : Any_Access;
     end case;
   end record;
 
