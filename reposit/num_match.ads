c872107f06fb15fb1643e5e26a75b40de425c42b
Pascal MALAISE <malaise@magic.fr>
2014-07-31 19:07:13 +0200

    Fix comment

--- reposit/num_match.ads
+++ reposit/num_match.ads
@@ -4,10 +4,11 @@
 -- <spec>    ::= <num> | <range>
 -- <range>   ::= [ <num> ] - [ <num> ]
 -- <num>     ::= Natural
+-- No num matches the empty spec "", every num matches the range "-"
 -- Raises Invalid_Criteria if incorrect criteria
 -- Raises Constraint_Error if Num < 0
+-- The order of the successive <spec> is not significant
 -- Example:  "-5,10-15,7,30-"
--- no num match "", very num match "-"
 generic
   type Integer_Type is range <>;
 package Num_Match is
