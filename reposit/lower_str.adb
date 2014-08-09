f82e2ceab8368222d67e4cab62fbf8ea29f50e32
Pascal MALAISE <malaise@magic.fr>
2014-08-09 12:23:55 +0200

    Use upper/lower char

--- reposit/lower_str.adb
+++ reposit/lower_str.adb
@@ -1,15 +1,12 @@
 -- Convert the characters of Str into lower char
+with Lower_Char;
 function Lower_Str (Str : String) return String is
-  Offset  : constant Integer   := Character'Pos('A') - Character'Pos('a');
-  Str_Loc : String (Str'Range) := Str;
+  Str_Loc : String (Str'Range);
 begin
 
   for I in Str_Loc'Range loop
-    if Str_Loc(I) in 'A' .. 'Z' then
-      Str_Loc(I) := Character'Val (Character'Pos (Str_Loc(I)) - Offset);
-    end if;
+    Str_Loc(I) := Lower_Char (Str(I));
   end loop;
-
   return Str_Loc;
 
 end Lower_Str;
--- reposit/upper_str.adb
+++ reposit/upper_str.adb
@@ -1,15 +1,12 @@
 -- Convert the characters of Str into upper char
+with Upper_Char;
 function Upper_Str (Str : String) return String is
-  Offset  : constant Integer   := Character'Pos('A') - Character'Pos('a');
-  Str_Loc : String (Str'Range) := Str;
+  Str_Loc : String (Str'Range);
 begin
 
   for I in Str_Loc'Range loop
-    if Str_Loc(I) in 'a' .. 'z' then
-      Str_Loc(I) := Character'Val (Character'Pos (Str_Loc(I)) + Offset);
-    end if;
+    Str_Loc(I) := Upper_Char (Str(I));
   end loop;
-
   return Str_Loc;
 
 end Upper_Str;
