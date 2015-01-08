23cedd723933cd39eaa7f9a98119cf408234c349
Pascal MALAISE <malaise@magic.fr>
2015-01-08 19:01:22 +0100

    Add new disks and make def_enigma independant from positions in XML

--- usr/enigma/def_enigma.adb
+++ usr/enigma/def_enigma.adb
@@ -5,9 +5,12 @@
 -- <switches> is pairs of letters, each letter appears at most once
 -- See Def_Enigma.txt for more information
 
+-- Def_Enigma expects that reflectors named A, B and C exists
+--  and that rotors I to X exist and have (at least) a carry
+
 with Ada.Calendar;
 with As.B, Perpet, Argument, Day_Mng, Normal, Upper_Str, Rnd,
-     Num_Letters, Basic_Proc, Str_Util, Parser, Trace.Loggers;
+     Num_Letters, Basic_Proc, Str_Util, Parser, Trace.Loggers, Romanic;
 with Types, Scrambler_Gen, Definition;
 procedure Def_Enigma is
 
@@ -537,7 +540,8 @@ begin
             raise Key_Error;
           end if;
           -- Check and adjust rotor num
-          Id := Xml.Get_Id (True, Str(Str'First .. Arob - 1));
+          Id := Romanic.Romanic2Arabic (Romanic.Value (
+                  Str(Str'First .. Arob - 1)));
           if Id = 0 then
             raise Key_Error;
           end if;
@@ -643,6 +647,7 @@ begin
     end;
 
     -- Reflector: daynum 1 -> A, 2 -> B, 3 -> C, 1 -> A...
+    -- No offset (A)
     Reflector_Num := ((Day - 1) rem 3) + 1;
     Reflector.Set (To_Letter (Reflector_Num) & 'A');
 
@@ -657,15 +662,15 @@ begin
       Day_3 := Upper_Str (
           Perpet.Day_Of_Week_List'Image (Perpet.Get_Day_Of_Week (T)))(1..3);
 
-      -- First rotor: day modulo 1 .. 10
+      -- First rotor: Day modulo 1 .. 10 @ First letter of day name
       Num := Store (((Day - 1) rem 10) + 1, 1);
       Rotors.Set (To_Letter (Num) & Day_3(1));
       Init_Offset.Set (Month_3(1));
-      -- Second rotor Month / 10
+      -- Second rotor: Day / 10 @ Second letter of day name
       Num := Store ((Day / 10) + 1, 2);
       Rotors.Append (To_Letter (Num) & Day_3(2));
       Init_Offset.Append (Month_3(2));
-      -- Third rotor
+      -- Third rotor: Month modulo 1 .. 10 @ Third letter of day name
       Num := Store (((Month - 1) rem 10) + 1, 3);
       Rotors.Append (To_Letter (Num) & Day_3(3));
       Init_Offset.Append (Month_3(3));
@@ -696,7 +701,8 @@ begin
         end if;
         declare
           Name : constant String
-               := Xml.Get_Name (True, To_Id (Rotors.Element (I)));
+               := Romanic.Image (Romanic.Arabic2Romanic (
+                   To_Id (Rotors.Element (I))));
         begin
           Basic_Proc.Put_Output (Name & '@');
         end;
