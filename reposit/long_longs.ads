884b04724af2804dc81af615119bb0fe9718d4bf
Pascal MALAISE <malaise@magic.fr>
2015-02-17 19:49:32 +0100

    Add Ll_Mod

--- reposit/long_longs.ads
+++ reposit/long_longs.ads
@@ -4,5 +4,8 @@ package Long_Longs is
   subtype Ll_Natural  is Ll_Integer range 0 .. Ll_Integer'Last;
   subtype Ll_Positive is Ll_Integer range 1 .. Ll_Integer'Last;
 
+  type Ll_Mod is mod Ll_Integer'Last + 1;
+  for Ll_Mod'Size use Ll_Integer'Size;
+
 end Long_Longs;
 
