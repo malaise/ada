d29c3434eb250c87002718ae6ae07df1d3c956ba
Pascal MALAISE <malaise@magic.fr>
2015-02-28 10:58:27 +0100

    Add Parser.Keywords

---
+++ reposit/parser-keywords.adb
@@ -0,0 +1,85 @@
+package body Parser.Keywords is
+
+  -- Keywords and data stored in Hased list
+  procedure Set (To : out Cell_Type; Val : in Cell_Type) is
+  begin
+    To := Val;
+  end Set;
+  function "=" (Current : Cell_Type; Criteria : Cell_Type) return Boolean is
+    use type As.U.Asu_Us;
+  begin
+    return Current.Keyword = Criteria.Keyword;
+  end "=";
+  function Image (Cell : Cell_Type) return String is
+  begin
+    return Cell.Keyword.Image;
+  end Image;
+
+
+  -- Add a keyword
+  -- Any redefinition of a keyword overwrites the previous
+  procedure Add (Keys : in out Iterator;
+                 Keyword : in As.U.Asu_Us; Data : in Data_Type) is
+  begin
+    Keys.List.Insert ( (Keyword, Data) );
+  end Add;
+
+  -- Clears all the words
+  procedure Reset (Keys : in out Iterator) is
+  begin
+    Keys.List.Delete_List;
+  end Reset;
+
+  -- Set the sentence to parse, the parsing will start from the Next_Word
+  procedure Set (Keys : in out Iterator; Iter : in Parser.Iterator) is
+  begin
+    Parser.Copy (Keys.Iter, Iter);
+    Keys.End_Reached := False;
+  end Set;
+
+  -- Result of parsing
+  -- Next_Word is a keyword, not a keyword, or end iterator is reached
+  -- type Result_Kind is (Found, Not_Found, End_Reached);
+
+  -- type Result_Rec (Kind : Result_Kind := Found) is record
+  --   case Kind is
+  --     when Found =>
+  --       -- Keyword found
+  --       Keyword : As.U.Asu_Us;
+  --       Data    : Data_Type;
+  --     when Not_Found =>
+  --       Word : As.U.Asu_Us;
+  --     when End_Reached => null;
+  --   end case;
+  -- end record;
+
+  -- Parse next word
+  -- Raises End_Error if End_Reached has already been returned
+  function Next_Word (Keys : in out Iterator) return Result_Rec is
+    Found : Boolean;
+    Crit : Cell_Type;
+  begin
+    if Keys.End_Reached then
+      -- End already reached
+      raise End_Error;
+    end if;
+    -- Read next word of iterator
+    Crit.Keyword.Set (Keys.Iter.Next_Word);
+    if Crit.Keyword.Is_Null then
+      -- End of iterator
+      Keys.End_Reached := True;
+      return (Kind => End_Reached);
+    end if;
+    -- Search keyword
+    Keys.List.Search (Crit, Found);
+    if Found then
+      Keys.List.Read (Crit);
+      return (Kind => Parser.Keywords.Found,
+              Keyword => Crit.Keyword, Data => Crit.Data);
+    else
+      return (Kind => Not_Found, Word => Crit.Keyword);
+    end if;
+  end Next_Word;
+
+end Parser.Keywords;
+
---
+++ reposit/parser-keywords.ads
@@ -0,0 +1,62 @@
+-- Parser of keywords
+with As.U, Hashed_List.Unique;
+generic
+  -- Private data stored with the keyword
+  type Data_Type is private;
+package Parser.Keywords is
+
+  -- Keywords parser iterator
+  type Iterator is tagged limited private;
+
+  -- Add a keyword
+  -- Any redefinition of a keyword overwrites the previous
+  procedure Add (Keys : in out Iterator;
+                 Keyword : in As.U.Asu_Us; Data : in Data_Type);
+  -- Clears all the words
+  procedure Reset (Keys : in out Iterator);
+
+  -- Set the sentence to parse, the parsing will start from the Next_Word
+  procedure Set (Keys : in out Iterator; Iter : in Parser.Iterator);
+
+  -- Result of parsing
+  -- Next_Word is a keyword, not a keyword, or end iterator is reached
+  type Result_Kind is (Found, Not_Found, End_Reached);
+  type Result_Rec (Kind : Result_Kind := Found) is record
+    case Kind is
+      when Found =>
+        -- Keyword found
+        Keyword : As.U.Asu_Us;
+        Data    : Data_Type;
+      when Not_Found =>
+        Word : As.U.Asu_Us;
+      when End_Reached => null;
+    end case;
+  end record;
+
+  -- Parse next word
+  -- Raises End_Error if End_Reached has already been returned
+  End_Error : exception;
+  function Next_Word (Keys : in out Iterator) return Result_Rec;
+
+private
+
+  type Cell_Type is record
+    Keyword : As.U.Asu_Us;
+    Data : Data_Type;
+  end record;
+  type Cell_Access is access all Cell_Type;
+  procedure Set (To : out Cell_Type; Val : in Cell_Type);
+  function "=" (Current : Cell_Type; Criteria : Cell_Type) return Boolean;
+  function Image (Cell : Cell_Type) return String;
+  package H_Cell_List_Mng is new Hashed_List (Cell_Type, Cell_Access,
+             Set, "=", Image);
+  package Cell_List_Mng is new H_Cell_List_Mng.Unique;
+
+  type Iterator is tagged limited record
+    List : Cell_List_Mng.Unique_List_Type;
+    End_Reached : Boolean := False;
+    Iter : Parser.Iterator;
+  end record;
+
+end Parser.Keywords;
+
