b517253f4edc0f5ae5ecc9d4acaa40c1a5269a8f
Pascal MALAISE <malaise@magic.fr>
2015-03-02 18:48:05 +0100

    Add syntax 'Severity+' for traces

--- usr/tests/t_trace.adb
+++ usr/tests/t_trace.adb
@@ -9,10 +9,22 @@ procedure T_Trace is
 begin
   Basic_Proc.Put_Line_Output ("Starting");
   Logn.Init ("Logn");
+
+  Bloga.Log_Error ("Bloga Error");
+  Blogn.Log_Error ("Blogn Error");
+  Loga.Log_Error ("Loga Error");
+  Logn.Log_Error ("Logn Error");
+
+  Bloga.Log_Warning ("Bloga Warning");
+  Blogn.Log_Warning ("Blogn Warning");
+  Loga.Log_Warning ("Loga Warning");
+  Logn.Log_Warning ("Logn Warning");
+
   Bloga.Log_Info ("Bloga Info");
   Blogn.Log_Info ("Blogn Info");
   Loga.Log_Info ("Loga Info");
   Logn.Log_Info ("Logn Info");
+
   Bloga.Log_Debug ("Bloga Debug");
   Blogn.Log_Debug ("Blogn Debug");
   Loga.Log_Debug ("Loga Debug");
