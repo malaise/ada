b810884637df68e0c8d2b027d86efbf90c570783
Pascal MALAISE <malaise@magic.fr>
2015-08-14 17:58:31 +0200

    Handle status='T' (type changed)

--- usr/agite/commit.adb
+++ usr/agite/commit.adb
@@ -188,7 +188,7 @@ package body Commit is
     Changes.Move_At (Afpx.Line_List.Get_Position);
     Status := Changes.Access_Current.S3;
     if Stage then
-      if Status = '?' or else Status = 'M' then
+      if Status = '?' or else Status = 'M' or else Status = 'T' then
         -- Stage new file or modif or unresolved conflict
         Git_If.Do_Add (Changes.Access_Current.Name.Image);
       elsif Status = 'D' then
@@ -234,7 +234,7 @@ package body Commit is
       Changes.Rewind;
       loop
         Changes.Read (Change, Moved => Moved);
-        if Change.S3 = 'M' then
+        if Change.S3 = 'M' or else Change.S3 = 'T' then
           Git_If.Do_Add (Change.Name.Image);
         elsif Change.S3 = 'D' then
           Git_If.Do_Rm (Change.Name.Image);
