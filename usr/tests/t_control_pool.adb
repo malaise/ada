83136870192f5d0f7d938b7acbac703a8f852e2e
Pascal MALAISE <malaise@magic.fr>
2015-11-18 18:37:42 +0100

    Protect and improve output Client becomes not interrupted while processing resource

--- usr/tests/t_control_pool.adb
+++ usr/tests/t_control_pool.adb
@@ -1,23 +1,41 @@
 with Ada.Calendar;
-with Rnd, Event_Mng, Control_Pool, Sys_Calls;
+with Rnd, Event_Mng, Control_Pool, Sys_Calls, Mutex_Manager, Images;
 procedure T_Control_Pool is
 
   -- The pool to control resources
   subtype Pool_Range is Positive range 1 .. 5;
-  package Cotrol_Pool_Mng is new Control_Pool (Pool_Range);
-  Pool : Cotrol_Pool_Mng.Controlled_Pool_Type;
+  package Control_Pool_Mng is new Control_Pool (Pool_Range);
+  Pool : Control_Pool_Mng.Controlled_Pool_Type;
 
-  -- Detect CtrlC and signal to taksks the of program
-  Done : Boolean := False;
+  -- Protected logger
+  Mutex : Mutex_Manager.Mutex (Mutex_Manager.Simple, False);
+  procedure Log (Msg : in String) is
+  begin
+    Mutex.Get;
+    Sys_Calls.Put_Line_Output (Msg);
+    Mutex.Release;
+  end Log;
+
+  -- Detect Ctrl-C and signal to tasks the end of program
+  Main_Done : Boolean := False;
   procedure Term_Cb is
   begin
-    Done := True;
+    Log ("Term Cb");
+    Main_Done := True;
   end Term_Cb;
+
+  -- (Rounded) image of a duration
+  function Dur_Image (Dur : in Duration) return String is
+  begin
+    -- 3 digits after dot and no Plus sign
+    return (if Dur >= 0.0 then Images.Dur_Image (Dur, 3, False) & "s"
+            else "Infinite");
+  end Dur_Image;
 
   -- The clients of resources
   subtype Client_Range is Positive range 1 .. 9;
   task type Client_Task is
-    entry Start (Client_No : in Client_Range; Client_Pid : out Sys_Calls.Pid);
+    entry Start (Client_No : in Client_Range);
     entry Stop;
   end Client_Task;
 
@@ -27,41 +45,59 @@ procedure T_Control_Pool is
     Got : Boolean;
     T1 : Ada.Calendar.Time;
     Dur1, Dur2 : Duration;
+    Client_Done : Boolean := False;
     use type Ada.Calendar.Time;
   begin
-    accept Start (Client_No : in Client_Range;
-                  Client_Pid : out Sys_Calls.Pid) do
+    accept Start (Client_No : in Client_Range) do
       My_No := Client_No;
-      Client_Pid := Sys_Calls.Get_Pid;
-      Sys_Calls.Put_Line_Output (My_No'Img & ": started with pid "
-                               & Client_Pid'Img);
+      Log (My_No'Img & ": started");
     end Start;
     loop
       -- Get a random pool resource
       Pool_No := Rnd.Gen.Int_Random(Pool_Range'First, Pool_Range'Last);
       T1 := Ada.Calendar.Clock;
       Dur1 := Rnd.Gen.Dur_Random (-0.1, 3.0);
-      Sys_Calls.Put_Line_Output (My_No'Img & ": getting " & Pool_No'Img
-                               & " for " & Dur1'Img);
+      Log (My_No'Img & ": getting" & Pool_No'Img & " for " & Dur_Image (Dur1));
       Got := Pool.Get(Pool_No, Dur1);
       Dur2 := Ada.Calendar.Clock - T1;
-      Sys_Calls.Put_Line_Output (My_No'Img & ": got " & Pool_No'Img
-                               & " " & Got'Img
-           & " after " & Dur2'Img & " for " & Dur1'Img);
+      Log (My_No'Img & ": got" & Pool_No'Img & " " & Got'Img
+           & " after " & Dur_Image (Dur2) & " for " & Dur_Image (Dur1));
+
+      -- Check that no signal, otherwise give up
+      select
+        accept Stop;
+        Client_Done := True;
+      or
+        delay 0.0;
+        Client_Done := Main_Done;
+      end select;
+
       if Got then
-        -- Wait a bit (1 to 5 s) or signal
-        Sys_Calls.Put_Line_Output (My_No'Img & ": working on " & Pool_No'Img);
-        select
-          accept Stop;
-        or
+        if not Client_Done then
+          -- Wait a bit (1 to 5 s) to simulate ectivity
+          Log (My_No'Img & ": working on" & Pool_No'Img);
           delay Rnd.Gen.Dur_Random (1.0, 5.0);
-        end select;
-        Sys_Calls.Put_Line_Output (My_No'Img & ": releasing " & Pool_No'Img);
+        end if;
+        -- Release resource
+        Log (My_No'Img & ": releasing" & Pool_No'Img);
         Pool.Release (Pool_No);
       end if;
-      exit when Done;
+
+      -- Exit on signal
+      exit when Client_Done;
+      -- Check that no signal, otherwise give up
+      select
+        accept Stop;
+        Client_Done := True;
+      or
+        delay 0.0;
+        Client_Done := Main_Done;
+      end select;
+      exit when Client_Done;
     end loop;
-    Sys_Calls.Put_Line_Output (My_No'Img & ": exit");
+
+    Log (My_No'Img & ": exit");
+    -- Stop has already been accepted, or not yet
     select
       accept Stop;
     or
@@ -70,25 +106,33 @@ procedure T_Control_Pool is
   end Client_Task;
 
   Clients : array (Client_Range) of Client_Task;
-  Pids : array (Client_Range) of Sys_Calls.Pid;
 
 begin
+  Log ("Start");
+  -- General init
   Rnd.Gen.Randomize;
-  Event_Mng. Set_Sig_Term_Callback (Term_Cb'Unrestricted_Access);
+  Event_Mng.Set_Sig_Term_Callback (Term_Cb'Unrestricted_Access);
 
+  -- Init clients, each with a specific Id
   for I in Client_Range loop
-    Clients(I).Start(I, Pids(I));
+    Clients(I).Start(I);
   end loop;
 
+  -- Wait until Term_Cb
   loop
-    exit when Event_Mng.Wait (1_000) and then Done;
+    exit when Event_Mng.Wait (1_000) and then Main_Done;
   end loop;
 
+  -- Stop clients
   for I in Client_Range loop
     Clients(I).Stop;
   end loop;
 
+  -- Clear the control pool
   Pool.Clear;
+
+  -- The end
+  Log ("Done");
 
 end T_Control_Pool;
 
