dcfebe7aaa92c8cf07fdd0b1e942ae90e4cc99f8
Pascal MALAISE <malaise@magic.fr>
2016-08-29 18:44:39 +0200

    Cosmetic (fix space)

--- reposit/lower_char.adb
+++ reposit/lower_char.adb
@@ -3,6 +3,6 @@ function Lower_Char (Char : Character) return Character is
   Offset  : constant Integer   := Character'Pos('A') - Character'Pos('a');
 begin
   return (if Char not in 'A' .. 'Z' then Char
-          else Character'Val( Character'Pos(Char) - Offset));
+          else Character'Val (Character'Pos(Char) - Offset));
 end Lower_Char;
 
--- reposit/upper_char.adb
+++ reposit/upper_char.adb
@@ -3,6 +3,6 @@ function Upper_Char (Char : Character) return Character is
   Offset  : constant Integer   := Character'Pos('A') - Character'Pos('a');
 begin
   return (if Char not in 'a' .. 'z' then Char
-          else Character'Val( Character'Pos(Char) + Offset));
+          else Character'Val (Character'Pos(Char) + Offset));
 end Upper_Char;
 
