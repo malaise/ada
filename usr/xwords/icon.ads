bd32892daff64d8eccd0f4e8440aa279935a728b
Pascal MALAISE <malaise@magic.fr>
2017-03-01 19:18:05 +0100

    Alook

--- usr/xwords/icon.ads
+++ usr/xwords/icon.ads
@@ -1,6 +1,6 @@
 with As.U, Con_Io;
 package Icon is
-  Xwords_xpm : constant Con_Io.Icon_Pixmap := (
+  Xwords_Xpm : constant Con_Io.Icon_Pixmap := (
     As.U.Tus ("48 48 26 1"),
     As.U.Tus ("  c None"),
     As.U.Tus (". c #0E0E09"),
