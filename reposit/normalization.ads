3d0ff630df88e3463b4cf09592f909b83d95c753
Pascal MALAISE <malaise@magic.fr>
2018-04-27 18:54:40 +0200

    Fix comment of Normal_Fixed

--- reposit/normalization.ads
+++ reposit/normalization.ads
@@ -59,8 +59,8 @@ package Normalization is
                           Exp   : Positive) return String;
 
   -- Puts a float F, a real R or a delta D in a string of fixed length.
-  -- At least space/sign, then one digit, then a '.', then some fraction part,
-  --  then some Gap character if needed
+  -- At least a pace/sign, then Fore digits, then a '.', then some fraction
+  --  part, then some Gap character if needed to fit Len
   -- Fore is the length before the dot, padded with spaces if needed
   -- Example: Len=7, Fore=3, Gap='@' => " -x.yy@"
   -- A warning char at the beginning of the output signals an integer part
