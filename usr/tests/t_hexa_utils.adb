2906501ea33c7b33ac7d52e0ca9403e15e833409
Pascal MALAISE <malaise@magic.fr>
2019-03-27 19:19:54 +0100

    Set int limits through both first arguments

--- usr/tests/t_hexa_utils.adb
+++ usr/tests/t_hexa_utils.adb
@@ -1,27 +1,33 @@
 with Basic_Proc, Hexa_Utils, Argument;
 procedure T_Hexa_Utils is
-  Valid : Boolean;
-  V : Integer;
-  function Int_Image is new Hexa_Utils.Int_Image (Integer);
+  Min, Max : Integer;
 
-  subtype Subinteger is Integer range -21 .. Integer'Last;
-  function Sub_Image is new Hexa_Utils.Int_Image (Subinteger);
-begin
-
-  for I in 1 .. Argument.Get_Nbre_Arg loop
-    begin
-      Valid := False;
-      V := Integer'Value (Argument.Get_Parameter (Occurence => I));
-      Valid := True;
-    exception
+  function Valid (I : Positive) return Integer is
+  begin
+    return Integer'Value (Argument.Get_Parameter (Occurence => I));
+  exception
       when others =>
         Basic_Proc.Put_Line_Error ("Invalid_Argument");
-raise;
-    end;
-    if Valid then
-      Basic_Proc.Put_Line_Output (">" &
-        (if V in Subinteger then Sub_Image (V) else Int_Image (V)) & "<");
-    end if;
-  end loop;
+        raise;
+  end Valid;
+begin
+  if Argument.Get_Nbre_Arg >= 1 then
+    Min := Valid (1);
+  else
+    Min := Integer'First;
+  end if;
+  if Argument.Get_Nbre_Arg >= 2 then
+    Max := Valid (2);
+  else
+    Max := Integer'Last;
+  end if;
+  declare
+    subtype Subinteger is Integer range Min .. Max;
+    function Image is new Hexa_Utils.Int_Image (Subinteger);
+  begin
+    for I in 1 .. Argument.Get_Nbre_Arg loop
+      Basic_Proc.Put_Line_Output (">" & Image (Valid (I)) & "<");
+    end loop;
+  end;
 end T_Hexa_Utils;
 
