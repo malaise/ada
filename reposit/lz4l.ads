cdb3a1ef2ed0b916e1d7f9749ce46b192721d3a4
Pascal MALAISE <malaise@magic.fr>
2019-07-01 19:35:37 +0200

    Add lz4l, with large buffers

---
+++ reposit/lz4l.ads
@@ -0,0 +1,28 @@
+with C_Types, Long_Longs;
+-- Lz4 compression of large buffer
+package Lz4L is
+
+  -- An array of bytes to (un)compress
+  subtype Ll_Natural is Long_Longs.Ll_Natural;
+  subtype Ll_Positive is Long_Longs.Ll_Positive;
+  subtype Byte is C_Types.Byte;
+  type Byte_Array is array (Ll_Positive range <>) of Byte;
+
+  -- Compress Input into Output
+  -- Outlen is at most an extra 8 MBbs for 2 GB (0.4%)
+  -- May raise Too_Big if Output is too small
+  procedure Compress (Input  : in Byte_Array;
+                      Output : out Byte_Array;
+                      Outlen : out Ll_Natural);
+
+  -- Uncompress Input into Output
+  -- May raise Too_Big if Output is too small
+  procedure Uncompress (Input  : in Byte_Array;
+                        Output : out Byte_Array;
+                        Outlen : out Ll_Natural);
+
+  -- Output is too small or an offset is too large
+  Too_Big : exception;
+
+end Lz4L;
+
