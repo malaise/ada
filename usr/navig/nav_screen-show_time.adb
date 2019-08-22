40510ed56bdb1903a8282d5308915b8139906b6d
Pascal MALAISE <malaise@magic.fr>
2019-08-22 16:42:59 +0200

    Small fix

--- usr/navig/nav_screen-show_time.adb
+++ usr/navig/nav_screen-show_time.adb
@@ -4,11 +4,10 @@ with Date_Text;
 
 separate (Nav_Screen)
 procedure Show_Time is
-  Date : Date_Text.Date_Rec;
+  -- Get date and hours, minutes and seconds
+  Date : constant Date_Text.Date_Rec := Date_Text.Split (Ada.Calendar.Clock);
 begin
-  -- Get date and ours, minutes and seconds
-  Date := Date_Text.Split (Ada.Calendar.Clock);
-  -- put
+  -- Put
   W_Time.Move ( (0, 0));
   W_Time.Put (
    S => Date_Text.Put (Date, "%d/%m/%Y %H:%M:%S"));
