7f1726f2dfba6edc33a182756879c830cb033f99
Pascal MALAISE <malaise@magic.fr>
2019-08-22 11:39:54 +0200

    Add Arbitrary variant in Any

--- reposit/any_def.adb
+++ reposit/any_def.adb
@@ -5,17 +5,18 @@ package body Any_Def is
 
   function Image (A : Any) return String is
     (case A.Kind is
-       when None_Kind     => "",
-       when Bool_Kind     => Mixed_Str (A.Bool'Img),
-       when Trilean_Kind  => Trilean.Image (A.Tril),
-       when Integer_Kind  => Images.Integer_Image (A.Int),
-       when Lint_Kind     => Images.Llint_Image (A.Lint),
-       when Duration_Kind => Images.Dur_Image (A.Dur, 9, False),
-       when Float_Kind    => Images.Float_Image (A.Flo),
-       when Real_Kind     => Real_Image (A.Real),
-       when Str_Kind      => A.Str.Image,
-       when Address_Kind  => Address_Ops.Image (A.Addr),
-       when Access_Kind   => "->" & Image(A.Acc.all) );
+       when None_Kind      => "",
+       when Bool_Kind      => Mixed_Str (A.Bool'Img),
+       when Trilean_Kind   => Trilean.Image (A.Tril),
+       when Integer_Kind   => Images.Integer_Image (A.Int),
+       when Lint_Kind      => Images.Llint_Image (A.Lint),
+       when Arbitrary_Kind => Images.Arbitrary_Image (A.Arbi),
+       when Duration_Kind  => Images.Dur_Image (A.Dur, 9, False),
+       when Float_Kind     => Images.Float_Image (A.Flo),
+       when Real_Kind      => Real_Image (A.Real),
+       when Str_Kind       => A.Str.Image,
+       when Address_Kind   => Address_Ops.Image (A.Addr),
+       when Access_Kind    => "->" & Image(A.Acc.all) );
 
 end Any_Def;
 
