7d8f0e6aa1bd8b4884e833d36b99fcf42e80337c
Pascal MALAISE <malaise@magic.fr>
2020-07-13 17:17:50 +0200

    Alook

--- usr/great_circle/intercept-fpl.adb
+++ usr/great_circle/intercept-fpl.adb
@@ -166,7 +166,7 @@ package body Fpl is
       := (X => Conv.Deg2Rad (Conv.Deg_Coord_Range (Ades_Lon)),
           Y => Conv.Deg2Rad (Conv.Deg_Coord_Range (Ades_Lat)));
     H : constant Conv.Rad_Coord_Range
-      := Conv.Deg2Rad (Conv.reduct (Conv.Degree (Ang)
+      := Conv.Deg2Rad (Conv.Reduct (Conv.Degree (Ang)
                                   + Conv.Degree (Declination)));
     D : constant String_Util.Distance := String_Util.Distance (Dst);
     B : Lat_Lon.Lat_Lon_Rad_Rec;
