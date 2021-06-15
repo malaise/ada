a194d98e50fb2c6d6194a68a0e21d4eb3694265d
Pascal MALAISE <malaise@magic.fr>
2021-06-15 18:58:01 +0200

    Rename C_Types.Timeval_T

--- reposit/timeval.adb
+++ reposit/timeval.adb
@@ -11,9 +11,8 @@ package body Timeval is
   Million : constant := 1_000_000;
   Milliond : constant Duration := Duration (Million);
 
-  function Delta2Timeout (Delta_Date : Perpet.Delta_Rec)
-                         return C_Types.Timeval_T is
-    Result : C_Types.Timeval_T;
+  function Delta2Timeout (Delta_Date : Perpet.Delta_Rec) return Timeval_T is
+    Result : Timeval_T;
   begin
     -- Split Delta.Secs into Result.Tv_Sec and Result.Tv_Usec
     Result.Tv_Sec := C_Types.Time_T (Delta_Date.Secs / Dur_Day);
@@ -27,7 +26,7 @@ package body Timeval is
       raise Timeval_Error;
   end Delta2Timeout;
 
-  function Timeout2Delta (Timeout : C_Types.Timeval_T) return Perpet.Delta_Rec is
+  function Timeout2Delta (Timeout : Timeval_T) return Perpet.Delta_Rec is
     Result : Perpet.Delta_Rec;
   begin
     -- Split Timeout.Tv_Sec into Result.Days and Result.Secs
@@ -43,14 +42,14 @@ package body Timeval is
       raise Timeval_Error;
   end Timeout2Delta;
 
-  function Image (Timeout : C_Types.Timeval_T) return String is
+  function Image (Timeout : Timeval_T) return String is
     (if Timeout.Tv_Sec < 0 or else Timeout.Tv_Usec < 0 then "Infinite"
      else Sec_Image (Timeout.Tv_Sec) & "."
         & Normal (Integer (Timeout.Tv_Usec), 6, Gap => '0'));
 
   -- Ensure that abs(Tv_Usec) < 1_000_000 and that Tv_Sec and Tv_Usec have the
   --  same sign
-  procedure Normalize (Timeout : in out C_Types.Timeval_T) is
+  procedure Normalize (Timeout : in out Timeval_T) is
   begin
     -- Ensure that abs(Tv_Usec) < Million. Adjust Tv_Sec
     while Timeout.Tv_Usec <= -Million loop
