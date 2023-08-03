b115584919871d9970904aef64641c0bf24b6685
Pascal MALAISE <pascal.malaise@free.fr>
2023-08-03 19:08:59 +0200

    Update help (regsubst)

--- usr/mcd/mcd_mng.adb
+++ usr/mcd/mcd_mng.adb
@@ -4,7 +4,7 @@ pragma Elaborate_All (Random);
 package body Mcd_Mng is
 
   -- Current version
-  Mcd_Version : constant String := "V21.1";
+  Mcd_Version : constant String := "V22.0";
 
   package Stack is
     -- What can we store in stack
