c882dcf533e71693f8f6b127e4023a5017e4f9d1
Pascal MALAISE <pascal.malaise@free.fr>
2025-04-15 19:33:32 +0200

    Add option for reverse sorting

--- usr/misc/astat.adb
+++ usr/misc/astat.adb
@@ -11,7 +11,8 @@ procedure Astat is
   Args : Argument_Parser.Parsed_Dscr;
   Java_Syntax : Boolean := False;
   Details : Boolean := True;
-  Sort : Boolean := False;
+  Crescent : Boolean := False;
+  Decrescent : Boolean := False;
 
   -- What is stored for each file
   type Cell_Rec is record
@@ -29,22 +30,29 @@ procedure Astat is
   -- Sorting
   function Less_Than (El1, El2 : Cell_Rec) return Boolean is
     (El1.Metric.Statements < El2.Metric.Statements);
-  procedure Sort_Cells is new Cell_List_Mng.Sort (Less_Than);
+  function More_Than (El1, El2 : Cell_Rec) return Boolean is
+    (El1.Metric.Statements > El2.Metric.Statements);
+  procedure Sort_Crescent is new Cell_List_Mng.Sort (Less_Than);
+  procedure Sort_Decrescent is new Cell_List_Mng.Sort (More_Than);
 
   -- Global statistics
   Max_Len : Positive;
   Total : One_File_Statements.Metrics;
 
   Keys : constant Argument_Parser.The_Keys_Type := (
-    1 => (False, 'h', As.U.Tus ("help"),  False),
-    2 => (False, 'q', As.U.Tus ("quiet"), False),
-    3 => (False, 'j', As.U.Tus ("java"),  False),
-    4 => (False, 's', As.U.Tus ("sort"),  False) );
+    1 => (False, 'h', As.U.Tus ("help"),    False),
+    2 => (False, 'q', As.U.Tus ("quiet"),   False),
+    3 => (False, 'j', As.U.Tus ("java"),    False),
+    4 => (False, 's', As.U.Tus ("sort"),    False),
+    5 => (False, 'r', As.U.Tus ("reverse"), False) );
 
   procedure Usage is
   begin
     Basic_Proc.Put_Line_Output ("Usage: " & Argument.Get_Program_Name
-      & " [ -q | --quiet ] [ -j | --java] [ -s | --sort ] { <file_name> }");
+      & " [ { <option> } ] { <file_name> }");
+    Basic_Proc.Put_Line_Output (
+      "<option> ::= -q | --quiet | -j | --java | -s | --sort | - r | --reverse"
+      );
   end Usage;
 
  Abort_Error : exception;
@@ -65,17 +73,17 @@ begin
   if not Args.Is_Ok then
     Error ("Invalid arguments: " & Args.Get_Error);
   end if;
+  -- Help
+  if Args.Is_Set (1) then
+    Usage;
+    Basic_Proc.Set_Error_Exit_Code;
+    return;
+  end if;
   if Args.Get_Nb_Embedded_Arguments /= 0
   or else Args.Get_Nb_Occurences (Argument_Parser.No_Key_Index) = 0 then
     Error ("Invalid arguments");
   end if;
   -- Options
-  if Args.Is_Set (1) then
-    -- Help
-    Usage;
-    Basic_Proc.Set_Error_Exit_Code;
-    return;
-  end if;
   if Args.Is_Set (2) then
     Details := False;
   end if;
@@ -83,7 +91,16 @@ begin
     Java_Syntax := True;
   end if;
   if Args.Is_Set (4) then
-    Sort := True;
+    Crescent := True;
+  end if;
+  if Args.Is_Set (5) then
+    Decrescent := True;
+  end if;
+  if Crescent and then Decrescent then
+    Error ("Incompatible sorting options");
+  end if;
+  if not Details and then (Crescent or else Decrescent) then
+    Error ("Quiet is incompatible with sorting");
   end if;
 
   -- Store names and stats, and compute stats and longest file name
@@ -112,8 +129,10 @@ begin
   end if;
 
   -- Sort
-  if Sort then
-    Sort_Cells (Cell_List);
+  if Crescent then
+    Sort_Crescent (Cell_List);
+  elsif Decrescent then
+    Sort_Decrescent (Cell_List);
   end if;
 
   -- Put all files
